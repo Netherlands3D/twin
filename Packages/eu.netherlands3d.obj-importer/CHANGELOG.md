32f467f7014bc36907d7e30b45f5b3f895951512
mvangog <martijn.van.gog@amsterdam.nl>
2023-07-25 14:56:06 +0200

    Extracted https://github.com/Amsterdam/Netherlands3D/tree/main/Packages/Netherlands3D/ModelParsing    into its own package

---
+++ Packages/eu.netherlands3d.obj-importer/CHANGELOG.md
@@ -0,0 +1,14 @@
+﻿# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+
+
+## [1.0.0]
+
+### Added
+
+* Extracted https://github.com/Amsterdam/Netherlands3D/tree/main/Packages/Netherlands3D/ModelParsing 
+  into its own package
---
+++ Packages/eu.netherlands3d.obj-importer/Runtime/Scripts/ImportOBJ/Scripts/ObjImportManager.cs
@@ -0,0 +1,224 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Netherlands3D.ModelParsing;
+using UnityEngine.Events;
+
+public class ObjImportManager : MonoBehaviour
+{
+    [Header("Required input")]
+    [SerializeField] Material baseMaterial;
+    bool expectingObjFile = false;
+    bool expectingMTLFile = false;
+    bool expectingImageFile = false;
+    
+    [Header("Settings")]
+    [SerializeField] bool createSubMeshes = false;
+
+    [Header("Result")]
+    [SerializeField] UnityEvent<GameObject> CreatedMoveableGameObject;
+    [SerializeField] UnityEvent<GameObject> CreatedImmoveableGameObject;
+
+    [Header("Progress")]
+    [SerializeField] UnityEvent<bool> busy;
+    [SerializeField] UnityEvent<string> currentActivity;
+    [SerializeField] UnityEvent<string> currentAction;
+    [SerializeField] UnityEvent<float> progressPercentage;
+    
+
+    [Header("Alerts and errors")]
+    [SerializeField] UnityEvent<string> alertmessage;
+    [SerializeField] UnityEvent<string> errormessage;
+
+    string objFileName = "";
+    string objfilename
+    {
+        get
+        {
+            return objFileName;
+        }
+        set
+        {
+            objFileName = value;
+
+            Debug.Log("received objFile: " + objFileName);
+
+        }
+    }
+
+    string mtlFileName = "";
+    string mtlfilename
+    {
+        get
+        {
+            return mtlFileName;
+        }
+        set
+        {
+            mtlFileName = value;
+        }
+    }
+
+
+
+    string imgFileName = "";
+    string imgfilename
+    {
+        get
+        {
+            return imgFileName;
+        }
+        set
+        {
+            imgFileName = value;
+
+            //if (ReceivedImageFilename) ReceivedImageFilename.InvokeStarted(System.IO.Path.GetFileName(imgFileName));
+        }
+    }
+
+
+
+    ObjImporter importer;
+    
+
+
+
+
+    public void ParseFiles(string value)
+    {
+        Debug.Log("receiveid files: " + value);
+
+        if (value == "")//empty string received, so no selection was made
+        {
+            return;
+        }
+        string[] filenames = value.Split(',');
+
+        foreach (var file in filenames)
+        {
+            string fileextention = System.IO.Path.GetExtension(file).ToLower();
+            switch (fileextention)
+            {
+                case ".obj":
+                    objfilename = System.IO.Path.Combine(Application.persistentDataPath, file);
+                    break;
+
+                case ".mtl":
+                    mtlfilename = System.IO.Path.Combine(Application.persistentDataPath, file);
+                    break;
+
+                case ".jpg":
+                case ".png":
+                case ".jpeg":
+                    imgfilename = System.IO.Path.Combine(Application.persistentDataPath, file);
+                    break;
+            }
+        }
+
+        if (objfilename != "")
+        {
+
+                OnStartImporting();
+
+        }
+    }
+
+    void OnOBJFileReceived(string value)
+    {
+        objfilename = value;
+    }
+
+    void OnMTLFileReceived(string value)
+    {
+        mtlfilename = value;
+    }
+
+    public void  SetImageFile(string value)
+    {
+        imgfilename = value;
+    }
+
+
+
+    public void Cancel()
+    {
+        BroadcastMessage("Cancel");
+       currentActivity.Invoke("cancelling the import");
+    }
+
+    void OnStartImporting()
+    {
+        ConnectToImporter();
+
+        importer.objFilePath = objfilename;
+        importer.mtlFilePath = mtlfilename;
+        importer.imgFilePath = imgfilename;
+
+        importer.BaseMaterial = baseMaterial;
+        importer.createSubMeshes = createSubMeshes;
+        busy.Invoke(true);
+        importer.StartImporting(OnOBJImported);
+    }
+
+    void OnOBJImported(GameObject returnedGameObject)
+    {
+        bool canBemoved = importer.createdGameobjectIsMoveable;
+
+        busy.Invoke(false);
+       
+
+        objfilename = string.Empty;
+        mtlfilename = string.Empty;
+        imgfilename = string.Empty;
+
+        if (importer != null) Destroy(importer);
+
+        if (canBemoved)
+        {
+            CreatedMoveableGameObject.Invoke(returnedGameObject);
+        }
+        else
+        {
+            CreatedImmoveableGameObject.Invoke(returnedGameObject);
+        }
+    }
+
+    void ConnectToImporter()
+    {
+        if (importer != null) Destroy(importer);
+
+        importer = gameObject.AddComponent<ObjImporter>();
+        // give the importer handles for progress- and errormessaging
+        importer.currentActivity = BroadcastCurrentActivity;
+        importer.currentAction = BroadcastCurrentAction;
+        importer.progressPercentage = BroadcastProgressPercentage;
+        importer.alertmessage = BroadcastAlertmessage;
+        importer.errormessage = BroadcastErrormessage;
+
+        Debug.Log("Connected to new ObjImporter");
+    }
+    void BroadcastCurrentActivity(string value)
+    {
+       currentActivity.Invoke(value);
+    }
+    void BroadcastCurrentAction(string value)
+    {
+        currentAction.Invoke(value);
+    }
+
+    void BroadcastProgressPercentage(float value)
+    {
+        progressPercentage.Invoke(value);
+    }
+    void BroadcastAlertmessage(string value)
+    {
+       alertmessage.Invoke(value);
+    }
+    void BroadcastErrormessage(string value)
+    {
+        errormessage.Invoke(value);
+    }
+
+
+}
