9273ddeed747adbbe65d046a211e43300c1bc919
Tom Simons <tom.v.simons@gmail.com>
2023-10-06 14:24:36 +0200

    Added script to read CSV with a max line per frame variable. first CSV column should be ID, second should be a hex color (with or without leading #) Currently imported values are only printed to the console

---
+++ Assets/Scripts/CSVReader/CSVToColors.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using Netherlands3D.Twin;
+using UnityEngine;
+
+namespace Netherlands3D.Twin
+{
+    public class IDColor
+    {
+        [Index(0)] public string Id { get; set; }
+        [Index(1)] public string HexColor { get; set; }
+
+        public Color Color
+        {
+            get
+            {
+                var hex = HexColor;
+                if (!hex.StartsWith("#"))
+                    hex = "#" + hex;
+
+                var canParse = ColorUtility.TryParseHtmlString(hex, out var color);
+                return canParse ? color : Color.black; //todo: default color?
+            }
+        }
+    }
+
+    public class CSVToColors : MonoBehaviour
+    {
+        [SerializeField] private string path;
+
+        private void Start()
+        {
+            StartCoroutine(StreamReadCSV(2));
+        }
+
+        private IEnumerator StreamReadCSV(int maxParsesPerFrame)
+        {
+            var dictionaries = ReadCSVColors(path, maxParsesPerFrame).GetEnumerator();
+
+            while (dictionaries.MoveNext())
+            {
+                // print("frame: " + Time.frameCount);
+                var dictionary = dictionaries.Current;
+                foreach (var kvp in dictionary)
+                {
+                    print(kvp.Key + "\t" + kvp.Value);
+                }
+
+                yield return null;
+            }
+        }
+
+        public IEnumerable<Dictionary<string, Color>> ReadCSVColors(string path, int maxParsesPerFrame)
+        {
+            var config = new CsvConfiguration(CultureInfo.CurrentCulture)
+            {
+                HasHeaderRecord = false,
+                Delimiter = ";"
+            };
+
+            using var reader = new StreamReader(path);
+            using (var csv = new CsvReader(reader, config))
+            {
+                var records = csv.GetRecords<IDColor>().GetEnumerator();
+                var dictionary = new Dictionary<string, Color>();
+
+                while (records.MoveNext())
+                {
+                    var record = records.Current;
+                    dictionary[record.Id] = record.Color;
+
+                    if (dictionary.Count >= maxParsesPerFrame)
+                    {
+                        yield return dictionary;
+                        dictionary.Clear();
+                    }
+                }
+
+                //return the remaining elements of the part not divisible by maxParsesPerFrame 
+                if (dictionary.Count > 0)
+                {
+                    print("remaining: " + dictionary.Count);
+                    yield return dictionary;
+                }
+                // return records.ToDictionary(record => record.Id, record => record.Color);
+            }
+        }
+    }
+}
