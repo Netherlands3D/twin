6b971649b90eb42eff7badbb2ed4134995276c91
mvangog <44254917+mvangog@users.noreply.github.com>
2023-10-18 11:44:44 +0200

    Update CoordinateSetup.cs
    removed empty Update(),
    changed Awake() to Start();

--- Packages/eu.netherlands3d.coordinates/Runtime/Scripts/CoordinateSetup.cs
+++ Packages/eu.netherlands3d.coordinates/Runtime/Scripts/CoordinateSetup.cs
@@ -9,16 +9,10 @@ public class CoordinateSetup : MonoBehaviour
         public Vector2RD rdCoordinatesAtUnityCenter;
         public float napElevationAtUniytZero;
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
             EPSG7415.relativeCenter = rdCoordinatesAtUnityCenter;
             EPSG7415.zeroGroundLevelY = napElevationAtUniytZero;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
 }
