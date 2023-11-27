6a93f40e8065aaa8b0842d8b82ddc29c91d872f3
Sam Baas <samuelpetrus@gmail.com>
2023-11-27 16:11:44 +0100

    added selection export filetypes to data object

--- Assets/Scripts/Tools/AreaSelection.cs
+++ Assets/Scripts/Tools/AreaSelection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,10 +10,26 @@ namespace Netherlands3D.Twin
     public class AreaSelection : ScriptableObject
     {
         private List<Vector3> selectedArea = new();
-        public List<Vector3> SelectedArea { get => selectedArea; private set => selectedArea = value; }
+        [HideInInspector] public List<Vector3> SelectedArea { get => selectedArea; private set => selectedArea = value; }
 
+        [Header("Invoke events")]
         public UnityEvent<List<Vector3>> OnSelectionChanged = new();
         public UnityEvent OnSelectionCleared = new();
+
+        private string currentExportFormat = "dae";
+        [SerializeField] private ExportFormat[] exportFormats = new ExportFormat[]{
+            new("Collada", "dae"),
+            new("AutoCAD DXF", "dxf")
+        };
+        public ExportFormat[] ExportFormats { get => exportFormats; set => exportFormats = value; }
+        public class ExportFormat{
+            public string name;
+            public string extension;
+            public ExportFormat(string name, string extension){
+                this.name = name;
+                this.extension = extension;
+            }
+        }
 
         public void SetSelection(List<Vector3> selectedArea)
         {
@@ -21,9 +38,29 @@ namespace Netherlands3D.Twin
             OnSelectionChanged.Invoke(selectedArea);
         }
 
-        public void DownloadAreaAsCollada()
+        public void Download()
         {
-            //Find all gameobjects and export their meshes
+            var exportFormat = Array.Find(exportFormats, format => format.extension == currentExportFormat);
+            if(exportFormat == null){
+                Debug.LogError($"Export format {currentExportFormat} not found in exportFormats");
+                return;
+            }
+            
+            switch(currentExportFormat){
+                case "dae":
+                    //Clip and export to Collada
+
+                    break;
+                case "dxf":
+                    //Log not implemented warning
+                    Debug.LogWarning($"Export format {currentExportFormat} not implemented yet");
+                    break;
+            }
+        }
+
+        public void SetExportFormat(string extension)
+        {
+            currentExportFormat = extension;
         }
 
         public void ClearSelection()
