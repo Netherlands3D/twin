ee2c820da6797db7f40d13a88676d94f8bcf9165
Sam Baas <samuelpetrus@gmail.com>
2023-12-05 09:29:13 +0100

    remove forms not supported by build

--- Assets/Scripts/CSVReader/CSVToColors.cs
+++ Assets/Scripts/CSVReader/CSVToColors.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Windows.Forms;
 using CsvHelper;
 using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
