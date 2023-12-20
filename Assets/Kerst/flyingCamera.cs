5cecdae45ea463ead14c6ad4c4baa67d098f91da
Tom Simons <tom.v.simons@gmail.com>
2023-12-20 11:33:53 +0100

    made movement smooth, cam always moves unless break is pressed with a max break time

--- Assets/Kerst/flyingCamera.cs
+++ Assets/Kerst/flyingCamera.cs
@@ -1,25 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 namespace Netherlands3D.Twin
 {
     public class flyingCamera : MonoBehaviour
     {
-
-        public float rotationspeed=1;
-        public float movespeed=1000;
+        public float rotationspeed = 1;
+        public float movespeed = 0;
+        public float maxSpeed = 4000;
+        public float acceleration = 0;
+        public float maxBreakTimer = 5f;
+        float breakTimer;
+        public float jerkTime = 0.3f;
 
         public ContextMenuButton leftbutton;
 
         public bool shouldMove = false;
+        public bool shouldBreak = false;
         public bool turnLeft = false;
         public bool turnRight = false;
 
-        // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
-        
+            breakTimer = maxBreakTimer;
         }
 
         // Update is called once per frame
@@ -28,27 +34,35 @@ namespace Netherlands3D.Twin
             keyCapture();
             if (turnLeft)
             {
-                transform.RotateAround(Vector3.down, Time.deltaTime  * rotationspeed);
-                
+                transform.RotateAround(Vector3.down, Time.deltaTime * rotationspeed);
             }
+
             if (turnRight)
             {
-                transform.RotateAround(Vector3.up, Time.deltaTime  * rotationspeed);
-                
+                transform.RotateAround(Vector3.up, Time.deltaTime * rotationspeed);
             }
+
+            if (shouldBreak && breakTimer > 0)
+                shouldMove = false;
+            else
+                shouldMove = true;
+
             if (shouldMove)
-            {
-                Vector3 forwardFlat = transform.forward;
-                forwardFlat.y = 0;
-                transform.position += forwardFlat * movespeed * Time.deltaTime;
-            }
+                movespeed = Mathf.SmoothDamp(movespeed, maxSpeed, ref acceleration, jerkTime);
+            else
+                movespeed = Mathf.SmoothDamp(movespeed, 0, ref acceleration, jerkTime);
+
+            Vector3 forwardFlat = transform.forward.normalized;
+            forwardFlat.y = 0;
+
+            transform.position += forwardFlat * movespeed * Time.deltaTime;
         }
 
         void keyCapture()
         {
             //turn left
-            if(Input.GetKeyDown(KeyCode.LeftArrow)) turnLeft = true;
-            
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) turnLeft = true;
+
             if (Input.GetKeyUp(KeyCode.LeftArrow)) turnLeft = false;
 
             if (Input.GetKeyDown(KeyCode.Q)) turnLeft = true;
@@ -61,11 +75,28 @@ namespace Netherlands3D.Twin
             if (Input.GetKeyUp(KeyCode.E)) turnRight = false;
 
             //move
-            if (Input.GetKeyDown(KeyCode.UpArrow)) shouldMove = true;
-            if (Input.GetKeyUp(KeyCode.UpArrow)) shouldMove = false;
+            // if (Input.GetKeyDown(KeyCode.UpArrow)) shouldMove = true;
+            // if (Input.GetKeyUp(KeyCode.UpArrow)) shouldMove = false;
             if (Input.GetKeyDown(KeyCode.W)) shouldMove = true;
             if (Input.GetKeyUp(KeyCode.W)) shouldMove = false;
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                shouldBreak = true;
+                // shouldMove = false;
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                breakTimer -= Time.deltaTime;
+            }
+            else
+            {
+                breakTimer += Time.deltaTime;
+            }
+
+            breakTimer = Mathf.Clamp(breakTimer, 0, maxBreakTimer);
+
+            if (Input.GetKeyUp(KeyCode.DownArrow)) shouldBreak = false;
         }
-        
     }
 }
