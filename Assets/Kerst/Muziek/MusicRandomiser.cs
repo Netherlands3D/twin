e2730d2587a7b6acd814c43a262b70344878a48a
Tom Simons <tom.v.simons@gmail.com>
2023-12-20 16:22:22 +0100

    added music

---
+++ Assets/Kerst/Muziek/MusicRandomiser.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Netherlands3D.Twin
+{
+    public class MusicRandomiser : MonoBehaviour
+    {
+        private AudioSource source;
+        [SerializeField] private AudioClip[] clips;
+        private List<int> indices;
+        private int currentPlayIndex = 0;
+
+        private void Awake()
+        {
+            source = GetComponent<AudioSource>();
+            ShuffleAudioClips(clips);
+        }
+
+        private void Update()
+        {
+            if (source.isPlaying && !Input.GetKeyDown(KeyCode.A))
+                return;
+
+            currentPlayIndex++;
+            currentPlayIndex %= clips.Length;
+            source.clip = clips[currentPlayIndex];
+            source.Play();
+        }
+
+        void ShuffleAudioClips(AudioClip[] audioClips)
+        {
+            int n = audioClips.Length;
+            System.Random random = new System.Random();
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                int randIndex = random.Next(0, i + 1);
+
+                // Swap audio clips[i] and audio clips[randIndex]
+                (audioClips[i], audioClips[randIndex]) = (audioClips[randIndex], audioClips[i]);
+            }
+        }
+    }
+}
