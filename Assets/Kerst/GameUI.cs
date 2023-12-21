9aad2042e49274f7d8a22f613624bde7b8c143b3
Mike van Riel <mike@ingewikkeld.net>
2023-12-21 10:40:08 +0100

    Change mex screen space error

--- Assets/Kerst/GameUI.cs
+++ Assets/Kerst/GameUI.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Netherlands3D.Twin
 {
@@ -13,19 +14,22 @@ namespace Netherlands3D.Twin
 
         public GameObject presents;
         public GameObject presentPrefab;
+        public Sprite solidPresent;
 
-        private void Start()
-        {
-            gameManager.onGameStarted.AddListener(OnGameStarted);
-        }
-
-        private void OnGameStarted(int index)
+        public void OnGameStarted(int index)
         {
             presents.transform.ClearAllChildren();
+            // Remember: target count is actually the number of targets -1 because the starting position is a target
             for (int i = 0; i < distanceCalculator.targetcount; i++)
             {
                 GameObject.Instantiate(presentPrefab, presents.transform);
             }
+        }
+
+        public void OnTargetReached(int index)
+        {
+            // -1 is because the first target is the starting position and is never reached
+            presents.transform.GetChild(index - 1).GetComponent<Image>().sprite = solidPresent;
         }
 
         private void Update()
