7630408f493ab12ca5e52701a315b625328f55aa
mvangog <martijn.van.gog@amsterdam.nl>
2024-02-16 08:18:58 +0100

    fix child boundingbox calucluations

--- Packages/eu.netherlands3d.tiles3d/Runtime/Scripts/Tileset/BoundingVolume.cs
+++ Packages/eu.netherlands3d.tiles3d/Runtime/Scripts/Tileset/BoundingVolume.cs
@@ -61,54 +61,57 @@ namespace Netherlands3D.Tiles3D{
                     newBoundingVolume.values[7] = values[7] / 2d;
                     newBoundingVolume.values[8] = values[8] / 2d;
                     //Z-axis size and direction
-                    double heightDivision = 1;
+                    double heightDivision = 1d;
                     if (subdivisionScheme==SubdivisionScheme.Octree)
                     {
-                        heightDivision = 2;
+                        heightDivision = 2d;
                     }
-                    newBoundingVolume.values[9] = values[9]/heightDivision;
+                    newBoundingVolume.values[9] = values[9] / heightDivision;
                     newBoundingVolume.values[10] = values[10] / heightDivision;
                     newBoundingVolume.values[11] = values[11] / heightDivision;
 
-                    double bottomCenterX = values[0] + values[9];
-                    double bottomCenterY = values[1] + values[10];
-                    double bottomCenterZ = values[2]-values[11];
-
-                    double cornerBottomX = values[0]; //start at center
-                    double cornerBottomY = values[1];
-                    double cornerBottomZ = values[2];
                     
-                    //X-axis
+
+                    double centerX = values[0]; //start at center
+                    double centerY = values[1];
+                    double centerZ = values[2];
+                    
+                    //X-offset
                     double leftRightMultiplier = 1;
                     if (childIndex%2==0) //left from center
                     {
                         leftRightMultiplier = -1;
                     }
-                    cornerBottomX += leftRightMultiplier * newBoundingVolume.values[3];
-                    cornerBottomY += leftRightMultiplier * newBoundingVolume.values[4];
-                    cornerBottomZ += leftRightMultiplier * newBoundingVolume.values[5];
+                    centerX += (leftRightMultiplier * newBoundingVolume.values[3]);
+                    centerY += (leftRightMultiplier * newBoundingVolume.values[4]);
+                    centerZ += (leftRightMultiplier * newBoundingVolume.values[5]);
+                    //y-offset
                     double frontBackMultiplier = 1;
                     if (childIndex%4<2)
                     {
                         frontBackMultiplier = -1;
                     }
-                    cornerBottomX += frontBackMultiplier * newBoundingVolume.values[6];
-                    cornerBottomY += frontBackMultiplier * newBoundingVolume.values[7];
-                    cornerBottomZ += frontBackMultiplier * newBoundingVolume.values[8];
+                    centerX += (frontBackMultiplier * newBoundingVolume.values[6]);
+                    centerY += (frontBackMultiplier * newBoundingVolume.values[7]);
+                    centerZ += (frontBackMultiplier * newBoundingVolume.values[8]);
 
-                    double topBottomMultiplier = 1;
-                    if (childIndex>3)
+                    if (subdivisionScheme==SubdivisionScheme.Octree)
                     {
-                        topBottomMultiplier = 3;
+                        double topBottomMultiplier = -1;
+                        if (childIndex > 3)
+                        {
+                            topBottomMultiplier = 1;
+                        }
+                    
+                   
+                    centerX += topBottomMultiplier * newBoundingVolume.values[9];
+                    centerY += topBottomMultiplier * newBoundingVolume.values[10];
+                    centerZ += topBottomMultiplier * newBoundingVolume.values[11];
                     }
-                    cornerBottomX += topBottomMultiplier * newBoundingVolume.values[9];
-                    cornerBottomY += topBottomMultiplier * newBoundingVolume.values[10];
-                    cornerBottomZ += topBottomMultiplier * newBoundingVolume.values[11];
-
                     //calculate center
-                    newBoundingVolume.values[0] = (bottomCenterX + cornerBottomX) / 2d;
-                    newBoundingVolume.values[1] = (bottomCenterY + cornerBottomY) / 2d;
-                    newBoundingVolume.values[2] = (bottomCenterZ + cornerBottomZ) / 2d;
+                    newBoundingVolume.values[0] = centerX;
+                    newBoundingVolume.values[1] = centerY;
+                    newBoundingVolume.values[2] = centerZ;
 
                     
                     break;
