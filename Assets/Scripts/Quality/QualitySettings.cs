21faf018e95ab92d1b4dd8065bae90c1909be578
Sam Baas <samuelpetrus@gmail.com>
2024-04-22 14:37:28 +0200

    enabled disabled states for fancy post effects

--- Assets/Scripts/Quality/QualitySettings.cs
+++ Assets/Scripts/Quality/QualitySettings.cs
@@ -8,16 +8,66 @@ namespace Netherlands3D.Twin
     {
         [Header("Water Reflections")]
         [SerializeField] private GameObject waterReflectionsRenderer;
+        private UniversalRenderPipelineAsset activeRenderPipelineAsset;
+        [SerializeField] private ScriptableRendererFeature aoRenderFeature;
 
         void Awake()
         {
-            var urp = (UniversalRenderPipelineAsset)GraphicsSettings.currentRenderPipeline;
-           
+            activeRenderPipelineAsset = (UniversalRenderPipelineAsset)GraphicsSettings.currentRenderPipeline;
         }
 
-        public void SetPlanarWaterReflections(bool enable)
+        /// <summary>
+        /// Set quality level for the Twin application
+        /// </summary>
+        /// <param name="qualityLevel">0 to 2 (Low, Medium, High respectively)</param>
+        public void SetQualityLevel(int qualityLevel)
         {
-            waterReflectionsRenderer.SetActive(enable);
+            switch (qualityLevel)
+            {
+                case 0:
+                    ToggleAA(false);
+                    ToggleAO(false);
+                    ToggleWaterReflections(false);
+                    break;
+                case 1:
+                    ToggleAA(true);
+                    ToggleAO(false);
+                    ToggleWaterReflections(false);
+                    break;
+                case 2:
+                    ToggleAA(true);
+                    ToggleAO(true);
+                    ToggleWaterReflections(true);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Toggle Screen Space Ambient Occlusion render feature
+        /// </summary>
+        /// <param name="enabled">Render feature active state</param>
+        public void ToggleAO(bool enabled)
+        {
+            aoRenderFeature.SetActive(enabled);
+        }
+
+        /// <summary>
+        /// Toggle Fast Approximate Antialiasing on main camera
+        /// </summary>
+        /// <param name="enabled">Enabled FXAA</param>
+        public void ToggleAA(bool enabled)
+        {
+            UniversalAdditionalCameraData universalCameraData = Camera.main.GetComponent<UniversalAdditionalCameraData>();
+            universalCameraData.antialiasing = enabled ? AntialiasingMode.FastApproximateAntialiasing : AntialiasingMode.None;
+        }
+
+        /// <summary>
+        /// Enable or disable realtime planar water reflections
+        /// </summary>
+        /// <param name="enabled">Enabled water reflections</param>
+        public void ToggleWaterReflections(bool enabled)
+        {
+            waterReflectionsRenderer.SetActive(enabled);
         }
     }
 }
