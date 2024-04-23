afb6a463b99550fc11d02bd94af654319fc0fa79
Sam Baas <samuelpetrus@gmail.com>
2024-04-23 09:12:28 +0200

    Update PlaneLensFlare.cs

--- Assets/Scripts/Quality/PlaneLensFlare.cs
+++ Assets/Scripts/Quality/PlaneLensFlare.cs
@@ -4,21 +4,21 @@ namespace Netherlands3D.Twin.Effects
 {
     public class PlaneLensFlare : MonoBehaviour
     {
-        [SerializeField] private Transform sun;
+        [SerializeField][Tooltip("If not set, first Light will be used as Sun")] private Transform directionalLightSunTransform;
         [SerializeField] private Transform sunHalo;
         [SerializeField] private float haloScale = 1.0f;
         [SerializeField] private float offset = 0.01f;
 
         private void Awake() {
-            if(!sun)
-                sun = FindObjectOfType<Light>().transform;
+            if(!directionalLightSunTransform)
+                directionalLightSunTransform = FindObjectOfType<Light>().transform;
         }
 
         void Update()
         {
             var mainCamera = Camera.main;
 
-            this.transform.rotation = sun.rotation;
+            this.transform.rotation = directionalLightSunTransform.rotation;
             this.transform.position = mainCamera.transform.position;
 
             sunHalo.transform.localPosition = -Vector3.forward * (mainCamera.farClipPlane + (mainCamera.farClipPlane*offset));
