d484751ee3c95e34dda3a59abd8c10f1edc3ada4
Sam Baas <samuelpetrus@gmail.com>
2024-04-29 08:55:46 +0200

    add testscript that appends uploaded models

--- Assets/TestZip.cs
+++ Assets/TestZip.cs
@@ -1,21 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+using SharpZipLib.Unity.Helpers;
 using UnityEngine;
 
 namespace Netherlands3D.Twin
 {
     public class TestZip : MonoBehaviour
     {
-        // Start is called before the first frame update
+        ZipOutputStream zipOutputStream;
         void Start()
         {
-        
+            //Open our zip stream, and keep it open
+            zipOutputStream = new ZipOutputStream(File.Create(Application.persistentDataPath + "/test.zip"));
+            zipOutputStream.SetLevel(9); // 0 - store only to 9 - means best compression
         }
 
-        // Update is called once per frame
-        void Update()
+        // Append a file to zip
+        public void AddFileToZip(string filePath)
         {
-        
+            byte[] buffer = new byte[4096];
+            ZipEntry entry = new ZipEntry(filePath);
+            zipOutputStream.PutNextEntry(entry);
+
+            using (FileStream fs = File.OpenRead(filePath)) {
+                StreamUtils.Copy(fs, zipOutputStream, buffer);
+            }         
         }
     }
 }
