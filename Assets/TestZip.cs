9cc6c3fe1de39e15117361a64b712362c0cc2afd
Sam Baas <samuelpetrus@gmail.com>
2024-04-29 10:12:50 +0200

    download from indexeddb after closing zip

--- Assets/TestZip.cs
+++ Assets/TestZip.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using ICSharpCode.SharpZipLib.Core;
 using ICSharpCode.SharpZipLib.Zip;
 using SharpZipLib.Unity.Helpers;
@@ -11,24 +12,61 @@ namespace Netherlands3D.Twin
 {
     public class TestZip : MonoBehaviour
     {
+        [DllImport("__Internal")]
+        private static extern void DownloadFileFromIndexedDB(string filename, string callbackObjectName, string callbackMethodName);
+        [DllImport("__Internal")]
+        private static extern void SyncFilesToIndexedDB(string callbackObjectName, string callbackMethodName);
+
         ZipOutputStream zipOutputStream;
+
+
+        private string randomTime = "";
+
         void Start()
         {
+            randomTime = DateTime.Now.ToString("yyyyMMddHHmmss");
+
             //Open our zip stream, and keep it open
-            zipOutputStream = new ZipOutputStream(File.Create(Application.persistentDataPath + "/test.zip"));
+            zipOutputStream = new ZipOutputStream(File.Create(Application.persistentDataPath + $"/test_{randomTime}.zip"));
             zipOutputStream.SetLevel(9); // 0 - store only to 9 - means best compression
         }
 
-        // Append a file to zip
-        public void AddFileToZip(string filePath)
+        public void DoneDownloadZip()
         {
+            Debug.Log("Done downloading zip");
+        }
+
+        // Append a file to zip
+        public void AddFileToZip(string fileNames)
+        {
+            var fileName = fileNames.Split(",")[0];
+            var randomFileTag = DateTime.Now.ToString("yyyyMMddHHmmss");
+            var persistentDataPath = Application.persistentDataPath + "/" + randomFileTag + "_" + fileName;
+            Debug.Log("Adding file to zip: " + persistentDataPath);
             byte[] buffer = new byte[4096];
-            ZipEntry entry = new ZipEntry(filePath);
+            ZipEntry entry = new ZipEntry(fileName);
             zipOutputStream.PutNextEntry(entry);
 
-            using (FileStream fs = File.OpenRead(filePath)) {
+            using (FileStream fs = File.OpenRead(persistentDataPath)) {
                 StreamUtils.Copy(fs, zipOutputStream, buffer);
             }         
+        }
+
+        public void CloseZip()
+        {
+            zipOutputStream.Finish();
+            zipOutputStream.Close();
+
+            //Make sure indexedDB is synced
+            #if !UNITY_EDITOR && UNITY_WEBGL
+            SyncFilesToIndexedDB(gameObject.name, "ZipReadyInIndexedDB");
+            #endif
+        }
+
+        public void ZipReadyInIndexedDB()
+        {
+            Debug.Log("Zip is ready in indexedDB");
+            DownloadFileFromIndexedDB(Application.persistentDataPath + "/test.zip", this.gameObject.name, "DoneDownloadZip");
         }
     }
 }
