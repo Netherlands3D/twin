2c27c0abbad66f7f8e574bd46ce97963e0146841
Mike van Riel <mike@ingewikkeld.net>
2024-05-22 16:47:12 +0200

    Experimental changes to support serializing and zipping projects

---
+++ Assets/Scripts/Projects/Project.cs
@@ -0,0 +1,11 @@
+using Netherlands3D.Coordinates;
+
+namespace Netherlands3D.Twin.Projects
+{
+    public class Project
+    {
+        public int Version = 1;
+
+        public Coordinate Origin;
+    }
+}
