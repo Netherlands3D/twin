5ddbbfc11cf220d3b59d60345ca8a126935cfd57
Tom Simons <tom.v.simons@gmail.com>
2024-06-07 15:13:29 +0200

    Made UI layout flexible to accommodate the error message

--- Assets/Scripts/Tools/DownloadFile.cs
+++ Assets/Scripts/Tools/DownloadFile.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 namespace Netherlands3D.Twin
 {
@@ -18,17 +20,18 @@ namespace Netherlands3D.Twin
         {
             URL = url;
         }
-        
+
         public void DownloadFileFromURL()
         {
             StartCoroutine(Download(URL));
         }
-        
-        private IEnumerator Download(string url) {
+
+        private IEnumerator Download(string url)
+        {
             // var uwr = new UnityWebRequest("https://unity3d.com/", UnityWebRequest.kHttpVerbGET);
             var uwr = UnityWebRequest.Get(url);
             string path = Path.Combine(Application.persistentDataPath, "test.json");
-            
+
             Debug.Log("downloading from: " + url);
             Debug.Log("downloading to: " + path);
             print("start frame: " + Time.frameCount);
