4dac5011ace398e19f01fda6f11d557db8006a9d
Mike van Riel <mike@ingewikkeld.net>
2024-07-09 09:32:05 +0200

    Add a special loader that will always start the default scene when entering play mode in the editor

---
+++ Assets/Scripts/Editor/DefaultSceneLoader.cs
@@ -0,0 +1,32 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Netherlands3D.Twin.Editor
+{
+    ///
+    /// Shamelessly stolen from https://stackoverflow.com/a/48817315
+    /// 
+    [InitializeOnLoad]
+    public static class DefaultSceneLoader
+    {
+        static DefaultSceneLoader()
+        {
+            EditorApplication.playModeStateChanged += LoadDefaultScene;
+        }
+
+        static void LoadDefaultScene(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingEditMode)
+            {
+                EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+            }
+
+            if (state == PlayModeStateChange.EnteredPlayMode)
+            {
+                EditorSceneManager.LoadScene(0);
+            }
+        }
+    }
+}
+#endif
