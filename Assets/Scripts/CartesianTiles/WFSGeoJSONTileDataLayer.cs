b2e035b15812bb00fa489c7b2b78fdf5f2cdbd58
Sam Baas <samuelpetrus@gmail.com>
2024-07-16 13:07:55 +0200

    make sure tilehandler layer has at least 1 baseDataset (it required 1 to run)

--- Assets/Scripts/CartesianTiles/WFSGeoJSONTileDataLayer.cs
+++ Assets/Scripts/CartesianTiles/WFSGeoJSONTileDataLayer.cs
@@ -15,7 +15,6 @@ namespace Netherlands3D.CartesianTiles
     /// a WFS service to the client using the bbox parameter.
     /// The Twin GeoJSONLayer is used to render the GeoJSON data.
     /// </summary>
-	[AddComponentMenu("Netherlands3D/CartesianTiles/GeoJSONTileDataLayer")]
 	public class WFSGeoJSONTileDataLayer : Layer
 	{		
         private string wfsUrl = "";
@@ -37,7 +36,25 @@ namespace Netherlands3D.CartesianTiles
         private TileHandler tileHandler;
 
         private void Awake() {
-            //Make sure we live in a tilehandler
+			//Make sure Datasets at least has one item
+			if(Datasets.Count == 0)
+			{
+				var baseDataset = new DataSet()
+				{
+					maximumDistance = 3000,
+					maximumDistanceSquared = 1000 * 1000
+				};
+				Datasets.Add(baseDataset);
+			}
+
+			StartCoroutine(FindTileHandler());
+        }
+
+		private IEnumerator FindTileHandler()
+		{
+			yield return null;
+
+			//Make sure we live in a tilehandler
             tileHandler = GetComponentInParent<TileHandler>();
             
             if(!tileHandler)
@@ -45,11 +62,11 @@ namespace Netherlands3D.CartesianTiles
 
             if(tileHandler){
                 tileHandler.AddLayer(this);
-                return;
+                yield break;
             }
 
             Debug.LogError("No TileHandler found.", gameObject);
-        }
+		}
 
         private void OnGeoJSONLayerDestroyed()
         {
@@ -100,8 +117,8 @@ namespace Netherlands3D.CartesianTiles
 
 		private IEnumerator DownloadGeoJSON(TileChange tileChange, Tile tile, System.Action<TileChange> callback = null)
 		{
-			var bboxQuery = $"bbox={tileChange.X},{tileChange.Y},{(tileChange.X + tileSize)},{(tileChange.Y + tileSize)}";
-			string url = WfsUrl.Replace("{bbox}", bboxQuery);
+			var bboxValue = $"{tileChange.X},{tileChange.Y},{(tileChange.X + tileSize)},{(tileChange.Y + tileSize)}";
+			string url = WfsUrl.Replace("{bbox}", bboxValue);
 			Debug.Log("Downloading GeoJSON from: " + url);
 
 			var geoJsonRequest = UnityWebRequest.Get(url);
@@ -110,7 +127,10 @@ namespace Netherlands3D.CartesianTiles
 
 			if (geoJsonRequest.result == UnityWebRequest.Result.Success)
 			{
-                var localCacheFileName = Path.Combine(Application.persistentDataPath, $"{gameObject.name}{tileChange.X}_{tileChange.Y}.json");
+				var fileName = $"{gameObject.name}{tileChange.X}_{tileChange.Y}.json";
+				fileName = fileName.Replace(":", "_").Replace("/", "_").Replace("\\", "_");
+
+                var localCacheFileName = Path.Combine(Application.persistentDataPath, fileName);
 				File.WriteAllText(localCacheFileName, geoJsonRequest.downloadHandler.text);
 
 				geoJSONLayer.AdditiveParseGeoJSON(localCacheFileName);
