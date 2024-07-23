adf5d6e3055771adce12516eae079f493d13045e
Tom Simons <tom.v.simons@gmail.com>
2024-07-23 16:05:39 +0200

    Fixed camera loading

--- Assets/Scripts/Projects/ProjectDataCameraUpdater.cs
+++ Assets/Scripts/Projects/ProjectDataCameraUpdater.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using Netherlands3D.Coordinates;
 using Netherlands3D.Twin.Projects;
 using UnityEngine;
@@ -8,26 +6,7 @@ namespace Netherlands3D.Twin
 {
     public class ProjectDataCameraUpdater : MonoBehaviour
     {
-        // [SerializeField] private ProjectData project;
-
         private Matrix4x4 lastSavedCameraTransformMatrix = Matrix4x4.identity;
-
-        private void Update() {
-            var currentCameraMatrix = transform.localToWorldMatrix;        
-            if(Matrix4x4.Equals(currentCameraMatrix, lastSavedCameraTransformMatrix)) return;
-
-            lastSavedCameraTransformMatrix = currentCameraMatrix;
-            SaveCurrentCameraTransform();
-        }
-
-        private void SaveCurrentCameraTransform()
-        {
-            var cameraCoordinate = CoordinateConverter.ConvertTo(new Coordinate(CoordinateSystem.Unity,transform.position[0], transform.position[1],transform.position[2]), CoordinateSystem.RD);
-            var cameraRotation = transform.eulerAngles;
-
-            ProjectData.Current.CameraPosition = new double[] { cameraCoordinate.Points[0], cameraCoordinate.Points[1], cameraCoordinate.Points[2] };
-            ProjectData.Current.CameraRotation = new double[] { cameraRotation.x, cameraRotation.y, cameraRotation.z };
-        }
 
         private void OnEnable()
         {
@@ -39,11 +18,15 @@ namespace Netherlands3D.Twin
             ProjectData.Current.OnDataChanged.RemoveListener(OnProjectDataChanged);
         }
 
+        private void Start()
+        {
+            OnProjectDataChanged(ProjectData.Current); //set initial position based on the current project
+        }
+
         private void OnProjectDataChanged(ProjectData project)
         {
-            var cameraCoordinate = new Coordinate(CoordinateSystem.RD, project.CameraPosition[0], project.CameraPosition[1], project.CameraPosition[2]);
-            var cameraUnityCoordinate = CoordinateConverter.ConvertTo(cameraCoordinate, CoordinateSystem.Unity).ToVector3();
-
+            var cameraCoordinate = new Coordinate(CoordinateSystem.RDNAP, project.CameraPosition[0], project.CameraPosition[1], project.CameraPosition[2]);
+            var cameraUnityCoordinate = cameraCoordinate.ToUnity();
             transform.position = cameraUnityCoordinate;
 
             if(project.CameraRotation.Length == 3)
@@ -51,6 +34,24 @@ namespace Netherlands3D.Twin
                 var cameraRotation = new Vector3((float)project.CameraRotation[0], (float)project.CameraRotation[1], (float)project.CameraRotation[2]);
                 transform.rotation = Quaternion.Euler(cameraRotation);
             }
+        }
+
+        private void Update() {
+            var currentCameraMatrix = transform.localToWorldMatrix;        
+            if(Matrix4x4.Equals(currentCameraMatrix, lastSavedCameraTransformMatrix)) return;
+
+            lastSavedCameraTransformMatrix = currentCameraMatrix;
+            SaveCurrentCameraTransform();
+        }
+
+        private void SaveCurrentCameraTransform()
+        {
+            var cameraCoordinate = new Coordinate(CoordinateSystem.Unity, transform.position.x, transform.position.y, transform.position.z);
+            var cameraCoordinateRD = cameraCoordinate.ToVector3RD();
+            var cameraRotation = transform.eulerAngles;
+
+            ProjectData.Current.CameraPosition = new double[] { cameraCoordinateRD.x, cameraCoordinateRD.y, cameraCoordinateRD.z };
+            ProjectData.Current.CameraRotation = new double[] { cameraRotation.x, cameraRotation.y, cameraRotation.z };
         }
     }
 }
