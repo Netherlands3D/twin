63d1ea1bb54e3546a335a11dda085bea5061c8ff
Tom Simons <tom.v.simons@gmail.com>
2024-07-25 14:00:16 +0200

    Added auto populate UI to PrefabLibrary

---
+++ Assets/Scripts/Layers/PopulateAddLayerPanel.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Netherlands3D.Twin.Layers;
+using Netherlands3D.Twin.Projects;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Netherlands3D.Twin
+{
+    [RequireComponent(typeof(AddLayerPanel))]
+    public class PopulateAddLayerPanel : MonoBehaviour
+    {
+        private AddLayerPanel addLayerPanel;
+
+        [Header("Panel references")]
+        [SerializeField] private Button addLayerMenuButton;
+        [SerializeField] private Transform titleRow;
+        [SerializeField] private Transform mainButtonPanel;
+        [SerializeField] private Transform contentParent;
+        
+        [Header("Prefab references")]
+        [SerializeField] private GameObject titlePrefab;
+        [SerializeField] private Button panelButtonPrefab;
+        [SerializeField] private GameObject groupPanelPrefab;
+        [SerializeField] private ObjectLibraryButton buttonPrefab;
+        private void Awake()
+        {
+            addLayerPanel = GetComponent<AddLayerPanel>();
+        }
+
+        private void Start()
+        {
+            Populate(ProjectData.Current.PrefabLibrary);
+        }
+
+        private void Populate(PrefabLibrary library)
+        {
+            foreach (var group in library.prefabGroups)
+            {
+                if (!group.autoPopulateUI)
+                    continue;
+                
+                var groupPanel = CreateGroupPanel(group.groupName);
+                foreach (var prefab in group.prefabs)
+                {
+                    CreateButton(prefab, groupPanel);
+                }
+            }
+        }
+
+        private GameObject CreateGroupPanel(string groupGroupName)
+        {
+            var title = Instantiate(titlePrefab, titleRow);
+            title.GetComponentInChildren<TMP_Text>().text = groupGroupName;
+            title.SetActive(false);
+            
+            var button = Instantiate(panelButtonPrefab, mainButtonPanel);
+            var groupPanel = Instantiate(groupPanelPrefab, contentParent);
+            groupPanel.name = groupGroupName;
+        
+            mainButtonPanel.GetComponent<EqualSpacingCalculator>().AddLayoutGroup(groupPanel.GetComponent<LayoutGroup>());
+            
+            //todo: add functionality listener if needed
+            
+            button.onClick.AddListener(()=>mainButtonPanel.gameObject.SetActive(false));
+            button.onClick.AddListener(()=>groupPanel.SetActive(true));
+            button.onClick.AddListener(addLayerPanel.GetComponent<AdvancedScrollView>().ResetScrollActive);
+            
+            addLayerMenuButton.onClick.AddListener(()=> groupPanel.SetActive(false));
+            addLayerMenuButton.onClick.AddListener(()=> title.SetActive(false));
+            
+            return groupPanel;
+        }
+
+        private ObjectLibraryButton CreateButton(LayerGameObject prefab, GameObject groupPanel)
+        {
+            var button = Instantiate(buttonPrefab, groupPanel.transform);
+            button.Initialize(prefab.gameObject);
+            button.GetComponentInChildren<TMP_Text>().text = prefab.name;
+            return button;
+        }
+    }
+}
