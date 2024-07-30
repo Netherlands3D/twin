41a1932a6020919fe4c8f6e71443b08e3cbe3742
Mike van Riel <mike@ingewikkeld.net>
2024-07-30 07:44:28 +0200

    Re-added removed using directive because it is needed from within a pragma block

--- Assets/Scripts/Tools/AreaSelection.cs
+++ Assets/Scripts/Tools/AreaSelection.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
+// ReSharper disable once RedundantUsingDirective
+using System.Text;
 using Netherlands3D.Collada;
 using Netherlands3D.MeshClipping;
 using UnityEditor;
