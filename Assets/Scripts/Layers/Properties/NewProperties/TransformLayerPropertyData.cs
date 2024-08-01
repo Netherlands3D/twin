f865d529092300f53be274debc890fe8b4e55f60
Tom Simons <tom.v.simons@gmail.com>
2024-08-01 17:05:55 +0200

    Added explicit constructor for TransformPropertyData to fix webgl deserialization bug

--- Assets/Scripts/Layers/Properties/NewProperties/TransformLayerPropertyData.cs
+++ Assets/Scripts/Layers/Properties/NewProperties/TransformLayerPropertyData.cs
@@ -11,7 +11,7 @@ namespace Netherlands3D.Twin.Layers.Properties
     [Serializable]
     public class TransformLayerPropertyData : LayerPropertyData
     {
-        [SerializeField, JsonProperty] private Coordinate position = new Coordinate(CoordinateSystem.RDNAP);
+        [SerializeField, JsonProperty] private Coordinate position;
         [SerializeField, JsonProperty] private Vector3 eulerRotation;
         [SerializeField, JsonProperty] private Vector3 localScale;
 
@@ -51,5 +51,14 @@ namespace Netherlands3D.Twin.Layers.Properties
         [JsonIgnore] public readonly UnityEvent<Coordinate> OnPositionChanged = new();
         [JsonIgnore] public readonly UnityEvent<Vector3> OnRotationChanged = new();
         [JsonIgnore] public readonly UnityEvent<Vector3> OnScaleChanged = new();
+        
+        [JsonConstructor]
+        public TransformLayerPropertyData(Coordinate position, Vector3 eulerRotation, Vector3 localScale)
+        {
+            this.position = position.Convert(CoordinateSystem.RDNAP);
+            this.eulerRotation = eulerRotation;
+            this.localScale = localScale;
+        }
+
     }
 }
