af6b6ccfefa795729d58524abbec8de3e0d9617b
mvangog <martijn.van.gog@amsterdam.nl>
2024-08-06 10:52:44 +0200

    Update Origin.cs
    extracted function for calculation horizontal squared distance
    added comments to explain the use of wgs84
    improved variable naming

--- Assets/Scripts/FloatingOrigin/Origin.cs
+++ Assets/Scripts/FloatingOrigin/Origin.cs
@@ -47,26 +47,41 @@ namespace Netherlands3D.Twin.FloatingOrigin
             StartCoroutine(AttemptShift());
         }
 
+        private int CalculateDistanceSquaredInXZPlane(Vector3 pos1, Vector3 pos2)
+        {
+            float deltaX = pos1.x - pos2.x;
+            float deltaZ = pos1.z - pos2.z;
+            float distanceSquared = (deltaX * deltaX) + (deltaZ * deltaZ);
+            return (int)distanceSquared;
+        }
+
         private IEnumerator AttemptShift() 
         {
             while (true)
             {
                 yield return new WaitForEndOfFrame();
 
-                if (((mainShifter.position.x-transform.position.x)* (mainShifter.position.x - transform.position.x))+ ((mainShifter.position.z - transform.position.z) * (mainShifter.position.z - transform.position.z))< sqrDistanceBeforeShifting)
+                int squaredDistance = CalculateDistanceSquaredInXZPlane(mainShifter.position, transform.position);
+                if (squaredDistance < sqrDistanceBeforeShifting)
                 {
                     continue;
                 }
 
-
+                // get the Coordinate of the mainShifter in WGS84_LatLonHeight
                 var mainShifterPosition = mainShifter.position;
                 Coordinate mainShifterCoordinate = new Coordinate(mainShifterPosition);
                 Coordinate mainShifterWGS = mainShifterCoordinate.Convert(CoordinateSystem.WGS84_LatLonHeight);
 
+                // get the Coordinate of the Origin in WGS84_LatLonHeight
                 Coordinate originCoordinateWGS = new Coordinate(transform.position).Convert(CoordinateSystem.WGS84_LatLonHeight);
+                double originEllipsoidalHeight = originCoordinateWGS.height;
 
+                // move the origin to the coordinate of the mainShifter, maintaining its current ellipsoidalHeight.
+                // because the wgs84-ellipse roughly follows the surface of the earth, (in contrast to Earth-Center-Earth-Fixed, coordinateSystems,
+                // where the height is the distance from the equatorial plane in thedirection of the north pole),
+                // keeping the ellipsoidal height constant ensures that the vertical positions in unity don't become huge.
                 Coordinate newOriginCoordinate = mainShifterWGS;
-                newOriginCoordinate.height = originCoordinateWGS.height;
+                newOriginCoordinate.height = originEllipsoidalHeight;
 
                 Profiler.BeginSample("PerformOriginShift");
 
@@ -85,23 +100,23 @@ namespace Netherlands3D.Twin.FloatingOrigin
         /// (including the main shifter) so that there is a clear and distinct separation of responsibilities when
         /// it comes to moving the world's origin, and moving spatial (game) objects.
         /// </summary>
-        /// <param name="destination"></param>
-        public void MoveOriginTo(Coordinate destination)
+        /// <param name="originDestination"></param>
+        public void MoveOriginTo(Coordinate originDestination)
         {
-            Coordinate CurrentOrigin = new Coordinate(transform.position);
+            Coordinate currentOrigin = new Coordinate(transform.position);
 
 #if UNITY_EDITOR
-            Coordinate mainShifterRD = new Coordinate(transform.position).Convert(CoordinateSystem.RDNAP);
-            Coordinate destinationRD = destination.Convert(CoordinateSystem.RDNAP);
-            if (LogShifts) Debug.Log($"Moving origin from {mainShifterRD.ToVector3()} (EPSG:7415) to {destinationRD.ToVector3()} (EPSG:7415)");
+            Coordinate currentOriginInRDNAP = new Coordinate(transform.position).Convert(CoordinateSystem.RDNAP);
+            Coordinate originDestinationInRDNAP = originDestination.Convert(CoordinateSystem.RDNAP);
+            if (LogShifts) Debug.Log($"Moving origin from {currentOriginInRDNAP.ToVector3()} (EPSG:7415) to {originDestinationInRDNAP.ToVector3()} (EPSG:7415)");
 #endif
 
-            onPreShift.Invoke(CurrentOrigin, destination);
+            onPreShift.Invoke(currentOrigin, originDestination);
 
-            CoordinateSystems.SetOrigin(destination);
+            CoordinateSystems.SetOrigin(originDestination);
 
             // Shout to the world that the origin has changed to this coordinate
-            onPostShift.Invoke(CurrentOrigin, destination);
+            onPostShift.Invoke(currentOrigin, originDestination);
         }
     }
 }
