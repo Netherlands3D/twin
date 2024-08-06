991e7f721d9e51b726fead3b78870161408e5f82
Sam Baas <samuelpetrus@gmail.com>
2024-08-06 16:23:36 +0200

    removed unused usings

--- Assets/Scripts/Layers/LayerTypes/WFSGeoJSONLayerGameObject.cs
+++ Assets/Scripts/Layers/LayerTypes/WFSGeoJSONLayerGameObject.cs
@@ -1,18 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json;
 using UnityEngine;
-using GeoJSON.Net;
-using GeoJSON.Net.CoordinateReferenceSystem;
-using GeoJSON.Net.Feature;
-using GeoJSON.Net.Geometry;
-using Netherlands3D.Coordinates;
-using SimpleJSON;
-using UnityEngine.Events;
 using Netherlands3D.Twin.Layers.Properties;
-using System.Linq;
 using Netherlands3D.CartesianTiles;
 
 namespace Netherlands3D.Twin.Layers
