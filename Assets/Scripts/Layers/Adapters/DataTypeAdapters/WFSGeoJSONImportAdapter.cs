96749d3241abca974c4522338a522a9bc6efb82b
Sam Baas <samuelpetrus@gmail.com>
2024-08-06 15:45:21 +0200

    removed debug log

--- Assets/Scripts/Layers/Adapters/DataTypeAdapters/WFSGeoJSONImportAdapter.cs
+++ Assets/Scripts/Layers/Adapters/DataTypeAdapters/WFSGeoJSONImportAdapter.cs
@@ -107,7 +107,6 @@ namespace Netherlands3D.Twin
             WFSGeoJsonLayerGameObject newLayer = Instantiate(layerPrefab);
             newLayer.LayerData.SetParent(folderLayer);
             newLayer.name = featureType;
-            Debug.Log("WFS BBOX URL: " + getFeatureUrl);
 
             newLayer.CartesianTileWFSLayer.WfsUrl = getFeatureUrl;
         }
