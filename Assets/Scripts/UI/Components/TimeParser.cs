247fcd9808b6916a39545e969e65bec63a07cc6e
Tom Simons <tom.v.simons@gmail.com>
2024-08-20 15:24:10 +0200

    simplified parsing of different separation characters

--- Assets/Scripts/UI/Components/TimeParser.cs
+++ Assets/Scripts/UI/Components/TimeParser.cs
@@ -11,18 +11,13 @@ public class TimeParser : MonoBehaviour
 
     public void ParseTime(string timeString)
     {
-        Debug.Log("parsing: " + timeString);
-        string[] formats = { "HH:mm", "HH.mm", "HH;mm", "HH,mm", "H:mm", "H.mm", "H;mm", "H,mm", "H:m", "H.m", "H;m", "H,m" };
-        if (DateTime.TryParseExact(timeString, formats, null, System.Globalization.DateTimeStyles.None, out DateTime parsedTime))
+        //replace separation characters so parser will work 
+        timeString = timeString.Replace('.', ':');
+        timeString = timeString.Replace(';', ':');
+        timeString = timeString.Replace(',', ':');
+        
+        if (DateTime.TryParse(timeString, out var parsedTime)) //try default formats
         {
-            Debug.Log("parsed time using custom format: " + parsedTime);
-            hourParsed.Invoke(parsedTime.Hour);
-            minuteParsed.Invoke(parsedTime.Minute);
-            secondParsed.Invoke(parsedTime.Second);
-        }
-        else if (DateTime.TryParse(timeString, out parsedTime))
-        {
-            Debug.Log("parsed time: " + parsedTime);
             hourParsed.Invoke(parsedTime.Hour);
             minuteParsed.Invoke(parsedTime.Minute);
             secondParsed.Invoke(parsedTime.Second);
