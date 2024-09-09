cb3ec0c340b8aa4a84644026d353354f9e4f9332
Tom Simons <tom.v.simons@gmail.com>
2024-09-09 14:45:41 +0200

    attempt fix of shortcut

--- Assets/Scripts/Projects/FileOpenShortcut.cs
+++ Assets/Scripts/Projects/FileOpenShortcut.cs
@@ -1,64 +1,57 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using Netherlands3D.JavascriptConnection;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
-#if !UNITY_EDITOR && UNITY_WEBGL
-using Netherlands3D.JavascriptConnection;
-#endif
+
 namespace Netherlands3D.Twin
 {
     public class FileOpenShortcut : MonoBehaviour
     {
+        [DllImport("__Internal")]
+        private static extern void UploadFile(string gameObjectName, string methodName, string filter, bool multiselect);
         [Tooltip("Allowed file input selections")]
-        [SerializeField]
-        private string fileExtentions = "csv";
+        [SerializeField] private string fileExtentions = "nl3d";
 
         [Tooltip("Allowed selection multiple files")]
-        [SerializeField]
-        private bool multiSelect = false;
-
-        [DllImport("__Internal")]
-        private static extern void AddFileInput(string inputName, string fileExtentions, bool multiSelect);
+        [SerializeField] private bool multiSelect = false;        
         
         public UnityEvent<string> onFilesSelected = new();
 
-#if !UNITY_EDITOR && UNITY_WEBGL
-        private string fileInputName;
-        private FileInputIndexedDB javaScriptFileInputHandler;
-
-        void Start()
+        public void OpenFileDialog()
         {
-            javaScriptFileInputHandler = FindObjectOfType<FileInputIndexedDB>(true);
-            if (javaScriptFileInputHandler == null)
+            // Call the JavaScript function to open the file dialog
+            UploadFile(gameObject.name, "OnFileSelected", fileExtentions, multiSelect);
+        }
+        
+        // This is the callback method that gets called when the file is selected
+        public void OnFileSelected(string filePaths)
+        {
+            Debug.Log("shortcut received: " + filePaths);
+            onFilesSelected.Invoke(filePaths);        
+        }
+
+        private void Update()
+        {
+            var ctrlModifier = CtrlModifierIsPressed();
+
+            if (Keyboard.current.oKey.wasPressedThisFrame && ctrlModifier)
             {
-                GameObject go = new GameObject("UserFileUploads");
-                javaScriptFileInputHandler = go.AddComponent<FileInputIndexedDB>();
+                print("opening file with shortcut");
+                OpenFileDialog();
+            }
+        }
+        
+        public static bool CtrlModifierIsPressed()
+        {
+            if (SystemInfo.operatingSystemFamily == OperatingSystemFamily.MacOSX)
+            {
+                return Keyboard.current.leftCommandKey.isPressed || Keyboard.current.rightCommandKey.isPressed;
             }
 
-            // Set file input name with generated id to avoid html conflicts
-            fileInputName += "_" + gameObject.GetInstanceID();
-            name = fileInputName;
+            return Keyboard.current.ctrlKey.isPressed;
+        }
 
-            // DrawHTMLOverCanvas javascriptInput = gameObject.AddComponent<DrawHTMLOverCanvas>();
-            AddFileInput(fileInputName, fileExtentions, multiSelect);
-        }
-        
-        public void ClickNativeButton()
-        {
-            javaScriptFileInputHandler.SetCallbackAddress(SendResults);
-        }
-        #endif
-        
-        public void SendResults(string filePaths)
-        {
-            Debug.Log("o: " + Keyboard.current.oKey.isPressed);
-            Debug.Log("cmd: " + Keyboard.current.leftCommandKey.isPressed);
-            Debug.Log("shortcut object received: " + filePaths);
-            onFilesSelected.Invoke(filePaths);
-        }
     }
 }
