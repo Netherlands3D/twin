d4aa7f8d237138953458a50373fa0aa903247159
Mike van Riel <mike@ingewikkeld.net>
2024-10-14 09:48:50 +0200

    Add an explanation how Expressions match with the Interpreter design pattern for ease of understanding

--- Assets/_BuildingBlocks/LayerStyles/Scripts/Expression.cs
+++ Assets/_BuildingBlocks/LayerStyles/Scripts/Expression.cs
@@ -5,6 +5,10 @@ namespace Netherlands3D.LayerStyles
         /// <summary>
         /// Resolve the expression by performing any action necessary, optionally including information from
         /// context that can be passed to more complex expressions.
+        ///
+        /// This is an implementation of the Interpreter design pattern (https://sourcemaking.com/design_patterns/interpreter)
+        /// where the method that 'does the work' is called "Resolve" because in common (developer) language you say
+        /// that you "resolve" an expression.
         /// </summary>
         /// <param name="context">An object containing information on the current visualisation, dataLayer and/or feature</param>
         /// <returns></returns>
