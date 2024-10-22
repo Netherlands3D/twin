267d2b138dd0ce54f6f4fc8573ba9f94e36a843d
Mike van Riel <mike@ingewikkeld.net>
2024-10-22 18:50:41 +0200

    Add the functionality to import Gltf and Glb files
    This commit will add the importer, spawner and layer prefabs to import, persist and load a Gltf/Glb file.
    
    There is however the issue of textures not loading, but I think this has to do with the changes that we did to limit the number of shader variants for Gltfast; and as such stripping the texture loading.

---
+++ Assets/_Functionalities/GltfImporter/Scripts/GltfSpawner.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using GLTFast;
+using Netherlands3D.Twin.Layers.Properties;
+using UnityEngine;
+
+namespace Netherlands3D.Twin.Functionalities.GltfImporter
+{
+    public class GltfSpawner : MonoBehaviour, ILayerWithPropertyData
+    {
+        private GltfPropertyData propertyData = new();
+        public LayerPropertyData PropertyData => propertyData;
+
+        private void Awake()
+        {
+            gameObject.transform.position = ObjectPlacementUtility.GetSpawnPoint();
+        }
+
+        private async void Start()
+        {
+            var localPath = propertyData.Uri.LocalPath.TrimStart('/', '\\');
+            var path = Path.Combine(Application.persistentDataPath, localPath);
+            
+            byte[] data = await File.ReadAllBytesAsync(path);
+            var gltf = new GltfImport();
+            bool success = await gltf.LoadGltfBinary(data, new Uri(path));
+            if (success) {
+                await gltf.InstantiateMainSceneAsync(transform);
+            }
+        }
+
+        public void LoadProperties(List<LayerPropertyData> properties)
+        {
+            var propertyData = properties.OfType<GltfPropertyData>().FirstOrDefault();
+            if (propertyData == null) return;
+
+            // Property data is set here, and the parsing and loading of the actual data is done
+            // in the start method, there a coroutine is started to load the data in a streaming fashion.
+            // If we do that here, then this may conflict with the loading of the project file and it would
+            // cause duplication when adding a layer manually instead of through the loading mechanism
+            this.propertyData = propertyData;
+        }
+    }
+}
