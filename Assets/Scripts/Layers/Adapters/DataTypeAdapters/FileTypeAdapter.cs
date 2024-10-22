654cbc80b8e3c61c042cf073e5920c3bf52a9fef
Tom Simons <tom.v.simons@gmail.com>
2024-10-22 11:22:38 +0200

    attempted to force a copy of the file

--- Assets/Scripts/Layers/Adapters/DataTypeAdapters/FileTypeAdapter.cs
+++ Assets/Scripts/Layers/Adapters/DataTypeAdapters/FileTypeAdapter.cs
@@ -13,7 +13,7 @@ namespace Netherlands3D.Twin
         public string Extension;
         public UnityEvent<LocalFile> FileReceived;
     }
-    
+
     [CreateAssetMenu(menuName = "Netherlands3D/Adapters/FileTypeAdapter", fileName = "FileTypeAdapter", order = 0)]
     public class FileTypeAdapter : ScriptableObject
     {
@@ -33,27 +33,40 @@ namespace Netherlands3D.Twin
         {
             if (file.EndsWith(','))
                 file = file.Remove(file.Length - 1);
-            
+
             var absoluteFilePath = Path.Combine(Application.persistentDataPath, file);
-            
+
             var fileName = Path.GetFileNameWithoutExtension(file);
             var extension = Path.GetExtension(file).ToLower();
 
             var guid = Guid.NewGuid();
             var newFilePathRelative = guid + extension;
             var newFilePathAbsolute = Path.Combine(Application.persistentDataPath, guid + extension);
-            
-            Debug.Log(absoluteFilePath + " will be copied to: " + newFilePathAbsolute);
-            File.Copy(absoluteFilePath, newFilePathAbsolute);
+
+            // var newFilePathRelative = Path.Combine(fileName + extension);
+            using (FileStream fileStream = File.Create(newFilePathAbsolute))
+            {
+                // byte[] bytes = new byte[] { 0, 1, 2, 3, 5 };
+                // fileStream.Write(bytes);
                 
+                Debug.Log(absoluteFilePath + " will be copied to: " + newFilePathAbsolute);
+                var fileBytes = File.ReadAllBytes(absoluteFilePath);
+                // var fileStream = File.Create(newFilePathAbsolute);
+                Debug.Log("read: " + fileBytes.Length + " bytes");
+                fileStream.Write(fileBytes, 0, fileBytes.Length);
+                Debug.Log("writing: " + fileBytes.Length + " bytes");
+                File.WriteAllBytes(newFilePathAbsolute, fileBytes);
+                // File.Copy(absoluteFilePath, newFilePathAbsolute);
+            }
+
             if (extension.StartsWith('.'))
                 extension = extension.Substring(1);
-            
+
             var fileTypeEvent = fileTypeEvents.FirstOrDefault(fte => fte.Extension.ToLower() == extension);
-            
+
             Debug.Log("processing file: " + newFilePathRelative);
-            
-            if(fileTypeEvent != null)
+
+            if (fileTypeEvent != null)
             {
                 var localFile = new LocalFile()
                 {
@@ -61,6 +74,7 @@ namespace Netherlands3D.Twin
                     LocalFilePath = newFilePathRelative,
                     OriginalFileName = fileName
                 };
+
 
                 fileTypeEvent.FileReceived.Invoke(localFile);
             }
