e4ffe3169f43b23eaad9b3cec4ac3fc50cddec71
bozmir <bozmir@hotmail.com>
2024-10-28 11:30:56 +0100

    reverting objspawner part 2...

--- Assets/_Functionalities/ObjImporter/Scripts/ObjSpawner.cs
+++ Assets/_Functionalities/ObjImporter/Scripts/ObjSpawner.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Netherlands3D.Twin.Layers.Properties;
 using UnityEngine;
 
