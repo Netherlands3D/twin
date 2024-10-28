47caab79ec8b581389738011542d08492bae7e6f
bozmir <bozmir@hotmail.com>
2024-10-28 12:42:11 +0100

    also a datacontainer for bagids with assets that should not be visible for example demolished buildings

--- Assets/BagIdHider.cs
+++ Assets/BagIdHider.cs
@@ -8,6 +8,7 @@ namespace Netherlands3D.Twin
     public class BagIdHider : MonoBehaviour
     {
         [SerializeField] private HiddenBagIds data;
+        [SerializeField] private HiddenBagIds alwaysHiddenData;
         public ColorSetLayer ColorSetLayer { get; private set; } = new ColorSetLayer(0, new());
         private Dictionary<string, Color> buildingColors = new Dictionary<string, Color>();
 
@@ -31,6 +32,8 @@ namespace Netherlands3D.Twin
         {
             buildingColors.Clear();
             foreach (string id in ids)
+                buildingColors.Add(id, Color.clear);
+            foreach (string id in alwaysHiddenData.bagIds)
                 buildingColors.Add(id, Color.clear);
         }
 
