25933fa9f90e8b61b4a05b4ad8d30df993e48ade
Mike van Riel <mike@ingewikkeld.net>
2024-11-11 11:54:02 +0100

    Revert "attempted to load tiles from new RD url"
    This reverts commit 4865c85d85a3170ea4fa9d145a165d79f984d77f.

--- Assets/Scripts/ATMTileCoordinates.cs
+++ Assets/Scripts/ATMTileCoordinates.cs
@@ -8,7 +8,7 @@ namespace Netherlands3D.Twin
 {
     public class ATMTileCoordinates : MonoBehaviour
     {
-        // [SerializeField] private int[] z = new[] { 16, 19 };
+        [SerializeField] private int[] z = new[] { 16, 19 };
         [SerializeField] private string url = @"https://images.huygens.knaw.nl/webmapper/maps/pw-1943/{z}/{x}/{y}.png";
 
         private const double RDOriginX = 155000; // Origin X for RD New
@@ -22,19 +22,19 @@ namespace Netherlands3D.Twin
 
         private const double RDToWebMercatorScale = 1.9332276; // Accurate scaling factor for RD New to Web Mercator
 
-        // public static (int tileX, int tileY) RDToTileXY(double rdX, double rdY, int zoom)
-        // {
-        //     // Step 1: Normalize RD coordinates to a 0-1 scale based on RD New bounds
-        //     double normX = (rdX - RDMinX) / (RDMaxX - RDMinX);
-        //     double normY = (RDMaxY - rdY) / (RDMaxY - RDMinY); // Y axis is inverted for tile coordinates
-        //
-        //     // Step 2: Scale normalized coordinates to the appropriate tile coordinates
-        //     int mapSize = 1 << zoom; // Total tiles in one dimension at this zoom level
-        //     int tileX = (int)Math.Floor(normX * mapSize);
-        //     int tileY = (int)Math.Floor(normY * mapSize);
-        //
-        //     return (tileX, tileY);
-        // }
+        public static (int tileX, int tileY) RDToTileXY(double rdX, double rdY, int zoom)
+        {
+            // Step 1: Normalize RD coordinates to a 0-1 scale based on RD New bounds
+            double normX = (rdX - RDMinX) / (RDMaxX - RDMinX);
+            double normY = (RDMaxY - rdY) / (RDMaxY - RDMinY); // Y axis is inverted for tile coordinates
+
+            // Step 2: Scale normalized coordinates to the appropriate tile coordinates
+            int mapSize = 1 << zoom; // Total tiles in one dimension at this zoom level
+            int tileX = (int)Math.Floor(normX * mapSize);
+            int tileY = (int)Math.Floor(normY * mapSize);
+
+            return (tileX, tileY);
+        }
 
         // magic ChatGPT function 
         public static (int x, int y) LatLonToTileXY(double lat, double lon, int zoom)
@@ -44,7 +44,7 @@ namespace Netherlands3D.Twin
             int x = (int)Math.Floor(dx);
 
             // Calculate the y coordinate
-            double latRad = -lat * Math.PI / 180.0d;
+            double latRad = lat * Math.PI / 180.0d;
             var dy = (1 - Math.Log(Math.Tan(latRad) + 1 / Math.Cos(latRad)) / Math.PI) / 2 * (1 << zoom);
             int y = (int)Math.Floor(dy);
 
@@ -66,49 +66,25 @@ namespace Netherlands3D.Twin
         }
 
         public Vector2Int xy;
-        public static Vector2Int originOffset = new Vector2Int(419, -1618);
+
         public string GetTileUrl(Coordinate coord, int zoomLevel)
         {
             var converted = CoordinateToTileXY(coord, zoomLevel);
             print("conv: " + converted.x + ", " + converted.y);
             xy = new Vector2Int(converted.x, converted.y);
 
-            // var a = CoordinateToTileXY(new Coordinate(CoordinateSystem.RD, 121687, 487326), 16);
-            // var b = RDToTileXY(121687, 487326, 16);
-            // Debug.LogError(a + "-----" + b);
-            // CalculateTileOffset();
-            var offsetTileXY = OffsetTileXY(converted.x, converted.y);
-            return GetTileUrl(14, offsetTileXY.x, offsetTileXY.y);
+            var a = CoordinateToTileXY(new Coordinate(CoordinateSystem.RD, 121687, 487326), 16);
+            var b = RDToTileXY(121687, 487326, 16);
+            Debug.LogError(a + "-----" + b);
+
+            return GetTileUrl(zoomLevel, converted.x, converted.y);
         }
 
-        public static (int x, int y) OffsetTileXY(int x, int y)
+        public string GetTileURL(Vector2Int tileKey, int zoomLevel)
         {
-            x -= originOffset.x;
-            y -= originOffset.y;
-            return (x, y);
+            var converted = RDToTileXY(tileKey.x, tileKey.y, zoomLevel);
+            xy = new Vector2Int(converted.tileX, converted.tileY);
+            return GetTileUrl(zoomLevel, converted.tileX, converted.tileY);
         }
-
-        private void Start()
-        {
-            CalculateTileOffset();
-        }
-
-        public void CalculateTileOffset()
-        {
-            var coord = new Coordinate(CoordinateSystem.RD, 121950, 486054);
-            var converted = CoordinateToTileXY(coord, 19);
-            converted.x -= 3788*2+2;//7576/8620.png
-            converted.y -= 4310*2-1;
-            originOffset = new Vector2Int(converted.x, converted.y);
-            print("test " + converted);
-            // 13/3788/4310.png
-        }
-
-        // public string GetTileURL(Vector2Int tileKey, int zoomLevel)
-        // {
-        //     var converted = RDToTileXY(tileKey.x, tileKey.y, zoomLevel);
-        //     xy = new Vector2Int(converted.tileX, converted.tileY);
-        //     return GetTileUrl(zoomLevel, converted.tileX, converted.tileY);
-        // }
     }
 }
