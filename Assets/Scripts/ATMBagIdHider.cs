ccfd9114d0974fea64d5bd71604149fddb3b7f97
bozmir <bozmir@hotmail.com>
2024-11-27 19:53:00 +0100

    flattening the buildings with the colorset in the shader

--- Assets/Scripts/ATMBagIdHider.cs
+++ Assets/Scripts/ATMBagIdHider.cs
@@ -37,7 +37,7 @@ namespace Netherlands3D.Twin
         {
             buildingColors.Clear();
             foreach (string id in ids)
-                buildingColors.Add(id, Color.clear);
+                buildingColors.Add(id, new Color(0,2, 0, 0)); //for now a 2 mapping in the y channel to have the shader recognise this as the position vertex key
             ColorSetLayer = new ColorSetLayer(-2, buildingColors);
         }
 
