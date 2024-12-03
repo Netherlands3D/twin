b4453aba9cd8b04c0c325a891493f7c7d5c2eba7
Tom Simons <tom.v.simons@gmail.com>
2024-12-03 15:58:28 +0100

    fixed potential async list population issue where the list of elements could be cleared before a promise returned a response

--- Assets/_Functionalities/AmsterdamTimeMachine/Scripts/Ui/AtmInformationList.cs
+++ Assets/_Functionalities/AmsterdamTimeMachine/Scripts/Ui/AtmInformationList.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using KindMen.Uxios;
 using KindMen.Uxios.Api;
+using KindMen.Uxios.Errors.Connection;
+using RSG;
 using UnityEngine;
 using QueryParameters = KindMen.Uxios.Http.QueryParameters;
 
@@ -29,10 +31,11 @@ namespace Netherlands3D.Twin
         [SerializeField] private AtmInformationListItem regularListItem;
         private const string bagUriTemplate = "https://api.lod.uba.uva.nl/queries/ATM/images-building-nl3d/2/run?bag=http://bag.basisregistraties.overheid.nl/bag/id/pand/{id}";
         private const string adamLinkTemplate = "https://api.lod.uba.uva.nl/queries/ATM/images-historical-address-nl3d/2/run?address={id}";
+        private Promise<List<AtmInfo>> activePromise;
 
         public void LoadFromBagId(string bagId)
         {
-            QueryParameters parameters = new() {{"id", bagId}};
+            QueryParameters parameters = new() { { "id", bagId } };
 
             PopulateList(new TemplatedUri(bagUriTemplate, parameters));
         }
@@ -43,30 +46,36 @@ namespace Netherlands3D.Twin
 
             // check for an id field, otherwise it is not an Amsterdam Time Machine feature
             if (string.IsNullOrEmpty(adamLinkId)) return;
-            
+
             // check if it is a valid Amsterdam Time Machine feature, otherwise we ignore it 
             if (adamLinkId.StartsWith("https://adamlink.nl/geo/address/") == false) return;
 
-            QueryParameters parameters = new() {{"id", adamLinkId}};
+            QueryParameters parameters = new() { { "id", adamLinkId } };
 
             PopulateList(new TemplatedUri(adamLinkTemplate, parameters));
         }
 
         private void PopulateList(Uri url)
         {
+            if (activePromise != null)
+                Uxios.DefaultInstance.Abort(activePromise);
+            
             contentArea.transform.ClearAllChildren();
 
-            var promise = Resource<List<AtmInfo>>.At(url).Value;
-            promise.Then(CreateListItems);
-            promise.Catch(OnErrorDuringLoading);
+            activePromise = Resource<List<AtmInfo>>.At(url).Value;
+            activePromise.Then(CreateListItems);
+            activePromise.Catch(OnErrorDuringLoading);
         }
 
         private void OnErrorDuringLoading(Exception exception)
         {
             var error = exception as Error;
             
+            if(error is RequestAbortedError)
+                return;
+
             Debug.LogError(
-                $"An issue occurred while loading data from ATM ({error?.Request.Url}):\n{exception.Message}"
+                $"An issue occurred while loading data from ATM ({error?.Response.Request.Url}):\n{exception.Message}"
             );
         }
 
