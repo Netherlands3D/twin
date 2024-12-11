09aa424e302807c6e0ffae0b7559863bd18944d8
FrisoBooij <friso@protofunk.nl>
2024-12-11 11:38:22 +0100

    Updated the UI Ticker
    UI ticker now only triggers when the mouse is over the selected text. The child objects don't automatically tick any longer. however, when the mouse is over a child with a text that is long enough to 'tick', it also triggers its parent's ticker IF the parent's text is also long enough to 'tick'.

--- Assets/Scripts/UI/Components/TextTicker.cs
+++ Assets/Scripts/UI/Components/TextTicker.cs
@@ -11,8 +11,8 @@ namespace Netherlands3D.Twin
         public float scrollSpeed = 200f; // Speed of scrolling
 
         private RectTransform parentRect;
-        private float childWidth;
-        private float parentWidth;
+        public float childWidth;
+        public float parentWidth;
         private Coroutine scrollingCoroutine;
 
         void Awake()
@@ -22,45 +22,27 @@ namespace Netherlands3D.Twin
 
         void Start()
         {
-            // Get initial widths and set initial position  
-            UpdateWidths();
             SetInitialPosition();
-
-            // You can call this method to start monitoring for width changes  
-            StartMonitoringWidth();
         }
 
         private void SetInitialPosition()
         {
+            childWidth = childRect.rect.width;
+            parentWidth = parentRect.rect.width;
+
             if (childWidth > parentWidth)
             {
                 childRect.anchoredPosition = new Vector2(0, childRect.anchoredPosition.y);
             }
         }
 
-        private void UpdateWidths()
+  
+
+        public void CheckAndStartScrolling()
         {
-            // Get widths of parent and child RectTransforms  
             childWidth = childRect.rect.width;
             parentWidth = parentRect.rect.width;
-        }
 
-        // Start monitoring for width changes  
-        public void StartMonitoringWidth()
-        {
-            UpdateWidths(); // Initial check 
-            CheckAndStartScrolling(); // Check and start scrolling if necessary  
-        }
-
-        // Example event trigger method to be called when needed  
-        public void OnWidthChange() // Call this method when a width change occurs  
-        {
-            Invoke("UpdateWidths",0.1f);
-            Invoke("CheckAndStartScrolling",0.1f);
-        }
-
-        private void CheckAndStartScrolling()
-        {
             if (childWidth > parentWidth)
             {
                 if (scrollingCoroutine == null)
@@ -78,6 +60,7 @@ namespace Netherlands3D.Twin
         {
             while (true)
             {
+
                 Vector2 newPosition = childRect.anchoredPosition;
                 newPosition.x -= scrollSpeed * Time.deltaTime;
 
@@ -93,7 +76,7 @@ namespace Netherlands3D.Twin
             }
         }
 
-        private void StopScrolling()
+        public void StopScrolling()
         {
             if (scrollingCoroutine != null)
             {
