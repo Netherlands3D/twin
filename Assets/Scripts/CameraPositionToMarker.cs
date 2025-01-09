b097e6b9f152bf288bab1daf02fcad0a35f5519f
Tom Simons <tom.v.simons@gmail.com>
2025-01-09 12:56:32 +0100

    added option to add camera position markers in edit mode

---
+++ Assets/Scripts/CameraPositionToMarker.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Netherlands3D.Twin
+{
+#if UNITY_EDITOR
+    public class CameraPositionToMarker : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Keyboard.current.cKey.wasPressedThisFrame)
+            {
+                CreateMarker(Camera.main.transform);
+            }
+        }
+
+        private void CreateMarker(Transform transform)
+        {
+            var marker = SavePersistentGameObject.CreatePlayModeObject("Marker");
+            marker.transform.position = transform.position;
+            marker.transform.rotation = transform.rotation;
+            marker.transform.localScale = transform.localScale;
+        }
+    }
+#endif
+}
