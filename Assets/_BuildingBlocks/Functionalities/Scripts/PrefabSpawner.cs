83225d89ca8972d8d1093b5418e029eaaf1c3653
Tom Simons <tom.v.simons@gmail.com>
2025-01-20 14:29:18 +0100

    moved prefabspawner script

--- Assets/_BuildingBlocks/Functionalities/Scripts/PrefabSpawner.cs
+++ Assets/_BuildingBlocks/Functionalities/Scripts/PrefabSpawner.cs
@@ -1,11 +1,9 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using Netherlands3D.Twin.Functionalities;
 using UnityEngine.Events;
 
-namespace Netherlands3D.Twin
+namespace Netherlands3D.Twin.Functionalities
 {
     public class PrefabSpawner : MonoBehaviour
     {
