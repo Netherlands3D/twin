3d28c625b6ffc26602c243356de0ab25d75cf183
bozmir <bozmir@hotmail.com>
2025-01-28 18:32:14 +0100

    checking if position changed, if not then dont compute text. + using string builder to reduce memory overhead. (from 3kb to 1.2kb) + caching method for simple seperate string values brought the memory down from 1.2kb to 128 bytes. the 128b are stringbuilder.tostring which cannot be optimized

--- Assets/_Application/_Twin/UI/BottomBar.cs
+++ Assets/_Application/_Twin/UI/BottomBar.cs
@@ -1,4 +1,7 @@
 using Netherlands3D.Coordinates;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using TMPro;
 using UnityEngine;
 
@@ -9,6 +12,20 @@ namespace Netherlands3D.Twin.UI
         [Header("Camera coordinates")]
         [SerializeField] private TextMeshProUGUI coordinatesText;
         [SerializeField] private string coordinateFormat = "x{0} y{1} z{2}";
+
+        private string[] cachedValues = new string[10] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        private string[] currentSet = new string[10];
+        private string[] xyz = new string[3] { "x", "y", "z" };
+        private string negative = "-";
+        private string space = " ";
+
+        private StringBuilder builder;
+        private Vector3Int lastPosition;
+
+        private void Start()
+        {
+            builder = new StringBuilder();
+        }
         public void Update()
         {
             ApplyCameraPositionToText();
@@ -25,11 +42,49 @@ namespace Netherlands3D.Twin.UI
              );
             var rd = CoordinateConverter.ConvertTo(cameraCoordinate, CoordinateSystem.RDNAP);
 
-            //Replace the placeholders with the coordinates
-            coordinatesText.text = coordinateFormat
-                .Replace("{0}", rd.Points[0].ToString("0"))
-                .Replace("{1}", rd.Points[1].ToString("0"))
-                .Replace("{2}", rd.Points[2].ToString("0"));
+            Vector3Int position = new Vector3Int(Mathf.RoundToInt((float)rd.value1), Mathf.RoundToInt((float)rd.value2), Mathf.RoundToInt((float)rd.value3));
+            if (lastPosition != position)
+            {
+                builder.Clear();
+                AppendValueString(position.x, xyz[0], builder);
+                AppendValueString(position.z, xyz[1], builder);
+                AppendValueString(position.y, xyz[2], builder);
+                coordinatesText.text = builder.ToString();
+                lastPosition = position;
+            }            
+        }
+
+        private void AppendValueString(int value, string dimension, StringBuilder builder)
+        {
+            bool neg;
+            int count;
+            for (int i = 0; i < currentSet.Length; i++)
+                currentSet[i] = null;
+            builder.Append(dimension);
+            string[] result = GetNumberString(value, currentSet, out neg, out count);
+            if (neg)
+                builder.Append(negative);
+            for (int i = 0; i < count; i++)
+                builder.Append(currentSet[count - i - 1]);
+            builder.Append(space);
+        }
+
+
+        private string[] GetNumberString(int number, string[] set, out bool negative, out int count)
+        {
+            negative = number < 0;
+            number = Mathf.Abs(number);
+            int index = 0;
+            while (number > 0)
+            {                
+                int digit = number % 10;
+                number /= 10;
+                set[index] = cachedValues[digit];
+                index++;
+            }
+            //set.Reverse();
+            count = index;
+            return set;
         }
     }
 }
