ff173be5b2172a5fd670f74b602eda231a2bfe2c
Tom Simons <tom.v.simons@gmail.com>
2025-01-29 09:50:51 +0100

    renamed script and class

--- Assets/_Functionalities/WFS/Scripts/WFSRequest.cs
+++ Assets/_Functionalities/WFS/Scripts/WFSRequest.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 namespace Netherlands3D.Functionalities.Wfs
 {
-    public class GeoJSONWFSRequest
+    public class WFSRequest
     {
         private readonly string sourceUrl;
         private readonly string cachedBodyContent;
@@ -27,7 +27,7 @@ namespace Netherlands3D.Functionalities.Wfs
             Unsupported
         }
 
-        public GeoJSONWFSRequest(string sourceUrl, string cachedBodyFilePath)
+        public WFSRequest(string sourceUrl, string cachedBodyFilePath)
         {
             this.sourceUrl = sourceUrl;
             this.cachedBodyContent = cachedBodyFilePath;
