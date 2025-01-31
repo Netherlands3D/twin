a7040a85ff33b84abe954800fe03ad4919cc9f7e
bozmir <bozmir@hotmail.com>
2025-01-31 14:55:06 +0100

    disabling colliders

--- Assets/_Application/Layers/LayerTypes/GeoJsonLayers/GeoJsonLayerGameObject.cs
+++ Assets/_Application/Layers/LayerTypes/GeoJsonLayers/GeoJsonLayerGameObject.cs
@@ -172,7 +172,7 @@ namespace Netherlands3D.Twin.Layers.LayerTypes.GeoJsonLayers
                         }
                         mesh.vertices = vertices.ToArray();
                         mesh.triangles = triangles.ToArray();
-                        subObject.AddComponent<MeshCollider>();
+                        //subObject.AddComponent<MeshCollider>();
                         subObject.AddComponent<MeshRenderer>().material = lineLayer.LineRenderer3D.LineMaterial;
                     }
                     else if (feature.Geometry is MultiPolygon || feature.Geometry is Polygon)
@@ -186,7 +186,7 @@ namespace Netherlands3D.Twin.Layers.LayerTypes.GeoJsonLayers
                     {
                         subObject.transform.position = verts[0];
                         GeoJSONPointLayer pointLayer = layer as GeoJSONPointLayer;
-                        subObject.AddComponent<SphereCollider>().radius = pointLayer.PointRenderer3D.MeshScale * 0.5f;
+                        //subObject.AddComponent<SphereCollider>().radius = pointLayer.PointRenderer3D.MeshScale * 0.5f;
 
                     }
                 }
