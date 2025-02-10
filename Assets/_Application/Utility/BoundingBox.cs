ca94d1aafd4fb9be1b6cfaf47c0b21d68cf95f2c
Tom Simons <tom.v.simons@gmail.com>
2025-02-10 13:48:46 +0100

    fixed incorrect rd bounding coordinates

--- Assets/_Application/Utility/BoundingBox.cs
+++ Assets/_Application/Utility/BoundingBox.cs
@@ -99,6 +99,6 @@ namespace Netherlands3D.Twin.Utility
         }
         
         //RDBounds as defined by https://epsg.io/28992
-        public static BoundingBox RDBounds => new BoundingBox(new Coordinate(CoordinateSystem.RD, 482.06d, 3066602.42d), new Coordinate(CoordinateSystem.RD, 284182.97d, 637049.52d));
+        public static BoundingBox RDBounds => new BoundingBox(new Coordinate(CoordinateSystem.RD, 482.06d, 306602.42d), new Coordinate(CoordinateSystem.RD, 284182.97d, 637049.52d));
     }
 }
