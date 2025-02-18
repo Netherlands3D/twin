5247b9784a82b453d5eaea8e8bc72f81e18a80c8
bozmir <bozmir@hotmail.com>
2025-02-18 11:56:40 +0100

    netherlands bounds renaming

--- Assets/_Application/Utility/StandardBoundingBoxes.cs
+++ Assets/_Application/Utility/StandardBoundingBoxes.cs
@@ -7,6 +7,6 @@ namespace Netherlands3D.Twin.Utility
         // RDBounds as defined by https://epsg.io/28992
         public static BoundingBox RDBounds => new BoundingBox(new Coordinate(CoordinateSystem.RD, 482.06d, 306602.42d), new Coordinate(CoordinateSystem.RD, 284182.97d, 637049.52d));
         // Netherlands wgs84_latlon bounds
-        public static BoundingBox Wgs84_LatLonBounds => new BoundingBox(new Coordinate(CoordinateSystem.WGS84_LatLon, 50.8037d, 3.31497d), new Coordinate(CoordinateSystem.WGS84_LatLon, 53.5104d, 7.09205d));
+        public static BoundingBox Wgs84LatLon_NetherlandsBounds => new BoundingBox(new Coordinate(CoordinateSystem.WGS84_LatLon, 50.8037d, 3.31497d), new Coordinate(CoordinateSystem.WGS84_LatLon, 53.5104d, 7.09205d));
     }
 }
