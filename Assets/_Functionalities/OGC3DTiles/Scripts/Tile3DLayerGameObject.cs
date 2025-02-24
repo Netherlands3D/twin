a33e1ad6b095bb68d3e38401ff999c2f331dd57c
bozmir <bozmir@hotmail.com>
2025-02-24 14:22:18 +0100

    fixed tiles3d bounds

--- Assets/_Functionalities/OGC3DTiles/Scripts/Tile3DLayerGameObject.cs
+++ Assets/_Functionalities/OGC3DTiles/Scripts/Tile3DLayerGameObject.cs
@@ -6,6 +6,7 @@ using Netherlands3D.Twin.Layers;
 using Netherlands3D.Twin.Layers.LayerTypes;
 using Netherlands3D.Twin.Layers.LayerTypes.Credentials;
 using Netherlands3D.Twin.Layers.Properties;
+using Netherlands3D.Twin.Utility;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
@@ -16,6 +17,8 @@ namespace Netherlands3D.Functionalities.OGC3DTiles
     [RequireComponent(typeof(Read3DTileset))]
     public class Tile3DLayerGameObject : LayerGameObject, ILayerWithPropertyData, ILayerWithPropertyPanels, ILayerWithCredentials
     {
+        public override BoundingBox Bounds => tileSet.root != null ? new BoundingBox(tileSet.root.BottomLeft, tileSet.root.TopRight) : null;
+
         private Read3DTileset tileSet;
         [SerializeField] private bool usePropertySections = true;
         private List<IPropertySectionInstantiator> propertySections = new();
