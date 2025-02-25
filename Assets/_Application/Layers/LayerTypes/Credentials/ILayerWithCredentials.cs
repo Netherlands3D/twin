a3dfe759b7c358ee752d6a7955ec62fa5068ac3e
bozmir <bozmir@hotmail.com>
2025-02-25 17:45:05 +0100

    reverted the ILayerWithCredentials

--- Assets/_Application/Layers/LayerTypes/Credentials/ILayerWithCredentials.cs
+++ Assets/_Application/Layers/LayerTypes/Credentials/ILayerWithCredentials.cs
@@ -5,9 +5,21 @@ using UnityEngine.Networking;
 
 namespace Netherlands3D.Twin.Layers.LayerTypes.Credentials
 {
-    public interface ILayerWithCredentials : ICredentialsObject
+    public interface ILayerWithCredentials
     {
         public UnityEvent<UnityWebRequest> OnServerResponseReceived { get; }
         public UnityEvent<Uri> OnURLChanged { get; }
+        public string URL { get; set; } //The base URL to check the credentials for
+
+        //Header based credentials
+        public void SetCredentials(string username, string password);
+        public void SetBearerToken(string token);
+
+        //Url Query parameters
+        public void SetKey(string key);
+        public void SetToken(string token);
+        public void SetCode(string code);
+
+        public void ClearCredentials();
     }
 }
--- Assets/_BuildingBlocks/Credentials/Scripts/ICredentialsObject.cs
+++ Assets/_BuildingBlocks/Credentials/Scripts/ICredentialsObject.cs
@@ -6,17 +6,6 @@ namespace Netherlands3D.Credentials
 {
     public interface ICredentialsObject
     {
-        public string URL { get; set; } //The base URL to check the credentials for
-
-        //Header based credentials
-        public void SetCredentials(string username, string password);
-        public void SetBearerToken(string token);
-
-        //Url Query parameters
-        public void SetKey(string key);
-        public void SetToken(string token);
-        public void SetCode(string code);
-
-        public void ClearCredentials();
+      
     }
 }
