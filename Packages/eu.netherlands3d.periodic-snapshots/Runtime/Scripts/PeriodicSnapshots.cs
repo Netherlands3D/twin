cfab52ae8e8789292b24c240ffbee61fb264f85f
bozmir <bozmir@hotmail.com>
2025-02-27 18:46:31 +0100

    we also need to be able to set it with a new dynamic list of moments

--- Packages/eu.netherlands3d.periodic-snapshots/Runtime/Scripts/PeriodicSnapshots.cs
+++ Packages/eu.netherlands3d.periodic-snapshots/Runtime/Scripts/PeriodicSnapshots.cs
@@ -37,7 +37,14 @@ namespace Netherlands3D.Snapshots
             }
         }
 
-        public List<Moment> Moments => moments;
+        public List<Moment> Moments
+        {
+            get { return moments; }
+            set
+            {
+                moments = value;
+            }
+        }
 
         [SerializeField] private Camera sourceCamera;
         [SerializeField] private SunTime sunTime;
