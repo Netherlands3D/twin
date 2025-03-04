f0e62eb92cc89724103a7e20e27ad4bc1c50a335
bozmir <bozmir@hotmail.com>
2025-03-04 11:39:29 +0100

    auto resolving download snapshots event for snapshots panel

---
+++ Assets/_Functionalities/Snapshots/Scripts/SnapshotsPanel.cs
@@ -0,0 +1,25 @@
+using Netherlands3D.Snapshots;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Netherlands3D.Twin
+{
+    public class SnapshotsPanel : MonoBehaviour
+    {
+        private PeriodicSnapshots snapshots;
+        [SerializeField] private Button downloadButton;
+
+        private void Start()
+        {
+            snapshots = FindObjectOfType<PeriodicSnapshots>();
+            downloadButton.onClick.AddListener(snapshots.DownloadSnapshots);
+        }
+
+        private void OnDestroy()
+        {
+            downloadButton.onClick.RemoveListener(snapshots.DownloadSnapshots);
+        }
+    }
+}
