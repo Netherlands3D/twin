4aed8561b66ebc2fa21ddc9ac68fab313cf93ff6
Tom Simons <tom.v.simons@gmail.com>
2025-03-14 12:53:56 +0100

    initial commit - Took the source scripts from https://github.com/mattjohnsonpint/GeoTimeZone/ to convert lat/lon to a string representing the time zone. Because WebGL has no access to TimeZoneInfo.FindSystemTimeZoneById(String) a custom implementation had to be built to calculate the offsets from UTC. The information was gotten from https://github.com/rxaviers/iana-tz-data/blob/master/iana-tz-data.json. This is IANA time zone data converted to json for easier parsing. In the future this should be preferably gotten from IANA directly

---
+++ Packages/eu.netherlands3d.geotimezone/Runtime/Scripts/TimeZoneConverter.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+//This is needed because TimeZoneInfo.FindSystemTimeZoneById does not work in WebGL and we need to make our own conversion table to be able to convert from local time to UTC
+namespace GeoTimeZone
+{
+    public class TimeZoneConversionInfo
+    {
+        public long?[] untils;
+        public double[] offsets;
+        public int[] isdsts;
+
+        public TimeZoneConversionInfo(JObject zoneInfo)
+        {
+            untils = zoneInfo["untils"].ToObject<long?[]>();
+            offsets = zoneInfo["offsets"].ToObject<double[]>();
+            isdsts = zoneInfo["isdsts"].ToObject<int[]>();
+        }
+    }
+
+    public static class TimeZoneConverter
+    {
+        private static readonly JObject timeZoneData;
+        private static string currentTimeZoneId;
+        private static TimeZoneConversionInfo currentTimeZoneInfo;
+
+        static TimeZoneConverter()
+        {
+            var tzData = Resources.Load<TextAsset>("iana-tz-data");
+            timeZoneData = JObject.Parse(tzData.text);
+        }
+
+        public static DateTime ConvertToUTC(DateTime localTime, string localTimeZone)
+        {
+            if (localTimeZone != currentTimeZoneId)
+            {
+                Debug.Log("getting time zone: " + localTimeZone);
+                currentTimeZoneInfo = GetZoneInfo(localTimeZone);
+            }
+
+            if (currentTimeZoneInfo == null)
+            {
+                throw new ArgumentException("Invalid time zone");
+            }
+
+            var offset = GetUTCOffset(localTime, currentTimeZoneInfo);
+            Debug.Log("time offset: " + offset);
+            return localTime + offset;
+        }
+
+        private static TimeZoneConversionInfo GetZoneInfo(string localTimeZone)
+        {
+            currentTimeZoneId = localTimeZone;
+            
+            var zones = timeZoneData["zoneData"] as JObject;
+            if (zones == null)
+            {
+                throw new ArgumentException("Invalid zone data format.");
+            }
+
+            // Try to split the full timezone name into continent and city if it's of the form "Europe/London"
+            var timeZoneParts = localTimeZone.Split('/');
+            if (timeZoneParts.Length == 2)
+            {
+                var continent = timeZoneParts[0]; // "Europe"
+                var city = timeZoneParts[1]; // "London"
+
+                // Check if the continent exists and contains the city
+                if (zones.ContainsKey(continent))
+                {
+                    var continentData = zones[continent] as JObject;
+                    if (continentData != null && continentData.ContainsKey(city))
+                    {
+                        return new TimeZoneConversionInfo(continentData[city] as JObject);
+                    }
+                }
+            }
+
+            // If the input is not in continent/city format, check for abbreviations and other timezone names
+            if (zones.ContainsKey(localTimeZone))
+            {
+                return new TimeZoneConversionInfo(zones[localTimeZone] as JObject);
+            }
+
+            // If no match found, search for abbreviations or other variants in all zone data
+            foreach (var continent in zones)
+            {
+                var continentData = continent.Value as JObject;
+                if (continentData != null)
+                {
+                    foreach (var key in continentData)
+                    {
+                        var zoneData = key.Value as JObject;
+                        var abbrs = zoneData["abbrs"].ToObject<List<string>>();
+                        if (abbrs.Contains(localTimeZone))
+                        {
+                            return new TimeZoneConversionInfo(zoneData); // Return the matching zone data
+                        }
+                    }
+                }
+            }
+
+            // If no matching time zone is found, throw an exception
+            throw new ArgumentException("Invalid time zone: " + localTimeZone);
+        }
+
+        public static TimeSpan GetUTCOffset(DateTime localTime, TimeZoneConversionInfo zoneInfo)
+        {
+            long unixMilliseconds = new DateTimeOffset(localTime).ToUnixTimeMilliseconds();
+            
+            for (int i = 0; i < zoneInfo.untils.Length; i++)
+            {
+                if (zoneInfo.untils[i] == null || zoneInfo.untils[i].Value >= unixMilliseconds)
+                {
+                    return TimeSpan.FromMinutes(zoneInfo.offsets[i]);
+                }
+            }
+
+            throw new InvalidOperationException("Unable to determine the time zone offset.");
+        }
+
+        // private static int GetUTCOffset(DateTime localTime, JToken zoneInfo)
+        // {
+        //     Debug.Log(zoneInfo);
+        //     
+        //     var abbrs = zoneInfo["abbrs"].ToObject<List<string>>();
+        //     var untils = zoneInfo["untils"].ToObject<List<long?>>();
+        //     var offsets = zoneInfo["offsets"].ToObject<List<double>>();
+        //     var isdsts = zoneInfo["isdsts"].ToObject<List<int>>();
+        //
+        //     for (int i = 0; i < untils.Count; i++)
+        //     {
+        //         if (untils[i].HasValue && localTime.Ticks < untils[i].Value)
+        //         {
+        //             continue;
+        //         }
+        //
+        //         var offset = offsets[i];
+        //         if (isdsts[i] == 1)
+        //         {
+        //             offset += 60; // Add 1 hour for DST
+        //         }
+        //
+        //         return (int)offset;
+        //     }
+        //
+        //     throw new ArgumentException("Unable to determine UTC offset");
+        // }
+    }
+}
---
+++ Packages/eu.netherlands3d.geotimezone/Runtime/Scripts/TimezoneFileReader.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using UnityEngine;
+
+namespace GeoTimeZone
+{
+    internal static class TimezoneFileReader
+    {
+        private const int LineLength = 8;
+        private const int LineEndLength = 1;
+
+        private static readonly Lazy<MemoryStream> LazyData = new(LoadData);
+        private static readonly Lazy<int> LazyCount = new(GetCount);
+
+        // private static MemoryStream LoadData()
+        // {
+        //     var rawData = Resources.Load<TextAsset>("TZ");
+        //     // var compressedData = Resources.Load<TextAsset>("TZ.dat");
+        //     // using var compressedStream = new MemoryStream(compressedData.bytes);
+        //     
+        //     // var assembly = typeof(TimezoneFileReader).Assembly;
+        //     // using var compressedStream = assembly.GetManifestResourceStream("GeoTimeZone.TZ.dat.gz");
+        //     // using var stream = new GZipStream(compressedStream!, CompressionMode.Decompress);
+        //     var ms = new StreamReader();
+        //     stream.CopyTo(ms);
+        //     ms.Seek(0, SeekOrigin.Begin);
+        //     return ms;
+        // }
+        //
+        private static MemoryStream LoadData()
+        {
+            // Load the compressed file from Unity's Resources
+            var compressedData = Resources.Load<TextAsset>("TZ.dat.gz");
+
+            using var compressedStream = new MemoryStream(compressedData.bytes);
+            using var stream = new GZipStream(compressedStream, CompressionMode.Decompress);
+
+            var ms = new MemoryStream();
+            stream.CopyTo(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+            return ms;
+        }
+
+        public static byte[] GetFileBufferFromPath(string path)
+        {
+            FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            byte[] buffer = new byte[fileStream.Length];
+            fileStream.Read(buffer, 0, (int)fileStream.Length);
+            fileStream.Close();
+            return buffer;
+        }
+        
+        private static int GetCount() => (int)(LazyData.Value.Length / (LineLength + LineEndLength));
+
+        public static int Count => LazyCount.Value;
+
+#if NET6_0_OR_GREATER || NETSTANDARD2_1
+        public static ReadOnlySpan<byte> GetGeohash(int line) => GetLine(line, 0, Geohash.Precision);
+#else
+    public static byte[] GetGeohash(int line) => GetLine(line, 0, Geohash.Precision);
+#endif
+
+        public static int GetLineNumber(int line)
+        {
+            var digits = GetLine(line, Geohash.Precision, LineLength - Geohash.Precision);
+            return GetDigit(digits[2]) + ((GetDigit(digits[1]) + (GetDigit(digits[0]) * 10)) * 10);
+        }
+
+        private static int GetDigit(byte b) => b - '0';
+
+#if NET6_0_OR_GREATER || NETSTANDARD2_1
+        private static ReadOnlySpan<byte> GetLine(int line, int start, int count)
+#else
+    private static byte[] GetLine(int line, int start, int count)
+#endif
+        {
+            var index = ((LineLength + LineEndLength) * (line - 1)) + start;
+            var stream = LazyData.Value;
+
+#if NET6_0_OR_GREATER || NETSTANDARD2_1
+            return new ReadOnlySpan<byte>(stream.GetBuffer(), index, count);
+#else
+        var buffer = new byte[count];
+        Array.Copy(stream.GetBuffer(), index, buffer, 0, count);
+        return buffer;
+#endif
+        }
+    }
+}
