f4a3af276612a534755707e2300486507a530445
Tom Simons <tom.v.simons@gmail.com>
2025-03-24 11:43:01 +0100

    removed todo

--- Assets/_BuildingBlocks/Credentials/Scripts/StoredAuthorization/FailedOrUnsupported.cs
+++ Assets/_BuildingBlocks/Credentials/Scripts/StoredAuthorization/FailedOrUnsupported.cs
@@ -2,8 +2,7 @@ using System;
 
 namespace Netherlands3D.Credentials.StoredAuthorization
 {
-    //this type is returned when all supported authorization types fail (403 error). This means either the provided credentials are invalid, or we do not support the authorization type of this url. 
-    //todo: try to determine the difference between wrong credentials and unsupported authorization types
+    //this type is returned when all supported authorization types fail (401 or 403 error). This means either the provided credentials are invalid, or we do not support the authorization type of this url. 
     [Serializable]
     public class FailedOrUnsupported : StoredAuthorization
     {
