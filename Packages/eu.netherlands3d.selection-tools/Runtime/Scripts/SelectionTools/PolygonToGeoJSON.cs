db58473e3c64394fa6479eff259165501df445a6
bozmir <bozmir@hotmail.com>
2025-04-01 13:35:24 +0200

    removed unity

--- Packages/eu.netherlands3d.selection-tools/Runtime/Scripts/SelectionTools/PolygonToGeoJSON.cs
+++ Packages/eu.netherlands3d.selection-tools/Runtime/Scripts/SelectionTools/PolygonToGeoJSON.cs
@@ -24,8 +24,8 @@ public class PolygonToGeoJSON : MonoBehaviour
         for (int i = 0; i < polygon.Count; i++)
         {
             var position = polygon[i];
-            var coordinate = new Coordinate(CoordinateSystem.Unity, position.x, position.y, position.z);
-            var rdCoordinate = CoordinateConverter.ConvertTo(coordinate, CoordinateSystem.WGS84).ToVector3WGS();
+            var coordinate = new Coordinate(position);
+            var rdCoordinate = coordinate.Convert(CoordinateSystem.WGS84).ToVector3WGS();
 
             stringBuilder.AppendLine($"[{rdCoordinate.lon},{rdCoordinate.lat}]");
             if (i != polygon.Count - 1) stringBuilder.Append(",");
