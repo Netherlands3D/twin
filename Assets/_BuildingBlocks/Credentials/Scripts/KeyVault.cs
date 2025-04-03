137fb62ccf2a51144e1f1e458b9db25ef9042584
Mike van Riel <me@mikevanriel.com>
2025-04-03 16:02:32 +0200

    Authentication failed when loading project with 2 datasources
    In the KeyVault, there was a coroutineRunner that was killed off the second time you tried to authenticate a service. This meant that when a project has multiple services, it will load them in rapid succession, killing of prior coroutineRunners and thus the coroutines searching for the authentication.
    
    By creating a dedicated coroutineRunner that is long lived, we can start multiple coroutines without fear of them vanishing :)

--- Assets/_BuildingBlocks/Credentials/Scripts/KeyVault.cs
+++ Assets/_BuildingBlocks/Credentials/Scripts/KeyVault.cs
@@ -1,15 +1,13 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.Events;
-using UnityEngine.Networking;
 using System;
 using System.Collections;
-using Netherlands3D.Web;
 using Netherlands3D.Credentials.StoredAuthorization;
-using System.Collections.Specialized;
 using KindMen.Uxios;
 using KindMen.Uxios.Errors;
 using KindMen.Uxios.Errors.Http;
+using RSG;
 
 namespace Netherlands3D.Credentials
 {
@@ -18,10 +16,10 @@ namespace Netherlands3D.Credentials
     {
         [TextArea(3, 10)] public string Description = "";
         [SerializeField] private bool log = false;
-        [SerializeField] private Dictionary<Uri, StoredAuthorization.StoredAuthorization> storedAuthorizations = new();
+        private Dictionary<Uri, StoredAuthorization.StoredAuthorization> storedAuthorizations = new();
 
-        private MonoBehaviour coroutineMonoBehaviour;
         public UnityEvent<StoredAuthorization.StoredAuthorization> OnAuthorizationTypeDetermined = new();
+        public UnityEvent<StoredAuthorization.StoredAuthorization> OnAuthorizationTypeDeterminationFailed = new();
 
         public Dictionary<string, Type> expectedAuthorizationTypes = new()
         {
@@ -53,16 +51,6 @@ namespace Netherlands3D.Credentials
             throw new InvalidOperationException($"Unsupported authorization type: {storedAuthorizationType.Name}");
         }
 
-        private StoredAuthorization.StoredAuthorization CreateStoredAuthorization<T>(Uri uri, params string[] args) where T : StoredAuthorization.StoredAuthorization
-        {
-            if (supportedAuthorizationTypes.TryGetValue(typeof(T), out var factory))
-            {
-                return factory(uri, args);
-            }
-
-            throw new InvalidOperationException($"Unsupported authorization type: {typeof(T).Name}");
-        }
-
         /// <summary>
         /// Get the stored authorization for a specific URL
         /// </summary>
@@ -77,58 +65,63 @@ namespace Netherlands3D.Credentials
                 return;
             }
 
-            //Only allow one simultaneous coroutine for now
-            if (coroutineMonoBehaviour != null)
-                Destroy(coroutineMonoBehaviour.gameObject);
-
-            //lets try to to find a valid authorization type for the given input url
-            var coroutineGameObject = new GameObject("KeyVaultCoroutine_TryParseAuthorizationInput");
-            coroutineMonoBehaviour = coroutineGameObject.AddComponent<KeyVaultCoroutines>();
-            coroutineMonoBehaviour.StartCoroutine(TryFindAuthorization(inputUri, username, passwordOrKey));
+            KeyVaultCoroutineRunner
+                .Instance()
+                .StartCoroutine(TryFindAuthorization(inputUri, username, passwordOrKey));
         }
 
         private IEnumerator TryFindAuthorization(Uri inputUri, string username, string passwordOrKey)
         {
-            bool authorizationSuccessful = false;
             Uri domain = new Uri(inputUri.GetLeftPart(UriPartial.Path));
 
-            //1. Try to find credentials in the url, and if found, we verify that this is a valid Auth method
+            // 1. Try to find credentials in the url, and if found, we verify that this is a valid Auth method
             if (TryToFindAuthorizationInUriQuery(inputUri, out var potentialAuthorisation))
             {
-                if (log) Debug.Log("found potential query key type in url: " + potentialAuthorisation.GetType() + " with key " + potentialAuthorisation.QueryKeyValue);
+                if (log) Debug.Log(
+                    $"Found potential query key type in url: {potentialAuthorisation.GetType()} with key {potentialAuthorisation.QueryKeyValue}"
+                );
+                
+                var request = TrySupportedAuthorization(potentialAuthorisation, inputUri);
+                
+                // Needed because this is all coroutines at the moment
+                yield return Uxios.WaitForRequest(request);
 
-                //try this one
-                yield return TrySupportedAuthorization(potentialAuthorisation, inputUri);
                 if (storedAuthorizations.ContainsKey(domain)) yield break; // if the Auth test was succesful, stop looking.
             }
 
-            //2. In case we know the type for this base Uri, try that first
+            // 2. In case we know the type for this base Uri, try that first
             if (expectedAuthorizationTypes.TryGetValue(domain.Host, out var expectedType))
             {
-                if (expectedType != typeof(Public) && string.IsNullOrEmpty(passwordOrKey)) //it's not public, so we need some kind of authorization. if the passwordOrKey is empty, we already know it will fail. Maybe expand this in the future with a more robust check
+                //it's not public, so we need some kind of authorization. if the passwordOrKey is empty, we already know it will fail. Maybe expand this in the future with a more robust check
+                if (expectedType != typeof(Public) && string.IsNullOrEmpty(passwordOrKey))
                 {
                     OnAuthorizationTypeDetermined.Invoke(new FailedOrUnsupported(inputUri));
                     yield break;
                 }
 
-                var potentialAuth = CreateStoredAuthorization(expectedType, inputUri, passwordOrKey, username); // pass usernameOrKey before username for constructor order
-                yield return TrySupportedAuthorization(potentialAuth, inputUri);
-                if (storedAuthorizations.ContainsKey(domain)) yield break; // if the Auth test was succesful, stop looking.
+                // pass usernameOrKey before username for constructor order
+                var potentialAuth = CreateStoredAuthorization(expectedType, inputUri, passwordOrKey, username);
+                var request = TrySupportedAuthorization(potentialAuth, inputUri);
+                
+                // Needed because this is all coroutines at the moment
+                yield return Uxios.WaitForRequest(request);
+
+                // if the Auth test was succesful, stop looking.
+                if (storedAuthorizations.ContainsKey(domain)) yield break; 
             }
 
-            //3. Try all supported authorization types
+            // 3. Try all supported authorization types
             yield return TryAllSupportedAuthorizations(inputUri, username, passwordOrKey);
             if (storedAuthorizations.ContainsKey(domain)) yield break; // if the Auth test was succesful, stop looking.
 
-            //4. nothing worked, this url either has invalid credentials, or we don't support the credential type. We will not store this in the storedAuthorizations, so the user can retry
+            // 4. nothing worked, this url either has invalid credentials, or we don't support the credential type. We will not store this in the storedAuthorizations, so the user can retry
             if (log) Debug.Log("This url either has invalid credentials, or we don't support the credential type: " + inputUri);
             OnAuthorizationTypeDetermined.Invoke(new FailedOrUnsupported(inputUri));
         }
 
         private bool TryToFindAuthorizationInUriQuery(Uri uri, out QueryStringAuthorization potentialAuthorisation)
         {
-            var queryParameters = new NameValueCollection();
-            uri.TryParseQueryString(queryParameters);
+            var queryParameters = QueryString.Decode(uri.Query);
 
             foreach (var supportedAuthTypes in supportedAuthorizationTypes)
             {
@@ -153,83 +146,79 @@ namespace Netherlands3D.Credentials
         {
             foreach (var supportedAuthorization in supportedAuthorizationTypes.Keys)
             {
-                var potentialAuth = CreateStoredAuthorization(supportedAuthorization, uri, passwordOrKey, username); // pass usernameOrKey before username for constructor order
-                yield return TrySupportedAuthorization(potentialAuth, uri);
-                if (storedAuthorizations.ContainsKey(potentialAuth.Domain)) yield break; // if the Auth test was succesful, stop looking.
+                // pass usernameOrKey before username for constructor order
+                var potentialAuth = CreateStoredAuthorization(
+                    supportedAuthorization, uri, passwordOrKey, username
+                ); 
+
+                var promisedAuthorization = TrySupportedAuthorization(potentialAuth, uri) as Promise<IResponse>;
+
+                yield return Uxios.WaitForRequest(promisedAuthorization);
+                if (promisedAuthorization == null)
+                {
+                    throw new Exception("Expected promisedAuthorization to be casted to Promise<StoredAuthorization.StoredAuthorization>, but got null");
+                }
+
+                // if the Auth test was successful, stop looking.
+                if (promisedAuthorization.CurState == PromiseState.Resolved) yield break;
             }
         }
 
-        private IEnumerator TrySupportedAuthorization(StoredAuthorization.StoredAuthorization potentialAuth, Uri uri)
-        {
-            var config = Config.Default();
-            config = potentialAuth.AddToConfig(config);
-            var request = Uxios.DefaultInstance.Get<byte[]>(uri, config); //get as byte[] so Uxios doesn't try to interpret the response
-
-            bool isAuthorized = false;
-
-            request.Then(response => { isAuthorized = true; });
-            request.Catch(exception => { isAuthorized = CheckErrorAuthentication(exception); });
-
-            yield return Uxios.WaitForRequest(request);
-
-            if (isAuthorized)
+        private IPromise<IResponse> TrySupportedAuthorization(
+            StoredAuthorization.StoredAuthorization potentialAuth, 
+            Uri uri
+        ) {
+            void OnAccepted(IResponse _)
             {
-                if (log) Debug.Log("Access granted with authorization type: " + potentialAuth.GetType() + " for: " + uri);
+                if (log) Debug.Log($"Access GRANTED for {uri} with authorization type: {potentialAuth.GetType()}");
                 NewAuthorizationDetermined(potentialAuth);
             }
-            else
+
+            void OnError(Exception exception)
             {
-                if (log) Debug.Log("Access denied with authorization type: " + potentialAuth.GetType() + " for: " + uri);
-                //todo: send an event here so UI can update
+                if (CheckErrorAuthentication(exception))
+                {
+                    OnAccepted((exception as Error)?.Response);
+                    return;
+                }
+
+                if (log) Debug.Log($"Access DENIED for {uri} with authorization type: {potentialAuth.GetType()}");
+                OnAuthorizationTypeDeterminationFailed.Invoke(potentialAuth);
             }
+
+            var config = potentialAuth.AddToConfig(Config.Default());
+            var request = Uxios.DefaultInstance.Get<byte[]>(uri, config);
+            request.Then(OnAccepted);
+            request.Catch(OnError);
+
+            return request;
         }
 
         private static bool CheckErrorAuthentication(Exception exception)
         {
-            switch (exception)
+            return exception switch
             {
-                case AuthenticationError:
-                    return false;
-                case HttpClientError:
-                    return true;
-                case HttpServerError error:
-                    throw new Exception("the request returned a response that is not implemented: " + error.Status + " from Uri: " + error.Config.Url);
-                default:
-                    throw new Exception("the request returned an connection or data processing error: " + exception.Message + "from Uri: " + ((Error)exception).Config.Url);
-            }
+                AuthenticationError => false,
+                HttpClientError => true,
+                HttpServerError error => throw new Exception(
+                    $"the request returned a response that is not implemented: {error.Status} from Uri: {error.Config.Url}"
+                ),
+                _ => throw new Exception(
+                    $"the request returned an connection or data processing error: {exception.Message} from Uri: {((Error)exception).Config.Url}"
+                )
+            };
         }
 
         /// <summary>
-        /// Add a new known URL with a specific authorization type
+        /// Add a new known URL with a specific authorization type.
+        ///
+        /// Called when a authorization attempt was successful and stores it for future easy access via its baseUri
         /// </summary>
-        private void NewAuthorizationDetermined(StoredAuthorization.StoredAuthorization auth) //called when a authorization attempt was successful and stores it for future easy access via its baseUri
+        private void NewAuthorizationDetermined(StoredAuthorization.StoredAuthorization auth)
         {
             storedAuthorizations.Remove(auth.Domain);
             storedAuthorizations.Add(auth.Domain, auth);
             OnAuthorizationTypeDetermined.Invoke(auth);
         }
-
-        private static bool IsAuthorized(UnityWebRequest uwr)
-        {
-            if (uwr.result == UnityWebRequest.Result.Success)
-                return true;
-
-            if (uwr.responseCode == 401 || uwr.responseCode == 403)
-                return false;
-
-            if (uwr.result != UnityWebRequest.Result.ProtocolError)
-                throw new Exception("the request returned an connection or data processing error: " + uwr.responseCode + "from Uri: " + uwr.uri);
-
-            if (uwr.responseCode >= 500)
-                throw new Exception("the request returned a response that is not implemented: " + uwr.responseCode + " from Uri: " + uwr.uri);
-
-            // We kinda assume that anything below error code 500 -except for 401 and 403- would probably be OK since the server
-            // has processed the request and deemed it to contain a client side error
-            return true;
-        }
-    }
-
-    public class KeyVaultCoroutines : MonoBehaviour
-    {
     }
 }
