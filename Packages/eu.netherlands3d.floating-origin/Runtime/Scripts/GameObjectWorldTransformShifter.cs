82fa677afd3c636818ebd70eda508deb0f2babb3
Tom Simons <tom.v.simons@gmail.com>
2025-04-07 16:28:01 +0200

    put pre shift code back to test if build is still broken

--- Packages/eu.netherlands3d.floating-origin/Runtime/Scripts/GameObjectWorldTransformShifter.cs
+++ Packages/eu.netherlands3d.floating-origin/Runtime/Scripts/GameObjectWorldTransformShifter.cs
@@ -15,6 +15,8 @@ namespace Netherlands3D.Twin.FloatingOrigin
         
         public override void PrepareToShift(WorldTransform worldTransform, Coordinate fromOrigin, Coordinate toOrigin)
         {
+            // Always update the coordinate based on the Unity transform before shifting to make sure the coordinate is up to date.
+            UpdateCoordinateBasedOnUnityTransform();
         }
 
         public override void ShiftTo(WorldTransform worldTransform, Coordinate fromOrigin, Coordinate toOrigin)
