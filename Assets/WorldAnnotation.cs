4c30ae18cf9380ee360adf9619c536a1d7689b41
Tom Simons <tom.v.simons@gmail.com>
2025-04-11 15:18:54 +0200

    fixed rename

--- Assets/WorldAnnotation.cs
+++ Assets/WorldAnnotation.cs
@@ -1,44 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using GG.Extensions;
 using Netherlands3D.Twin.FloatingOrigin;
 using Netherlands3D.Twin.Layers;
+using Netherlands3D.Twin.Layers.Properties;
 using Netherlands3D.Twin.UI;
 using UnityEngine;
 
 namespace Netherlands3D
 {
     [RequireComponent(typeof(LayerGameObject))]
-    public class Annotation : MonoBehaviour
+    public class WorldAnnotation : MonoBehaviour, ILayerWithPropertyData
     {
         [SerializeField] private TextPopout popoutPrefab;
         private TextPopout annotation;
-        private string annotationText = "Test";
         private WorldTransform worldTransform;
+        private AnnotationPropertyData annotationPropertyData;
+
+        LayerPropertyData ILayerWithPropertyData.PropertyData => annotationPropertyData;
 
         private void Awake()
         {
             worldTransform = GetComponent<WorldTransform>();
+            annotationPropertyData = new AnnotationPropertyData();
+            CreateTextPopup();
+            annotationPropertyData.OnDataChanged.AddListener(UpdateAnnotation);
         }
 
-        private void Start()
+        private void CreateTextPopup()
         {
             var canvasTransform = FindAnyObjectByType<Canvas>();
-            annotation = CreateTextPopout(canvasTransform.transform, PivotPresets.BottomCenter);
-            annotation.Show("", worldTransform.Coordinate, true);
+
+            annotation = Instantiate(popoutPrefab, canvasTransform.transform);
+            annotation.RectTransform().SetPivot(PivotPresets.BottomCenter);
+            annotation.transform.SetSiblingIndex(0);
+            annotation.Show(annotationPropertyData.Data, worldTransform.Coordinate, true);
             annotation.ReadOnly = false;
+            annotation.OnEndEdit.AddListener(UpdateProjectData);
         }
 
-        private TextPopout CreateTextPopout(Transform canvasTransform, PivotPresets pivotPoint)
+        private void OnDestroy()
         {
-            var popout = Instantiate(popoutPrefab, canvasTransform);
-            popout.RectTransform().SetPivot(pivotPoint);
-            popout.transform.SetSiblingIndex(0);
+            annotation.OnEndEdit.AddListener(UpdateProjectData);
+            annotationPropertyData.OnDataChanged.RemoveListener(UpdateAnnotation);
+        }
 
-            return popout;
+        private void UpdateProjectData(string annotationText)
+        {
+            annotationPropertyData.Data = annotationText;
         }
 
         private void Update()
         {
             annotation.StickTo(worldTransform.Coordinate);
+        }
+
+        public void LoadProperties(List<LayerPropertyData> properties)
+        {
+            var annotationProperty = (AnnotationPropertyData)properties.FirstOrDefault(p => p is AnnotationPropertyData);
+            if (annotationProperty != null)
+            {
+                if (annotationPropertyData != null) //unsubscribe events from previous property object, resubscribe to new object at the end of this if block
+                {
+                    annotationPropertyData.OnDataChanged.RemoveListener(UpdateAnnotation);
+                }
+
+                this.annotationPropertyData = annotationProperty; //take existing TransformProperty to overwrite the unlinked one of this class
+
+                UpdateAnnotation(this.annotationPropertyData.Data);
+
+                annotationPropertyData.OnDataChanged.AddListener(UpdateAnnotation);
+            }
+        }
+
+        private void UpdateAnnotation(string newText)
+        {
+            annotation.SetTextWithoutNotify(newText);
         }
     }
 }
