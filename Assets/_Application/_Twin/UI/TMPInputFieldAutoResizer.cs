8e0c2a28793a19aedf9a0eb959c4a96872149fca
Tom Simons <tom.v.simons@gmail.com>
2025-04-11 14:28:29 +0200

    fixed newline calculation

--- Assets/_Application/_Twin/UI/TMPInputFieldAutoResizer.cs
+++ Assets/_Application/_Twin/UI/TMPInputFieldAutoResizer.cs
@@ -29,7 +29,25 @@ namespace Netherlands3D
         private void Resize(RectTransform rectTransform)
         {
             var textComponent = inputField.textComponent;
-            Vector2 preferred = textComponent.GetPreferredValues(inputField.text);
+            Vector2 preferred = Vector2.zero;
+            if (string.IsNullOrEmpty(inputField.text))
+            {
+                var placeholderText = inputField.placeholder as TMP_Text;
+                if (placeholderText)
+                    preferred = placeholderText.GetPreferredValues(placeholderText.text);
+            }
+            else
+            {
+                // textComponent.ForceMeshUpdate();
+                preferred = textComponent.GetPreferredValues(inputField.text);
+                
+                // Ensure the height accounts for the number of lines including newLines
+                int lineCount = textComponent.textInfo.lineCount;
+                float lineHeight = textComponent.textInfo.lineInfo[0].lineHeight + textComponent.lineSpacing + textComponent.lineSpacingAdjustment;
+                preferred.y = lineCount * lineHeight;
+            }
+
+
             rectTransform.sizeDelta = preferred + padding;
         }
     }
