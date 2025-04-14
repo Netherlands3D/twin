0ff58a787cdc07c5922c1545bc048b33f81ade04
Tom Simons <tom.v.simons@gmail.com>
2025-04-14 14:53:49 +0200

    added camera position prefab

---
+++ Assets/_Application/Layers/LayerTypes/HierarchicalObject/CameraPositionLayerGameObject.cs
@@ -0,0 +1,13 @@
+using Netherlands3D.Twin.FloatingOrigin;
+using UnityEngine;
+
+namespace Netherlands3D.Twin.Layers.LayerTypes.HierarchicalObject
+{
+    public class CameraPositionLayerGameObject : HierarchicalObjectLayerGameObject
+    {
+        public override void OnSelect()
+        {
+            //do not set transform handles
+        }
+    }
+}
