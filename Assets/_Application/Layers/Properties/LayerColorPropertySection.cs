736424dedebf4f579069312f0d32ae660c0d80d8
bozmir <bozmir@hotmail.com>
2025-04-14 16:49:35 +0200

    work in progress creating the propertysection

---
+++ Assets/_Application/Layers/Properties/LayerColorPropertySection.cs
@@ -0,0 +1,39 @@
+using Netherlands3D.LayerStyles;
+using Netherlands3D.Twin.UI.ColorPicker;
+using UnityEngine;
+
+namespace Netherlands3D.Twin.Layers.Properties
+{
+    public class LayerColorPropertySection : PropertySectionWithLayerGameObject
+    {
+        private LayerGameObject layer;
+
+        
+
+        public override LayerGameObject LayerGameObject
+        {
+            get => layer;
+            set
+            {
+                layer = value;
+                //colorPicker.SetColorWithoutNotify(layer.LayerData.DefaultStyle.AnyFeature.Symbolizer.GetFillColor() ?? defaultColor);
+            }
+        }
+
+        private void OnEnable()
+        {
+            //colorPicker.colorChanged.AddListener(OnPickedColor);
+        }
+
+        private void OnDisable()
+        {
+            //colorPicker.colorChanged.RemoveListener(OnPickedColor);
+        }
+
+        public void OnPickedColor(Color color)
+        {
+            layer.LayerData.DefaultStyle.AnyFeature.Symbolizer.SetFillColor(color);
+            layer.ApplyStyling();
+        }
+    }
+}
