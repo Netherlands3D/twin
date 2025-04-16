c7384398999743e71c048e382e4ddb9567e1707e
Tom Simons <tom.v.simons@gmail.com>
2025-04-16 10:10:04 +0200

    fixed annotation transform not saving correctly

--- Assets/_Application/Layers/LayerTypes/HierarchicalObject/Properties/AnnotationPropertyData.cs
+++ Assets/_Application/Layers/LayerTypes/HierarchicalObject/Properties/AnnotationPropertyData.cs
@@ -1,33 +1,37 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using Netherlands3D.Coordinates;
+using Netherlands3D.Twin.Layers.LayerTypes.HierarchicalObject.Properties;
 using Newtonsoft.Json;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace Netherlands3D.Twin.Layers.Properties
 {
     [DataContract(Namespace = "https://netherlands3d.eu/schemas/projects/layers/properties", Name = "Annotation")]
-    public class AnnotationPropertyData : LayerPropertyData
+    public class AnnotationPropertyData : TransformLayerPropertyData
     {
         [DataMember] private string annotationText;
 
-        [JsonIgnore] public readonly UnityEvent<string> OnDataChanged = new();
+        [JsonIgnore] public readonly UnityEvent<string> OnAnnotationTextChanged = new();
 
         [JsonIgnore]
-        public string Data
+        public string AnnotationText
         {
             get => annotationText;
             set
             {
                 annotationText = value;
-                OnDataChanged.Invoke(value);
+                OnAnnotationTextChanged.Invoke(value);
             }
         }
         
         [JsonConstructor]
-        public AnnotationPropertyData(string annotationText)
+        public AnnotationPropertyData(Coordinate position, Vector3 eulerRotation, Vector3 localScale, string annotationText) : base(position, eulerRotation, localScale)
         {
             this.annotationText = annotationText;
         }
+
     }
 }
