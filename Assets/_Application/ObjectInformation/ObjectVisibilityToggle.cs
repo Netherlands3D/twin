15f3bbc03666cc06304de7178fec18ec31fc03dc
Tom Simons <tom.v.simons@gmail.com>
2025-05-01 10:55:45 +0200

    added first draft of visibility toggle UI

---
+++ Assets/_Application/ObjectInformation/ObjectVisibilityToggle.cs
@@ -0,0 +1,11 @@
+using Netherlands3D.Functionalities.ObjectInformation;
+using UnityEngine;
+
+namespace Netherlands3D
+{
+    public class ObjectVisibilityToggle : MonoBehaviour
+    {
+        private ObjectSelector selector;
+        
+    }
+}
