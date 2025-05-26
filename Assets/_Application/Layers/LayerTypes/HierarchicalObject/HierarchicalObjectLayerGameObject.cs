7cfe334e7dc3ca3f3321df785c8f10daef709f68
Mike van Riel <me@mikevanriel.com>
2025-05-26 10:54:25 +0200

    Remove merge artefact

--- Assets/_Application/Layers/LayerTypes/HierarchicalObject/HierarchicalObjectLayerGameObject.cs
+++ Assets/_Application/Layers/LayerTypes/HierarchicalObject/HierarchicalObjectLayerGameObject.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Netherlands3D.Coordinates;
-using Netherlands3D.LayerStyles;
-using Netherlands3D.LayerStyles.ExtensionMethods;
 using Netherlands3D.Services;
 using Netherlands3D.Twin.FloatingOrigin;
 using Netherlands3D.Twin.Layers.LayerTypes.HierarchicalObject.Properties;
