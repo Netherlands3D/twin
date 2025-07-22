c9d99a689f70f8294b6421df18b2af7585bbe011
Mike van Riel <me@mikevanriel.com>
2025-07-22 11:49:02 +0200

    Initial setup for the new OGC API package, including tests against at least two instances

---
+++ Packages/eu.netherlands3d.ogc-api/Tests/Runtime/Fixtures/DigilabDemoAPI.cs
@@ -0,0 +1,27 @@
+﻿namespace Netherlands3D.OgcApi.Tests.Fixtures
+{
+    public record DigilabDemoAPI : OgcApiFixture
+    {
+        public DigilabDemoAPI()
+        {
+            BaseUrl = "https://digilab.geocat.live/ogcapi-records";
+            Title = "Digilab Demo Catalogus";
+            Description = null;
+            Catalogues = new[]
+            {
+                new OgcApiCatalogueFixture
+                {
+                    Id = "03a71d9b-78d5-4014-940b-287ae4c99fc7",
+                    Url = "https://digilab.geocat.live/ogcapi-records/collections/03a71d9b-78d5-4014-940b-287ae4c99fc7?f=application%2Fjson",
+                    Title = "Digilab Demo Catalogus"
+                },
+                new OgcApiCatalogueFixture
+                {
+                    Id = "2211dada-a830-47f0-9762-5e5c370d315a",
+                    Url = "https://digilab.geocat.live/ogcapi-records/collections/2211dada-a830-47f0-9762-5e5c370d315a?f=application%2Fjson",
+                    Title = "Clearly Rotterdam 3D"
+                }
+            };
+        }
+    }
+}
---
+++ Packages/eu.netherlands3d.ogc-api/Tests/Runtime/Fixtures/PyGeoAPI.cs
@@ -0,0 +1,21 @@
+﻿namespace Netherlands3D.OgcApi.Tests.Fixtures
+{
+    public record PyGeoAPI : OgcApiFixture
+    {
+        public PyGeoAPI()
+        {
+            BaseUrl = "https://demo.pygeoapi.io/master";
+            Title = "pygeoapi Demo instance - running latest GitHub version";
+            Description = "pygeoapi provides an API to geospatial data";
+            Catalogues = new[]
+            {
+                new OgcApiCatalogueFixture
+                {
+                    Id = "dutch-metadata",
+                    Url = "https://demo.pygeoapi.io/master/collections/dutch-metadata?f=json",
+                    Title = "Sample metadata records from Dutch Nationaal georegister"
+                }
+            };
+        }
+    }
+}
