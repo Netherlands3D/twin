f037ca317a39644df3cf6df37382195c9e1d5623
Mike van Riel <me@mikevanriel.com>
2025-07-23 13:09:20 +0200

    Add additional handling when expressions are null, or when they are left-overs from the old implementation

--- Packages/eu.netherlands3d.serializable-gis-expressions/Runtime/Scripts/ExpressionConverter.cs
+++ Packages/eu.netherlands3d.serializable-gis-expressions/Runtime/Scripts/ExpressionConverter.cs
@@ -15,14 +15,20 @@ namespace Netherlands3D.SerializableGisExpressions
             bool hasExistingValue, JsonSerializer serializer)
         {
             var token = JToken.Load(reader);
-            if (token.Type == JTokenType.Boolean)
+            switch (token.Type)
             {
-                return Expression.Boolean(((JValue)token).Value);
-            }
-            
-            if (token.Type != JTokenType.Array)
-            {
-                throw new JsonSerializationException($"Expected JSON array for Expression, got {token.Type}");
+                case JTokenType.Boolean:
+                    return Expression.Boolean(((JValue)token).Value);
+                case JTokenType.Null:
+                    // if the token type is a null, we accept that as a "*", or better said: always true. The default
+                    // styling rule is an example of a rule that always matches.
+                    return Expression.Boolean(true);
+                case JTokenType.Object:
+                    return BackwardsCompatibilityWithOldBooleanExpression(token);
+                case JTokenType.Array:
+                    break;
+                default:
+                    throw new JsonSerializationException($"Expected JSON array for Expression, got {token.Type}");
             }
 
             var arr = (JArray)token;
@@ -50,6 +56,23 @@ namespace Netherlands3D.SerializableGisExpressions
             }
 
             return new Expression(opCode, operands);
+        }
+
+        private static Expression BackwardsCompatibilityWithOldBooleanExpression(JToken token)
+        {
+            // In the first version of the expression library, the boolean expression was represented as an object;
+            // some projects remain that use this notation. Since we moved on from that implementation quickly,
+            // no other type was in use other than the default boolean expression.
+            var tokenObject = (JObject)token;
+            tokenObject.TryGetValue("$type", out var typeToken);
+            if (typeToken?.Value<string>() !=
+                "https://netherlands3d.eu/schemas/projects/layers/styling/expressions/Bool")
+            {
+                return Expression.Boolean(false);
+            }
+                
+            tokenObject.TryGetValue("value", out var valueToken);
+            return Expression.Boolean(valueToken?.Value<bool>() ?? false);
         }
 
         public override void WriteJson(JsonWriter writer, Expression value, JsonSerializer serializer)
