9dc7dac135d248a7a759097759ef395fa286bd19
Tom Simons <tom.v.simons@gmail.com>
2025-07-24 10:16:03 +0200

    Revert "Revert "Merge branch 'main' into feature/settings-icon-van-een-laag-aanpassen""
    This reverts commit dd326f43f704d0b7d9644e466dc8e283c6bc296e.

---
+++ Packages/eu.netherlands3d.serializable-gis-expressions/Runtime/Scripts/ExpressionConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine.Scripting;
+
+namespace Netherlands3D.SerializableGisExpressions
+{
+    /// <summary>
+    /// Custom JsonConverter to read/write Expression structs directly from JSON arrays.
+    /// </summary>
+    [Preserve]
+    public class ExpressionConverter : JsonConverter<Expression>
+    {
+        public override Expression ReadJson(JsonReader reader, Type objectType, Expression existingValue,
+            bool hasExistingValue, JsonSerializer serializer)
+        {
+            var token = JToken.Load(reader);
+            if (token.Type == JTokenType.Boolean)
+            {
+                return Expression.Boolean(((JValue)token).Value);
+            }
+            
+            if (token.Type != JTokenType.Array)
+            {
+                throw new JsonSerializationException($"Expected JSON array for Expression, got {token.Type}");
+            }
+
+            var arr = (JArray)token;
+            if (arr.Count < 1)
+            {
+                throw new JsonSerializationException("Expression array must have at least one element (the operator)");
+            }
+
+            // Parse operator via EnumMember
+            var opCode = arr[0].ToObject<Expression.Operators>(serializer);
+
+            // Prepare operands
+            var operands = new object[arr.Count - 1];
+            for (int i = 1; i < arr.Count; i++)
+            {
+                var element = arr[i];
+                if (element.Type == JTokenType.Array)
+                {
+                    operands[i - 1] = element.ToObject<Expression>(serializer);
+                }
+                else
+                {
+                    operands[i - 1] = ((JValue)element).Value;
+                }
+            }
+
+            return new Expression(opCode, operands);
+        }
+
+        public override void WriteJson(JsonWriter writer, Expression value, JsonSerializer serializer)
+        {
+            writer.WriteStartArray();
+            // Write operator via EnumMember
+            serializer.Serialize(writer, value.Operator);
+
+            // Write operands
+            foreach (var operand in value.Operands)
+            {
+                if (operand is Expression expr)
+                    serializer.Serialize(writer, expr);
+                else
+                    serializer.Serialize(writer, operand);
+            }
+
+            writer.WriteEndArray();
+        }
+    }
+}
