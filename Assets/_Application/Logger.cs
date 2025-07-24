695bdfb3ecb4ebdce5a63db8a45cb7b5144e5de5
Mike van Riel <me@mikevanriel.com>
2025-07-24 11:26:02 +0200

    Introduce basic application-wide logger that overrides unity's default logging.
    The application-wide logging can be disabled so that Unity's logger is no longer called. In a future version we can refine this by checking the context to disable logging for specific parts of the app or by limiting logging on a per-logtype basis, or prepending logs with a timestamp, etc etc

---
+++ Assets/_Application/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Netherlands3D.Twin
+{
+    public class Logger : MonoBehaviour, ILogHandler 
+    {
+        private ILogHandler decoratedLogHandler;
+        [field: SerializeField] public bool IsEnabled { get; private set; } = true;
+
+        private void Awake()
+        {
+            this.decoratedLogHandler = Debug.unityLogger.logHandler; 
+            Debug.unityLogger.logHandler = this;
+        }
+
+        public void LogFormat(LogType logType, Object context, string format, params object[] args)
+        {
+            if (IsEnabled) decoratedLogHandler.LogFormat(logType, context, format, args);
+        }
+
+        public void LogException(Exception exception, Object context)
+        {
+            if (IsEnabled) decoratedLogHandler.LogException(exception, context);
+        }
+    }
+}
