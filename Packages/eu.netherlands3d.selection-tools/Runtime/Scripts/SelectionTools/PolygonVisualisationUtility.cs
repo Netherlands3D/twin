98ad26f6edae800cf130b000f03a19804d115a4b
bozmi <bozmi@OPGGAMES>
2025-07-30 15:02:09 +0200

    imported nettopology suite

--- Packages/eu.netherlands3d.selection-tools/Runtime/Scripts/SelectionTools/PolygonVisualisationUtility.cs
+++ Packages/eu.netherlands3d.selection-tools/Runtime/Scripts/SelectionTools/PolygonVisualisationUtility.cs
@@ -1,10 +1,41 @@
+using NetTopologySuite;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Geometries.Implementation;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 namespace Netherlands3D.SelectionTools
 {
     public static class PolygonVisualisationUtility
     {
+        private static NtsGeometryServices instance;
+        private static GeometryFactory geometryFactory;
+       
+
+        private static void SetUpInstance()
+        {
+            if (instance != null)
+                return;
+
+            instance = new NtsGeometryServices(
+            // default CoordinateSequenceFactory
+            CoordinateArraySequenceFactory.Instance,
+            // default precision model
+            new PrecisionModel(1000d),
+            // default SRID
+            4326,
+            /********************************************************************
+             * Note: the following arguments are only valid for NTS >= v2.2
+             ********************************************************************/
+            // Geometry overlay operation function set to use (Legacy or NG)
+            GeometryOverlay.NG,
+            // Coordinate equality comparer to use (CoordinateEqualityComparer or PerOrdinateEqualityComparer)
+            new CoordinateEqualityComparer());
+
+            geometryFactory = instance.CreateGeometryFactory();
+        }
+
         #region UnityComponents
         //Treat first contour as outer contour, and extra contours as holes
         public static PolygonVisualisation CreateAndReturnPolygonObject(List<List<Vector3>> contours,
@@ -53,40 +84,73 @@ namespace Netherlands3D.SelectionTools
             if (contours.Count == 0)
                 return null;
 
-            var polygon = new Poly2Mesh.Polygon();
-            var outerContour = (List<Vector3>)contours[0];
+            SetUpInstance();
 
-            if (outerContour.Count < 3)
-                return null;
 
-            polygon.outside = outerContour;
+            // Convert the first ring to the shell
+            var shellCoords = ConvertToCoordinateArray(contours[0]);
+            var shell = geometryFactory.CreateLinearRing(shellCoords);
 
-            for (int i = 0; i < polygon.outside.Count; i++)
-            {
-                polygon.outside[i] = new Vector3(polygon.outside[i].x, polygon.outside[i].y, polygon.outside[i].z);
-            }
-
+            // Convert remaining rings to holes
+            LinearRing[] holes = null;
             if (contours.Count > 1)
             {
+                holes = new LinearRing[contours.Count - 1];
                 for (int i = 1; i < contours.Count; i++)
                 {
-                    var holeContour = (List<Vector3>)contours[i];
-                    PolygonCalculator.FixSequentialDoubles(holeContour); //todo: put this function in this class or is it a usefull calculation tool in general?
-
-                    if (holeContour.Count > 2)
-                    {
-                        polygon.holes.Add(holeContour);
-                    }
+                    holes[i - 1] = geometryFactory.CreateLinearRing(ConvertToCoordinateArray(contours[i]));
                 }
             }
-            var newPolygonMesh = Poly2Mesh.CreateMesh(polygon, extrusionHeight, addBottom);
-            if (newPolygonMesh)
-            {
-                newPolygonMesh.RecalculateNormals();
-                SetUVCoordinates(newPolygonMesh, uvCoordinate);
-            }
-            return newPolygonMesh;
+
+            Polygon polygon = geometryFactory.CreatePolygon(shell, holes);
+            return null;
+
+            //var polygon = new Poly2Mesh.Polygon();
+            //var outerContour = (List<Vector3>)contours[0];
+
+            //if (outerContour.Count < 3)
+            //    return null;
+
+            //polygon.outside = outerContour;
+
+            //for (int i = 0; i < polygon.outside.Count; i++)
+            //{
+            //    polygon.outside[i] = new Vector3(polygon.outside[i].x, polygon.outside[i].y, polygon.outside[i].z);
+            //}
+
+            //if (contours.Count > 1)
+            //{
+            //    for (int i = 1; i < contours.Count; i++)
+            //    {
+            //        var holeContour = (List<Vector3>)contours[i];
+            //        PolygonCalculator.FixSequentialDoubles(holeContour); //todo: put this function in this class or is it a usefull calculation tool in general?
+
+            //        if (holeContour.Count > 2)
+            //        {
+            //            polygon.holes.Add(holeContour);
+            //        }
+            //    }
+            //}
+            //var newPolygonMesh = Poly2Mesh.CreateMesh(polygon, extrusionHeight, addBottom);
+            //if (newPolygonMesh)
+            //{
+            //    newPolygonMesh.RecalculateNormals();
+            //    SetUVCoordinates(newPolygonMesh, uvCoordinate);
+            //}
+            //return newPolygonMesh;
         }
+
+        private static Coordinate[] ConvertToCoordinateArray(List<Vector3> points)
+        {
+            var coords = new Coordinate[points.Count + 1];
+            for (int i = 0; i < points.Count; i++)
+            {
+                coords[i] = new Coordinate(points[i].x, points[i].z); // use .y if working in 2D
+            }
+            coords[points.Count] = coords[0]; // close the ring
+            return coords;
+        }
+
         #endregion
 
         #region PolygonLine
