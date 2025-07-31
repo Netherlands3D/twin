6c096d34a24b4cd7d0eb973d7feb620978a4b72a
bozmir <bozmir@hotmail.com>
2025-07-31 15:44:00 +0200

    fixing also the version, and refactor for efficiency

--- Assets/_Functionalities/WFS/Scripts/WFSGeoJSONImportAdapter.cs
+++ Assets/_Functionalities/WFS/Scripts/WFSGeoJSONImportAdapter.cs
@@ -9,6 +9,8 @@ using Netherlands3D.Twin.Layers.LayerTypes;
 using Netherlands3D.Twin.Layers.Properties;
 using Netherlands3D.Twin.Projects;
 using System.Linq;
+using System.Collections.Generic;
+using KindMen.Uxios.Http;
 
 namespace Netherlands3D.Functionalities.Wfs
 {
@@ -103,34 +105,26 @@ namespace Netherlands3D.Functionalities.Wfs
             if (isWfsGetFeature)
             {
                 var queryParameters = QueryString.Decode(sourceUrl);
-                var paramName = WfsGetCapabilities.ParameterNameOfTypeNameBasedOnVersion(wfsVersion);
-                string featureType = queryParameters
-                    .Where(entry => string.Equals(entry.Key, paramName, StringComparison.OrdinalIgnoreCase))
-                    .Select(entry =>
-                    {
-                        string val = entry.Value.ToString();
-                        int equalsIndex = val.IndexOf('=');
-                        string extracted = equalsIndex >= 0 && equalsIndex < val.Length - 1
-                            ? val.Substring(equalsIndex + 1)
-                            : val;
-
-                        return extracted;
-                    })
-                    .SingleOrDefault();
-
-                if (string.IsNullOrEmpty(featureType) == false)
+                var paramName = WfsGetCapabilities.ParameterNameOfTypeNameBasedOnVersion(OgcWebServicesUtility.GetVersionFromUrl(url));
+                string featureType = null;
+                string crs = null;
+                foreach (KeyValuePair<string, QueryParameter> kv in queryParameters)
                 {
-                    string crs = queryParameters
-                        .Where(entry => string.Equals(entry.Key, "srsname", StringComparison.OrdinalIgnoreCase))
-                        .Select(entry =>
-                        {
-                            string val = entry.Value.ToString();
-                            int equalsIndex = val.IndexOf('=');
-                            return equalsIndex >= 0 && equalsIndex < val.Length - 1
-                                ? val.Substring(equalsIndex + 1)
-                                : val;
-                        })
-                        .SingleOrDefault();
+                    if(string.Equals(kv.Key, paramName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        string val = kv.Value.ToString();
+                        int equalsIndex = val.IndexOf('=');
+                        featureType = equalsIndex >= 0 && equalsIndex < val.Length - 1 ? val.Substring(equalsIndex + 1) : val;                        
+                    }
+                    if(string.Equals(kv.Key, "srsname", StringComparison.OrdinalIgnoreCase))
+                    {
+                        string val = kv.Value.ToString();
+                        int equalsIndex = val.IndexOf('=');
+                        crs = equalsIndex >= 0 && equalsIndex < val.Length - 1 ? val.Substring(equalsIndex + 1) : val;
+                    }
+                }
+                if (string.IsNullOrEmpty(featureType) == false)
+                {                     
                     // Can't deduct a human-readable title at the moment, we should add that we always query for the
                     // capabilities; this also helps with things like outputFormat and CRS
                     AddWFSLayer(featureType, sourceUrl, crs, wfsFolder, featureType, geoJsonOutputFormatString);
