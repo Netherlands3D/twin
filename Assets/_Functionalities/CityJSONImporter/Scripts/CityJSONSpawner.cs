f2d0fc66741e991e022a5409978a92714bad4dfb
Tom Simons <tom.v.simons@gmail.com>
2025-08-18 14:37:46 +0200

    CityJSONSpawner now imports through the T3D cityjson pacakge

--- Assets/_Functionalities/CityJSONImporter/Scripts/CityJSONSpawner.cs
+++ Assets/_Functionalities/CityJSONImporter/Scripts/CityJSONSpawner.cs
@@ -1,11 +1,12 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Netherlands3D.Coordinates;
-using Netherlands3D.Tiles3D;
+using Netherlands3D.T3DPipeline;
+using Netherlands3D.Twin.Cameras;
 using Netherlands3D.Twin.Layers.LayerTypes.HierarchicalObject;
+using Netherlands3D.Twin.Layers.LayerTypes.HierarchicalObject.Properties;
 using Netherlands3D.Twin.Layers.Properties;
 using Netherlands3D.Twin.Projects;
 using Netherlands3D.Twin.Utility;
@@ -15,8 +16,9 @@ namespace Netherlands3D.Functionalities.CityJSON
 {
     public class CityJSONSpawner : MonoBehaviour, ILayerWithPropertyData
     {
+        [SerializeField] private float cameraDistanceFromGeoReferencedObject = 150f;
         private CityJSONPropertyData propertyData = new();
-        public LayerPropertyData PropertyData { get; }
+        public LayerPropertyData PropertyData => propertyData;
         private GameObject importedObject;
 
         private void Awake()
@@ -55,41 +57,21 @@ namespace Netherlands3D.Functionalities.CityJSON
         {
             if (string.IsNullOrWhiteSpace(file) || !File.Exists(file))
             {
-                Debug.LogError("Invalid file path.");
+                Debug.LogError("Invalid file path: " + file);
                 yield break;
             }
+
+            // FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.None);
+            var json = File.ReadAllText(file);
             
-            FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.None);
-            byte[] data = new byte[fileStream.Length];
-            int totalRead = 0;
-            fileStream.Read(data, 0, data.Length);
-            fileStream.Close();
-
-            var loadTask = gltf.Load(data);
-            while (!loadTask.IsCompleted)
+            var root = new GameObject("CityJSONRoot");
+            var cityjson = root.AddComponent<T3DPipeline.CityJSON>();
+            cityjson.ParseCityJSON(json);
+            foreach (var cityObject in root.GetComponentsInChildren<CityObject>())
             {
-                yield return null;
+                cityObject.gameObject.AddComponent<CityObjectVisualizer>();
             }
 
-            if (!loadTask.Result)
-            {
-                Debug.LogError("Failed to load GLB file.");
-                yield break;
-            }
-
-            var root = new GameObject("GLBRoot");
-            var instantiateTask = gltf.InstantiateMainSceneAsync(root.transform);
-            while (!instantiateTask.IsCompleted)
-            {
-                yield return null;
-            }
-
-            if (!instantiateTask.Result)
-            {
-                Debug.LogError("Failed to instantiate GLB.");
-                yield break;
-            }
-            root.transform.Rotate(Vector3.up, 180); // compensate for 180 degree rotation
             OnObjImported(root);
         }
 
@@ -105,12 +87,12 @@ namespace Netherlands3D.Functionalities.CityJSON
                 var referencePosition = returnedGameObject.transform.GetChild(0).localPosition;
                 if (IsValidRD(referencePosition.x, referencePosition.y, referencePosition.z, out var origin))
                 {
-                    PositionGeoReferencedGlb(returnedGameObject, holgo, origin);
+                    PositionGeoReferencedCityJson(returnedGameObject, holgo, origin);
                 }
             }
             
             if(!isGeoReferenced)
-                PositionNonGeoReferencedGlb(returnedGameObject, holgo);
+                PositionNonGeoReferencedCityJson(returnedGameObject, holgo);
 
             importedObject = returnedGameObject;
             foreach (var meshFilter in returnedGameObject.GetComponentsInChildren<MeshFilter>())
@@ -122,14 +104,64 @@ namespace Netherlands3D.Functionalities.CityJSON
             holgo.ApplyStyling();
         }
         
-        private void PositionNonGeoReferencedGlb(GameObject returnedGameObject, HierarchicalObjectLayerGameObject holgo)
+        //todo: this function should move to the Coordinates package
+        private bool IsValidRD(double x, double y, double z, out Coordinate rdOrigin)
         {
-            throw new NotImplementedException();
+            if (EPSG7415.IsValid(new Vector3RD(x, z, y)))
+            {
+                rdOrigin = new Coordinate(CoordinateSystem.RDNAP, x, z, 0); //don't offset the height
+                return true;
+            }
+
+            if (EPSG7415.IsValid(new Vector3RD(x, y, z)))
+            {
+                rdOrigin = new Coordinate(CoordinateSystem.RDNAP, x, y, 0); //don't offset the height
+                return true;
+            }
+
+            rdOrigin = new Coordinate();
+            return false;
+        }
+        
+        private void PositionNonGeoReferencedCityJson(GameObject returnedGameObject, HierarchicalObjectLayerGameObject holgo)
+        {
+            //if we have saved transform data, we will use that position, otherwise we will use this object's current position.
+            if (holgo.TransformIsSetFromProperty)
+            {
+                //apply any transformation if present in the data
+                var transformPropterty = (TransformLayerPropertyData)((ILayerWithPropertyData)holgo).PropertyData;
+                transform.position = transformPropterty.Position.ToUnity();
+                returnedGameObject.transform.SetParent(transform, false); // imported object should move to saved (parent's) position
+            }
+            else
+            {
+                //no transform property or georeference present, this object should just take on the parent's position
+                returnedGameObject.transform.SetParent(transform, false); // imported object should move to saved (parent's) position
+            }
         }        
         
-        private void PositionGeoReferencedGlb(GameObject returnedGameObject, HierarchicalObjectLayerGameObject holgo, Coordinate origin)
+        private void PositionGeoReferencedCityJson(GameObject returnedGameObject, HierarchicalObjectLayerGameObject holgo, Coordinate origin)
         {
-            throw new NotImplementedException();
+            if (!holgo.TransformIsSetFromProperty) //move the camera only if this is is a user imported object, not if this is a project import. We know this because a project import has its Transform property set.
+            {
+                var cameraMover = Camera.main.GetComponent<MoveCameraToCoordinate>();
+                cameraMover.LookAtTarget(origin, cameraDistanceFromGeoReferencedObject); //move the camera to the georeferenced position, this also shifts the origin if needed.
+            }
+            
+            holgo.WorldTransform.MoveToCoordinate(origin); //set this object to the georeferenced position, since this is the correct position.
+            returnedGameObject.transform.SetParent(transform, false); // we set the parent and reset its localPosition, since the origin might have changed.
+            returnedGameObject.transform.localPosition = Vector3.zero;
+            foreach (Transform t in returnedGameObject.transform)
+            {
+                t.localPosition -= origin.ToUnity();
+            }
+
+            // imported object should stay where it is initially, and only then apply any user transformations if present.
+            if (holgo.TransformIsSetFromProperty)
+            {
+                var transformPropterty = (TransformLayerPropertyData)((ILayerWithPropertyData)holgo).PropertyData;
+                holgo.WorldTransform.MoveToCoordinate(transformPropterty.Position); //apply saved user changes to position.
+            }
         }
         
         public void SetCityJSONPathInPropertyData(string fullPath)
