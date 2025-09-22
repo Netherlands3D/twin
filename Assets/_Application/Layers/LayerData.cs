4637c7fdd3d22f04e009b771cf08b95ac5d4df34
Tom Simons <tom.v.simons@gmail.com>
2025-09-22 10:11:18 +0200

    removed namespace

--- Assets/_Application/Layers/LayerData.cs
+++ Assets/_Application/Layers/LayerData.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
-using System.Windows.Forms;
 using UnityEngine;
 using UnityEngine.Events;
 
