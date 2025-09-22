b523a8c6504a88e4e440deffa622d2fb98ed090e
Mike van Riel <me@mikevanriel.com>
2025-09-22 16:59:32 +0200

    Refactored CityJSON creation to use new layers system

--- Assets/_Functionalities/CityJSONImporter/Scripts/CityJSONSpawner.cs
+++ Assets/_Functionalities/CityJSONImporter/Scripts/CityJSONSpawner.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Netherlands3D.Coordinates;
 using Netherlands3D.Twin.Cameras;
+using Netherlands3D.Twin.Layers.ExtensionMethods;
 using Netherlands3D.Twin.Layers.LayerTypes.HierarchicalObject;
 using Netherlands3D.Twin.Layers.LayerTypes.HierarchicalObject.Properties;
 using Netherlands3D.Twin.Layers.Properties;
@@ -20,15 +21,9 @@ namespace Netherlands3D.Functionalities.CityJSON
         private CityJSONPropertyData propertyData = new();
         public LayerPropertyData PropertyData => propertyData;
 
-        private void Awake()
-        {
-            gameObject.transform.position = ObjectPlacementUtility.GetSpawnPoint();
-        }
-
-
         public void LoadProperties(List<LayerPropertyData> properties)
         {
-            var propertyData = properties.OfType<CityJSONPropertyData>().FirstOrDefault();
+            var propertyData = properties.Get<CityJSONPropertyData>();
             if (propertyData == null) return;
 
             // Property data is set here, and the parsing and loading of the actual data is done
@@ -65,22 +60,20 @@ namespace Netherlands3D.Functionalities.CityJSON
 
         private void OnCityJSONImported(CityJson.Structure.CityJSON cityJson)
         {
-            var holgo = GetComponent<HierarchicalObjectLayerGameObject>();
+            var layerGameObject = GetComponent<HierarchicalObjectLayerGameObject>();
 
-            if (holgo.TransformIsSetFromProperty) //use transform property if it is set
+            if (!layerGameObject.LayerData.IsNew) //use transform property if it is set
             {
-                var transformPropterty = (TransformLayerPropertyData)((ILayerWithPropertyData)holgo).PropertyData;
-                holgo.WorldTransform.MoveToCoordinate(transformPropterty.Position);
+                var transformProperty = layerGameObject.LayerData.GetProperty<TransformLayerPropertyData>();
+                layerGameObject.WorldTransform.MoveToCoordinate(transformProperty.Position);
             }
-            else //transform property is not set, we need to set it if it is georeferenced, if not, we just keep the position it was at.
+            else if (transform.childCount > 0)
             {
-                if (transform.childCount > 0)
+                // transform property is not set, we need to set it if it is georeferenced, if not, we just keep the position it was at.
+                var referencePosition = cityJson.AbsoluteCenter;
+                if (EPSG7415.IsValid(referencePosition.x, referencePosition.y, referencePosition.z, out var origin))
                 {
-                    var referencePosition = cityJson.AbsoluteCenter;
-                    if (EPSG7415.IsValid(referencePosition.x, referencePosition.y, referencePosition.z, out var origin))
-                    {
-                        PositionGeoReferencedCityJson(holgo, origin);
-                    }
+                    PositionGeoReferencedCityJson(layerGameObject, origin);
                 }
             }
 
@@ -93,41 +86,33 @@ namespace Netherlands3D.Functionalities.CityJSON
             }
 
             // Object is loaded / replaced - trigger the application of styling
-            holgo.ApplyStyling();
+            layerGameObject.ApplyStyling();
         }
         
 
-        private void PositionGeoReferencedCityJson(HierarchicalObjectLayerGameObject holgo, Coordinate origin)
+        private void PositionGeoReferencedCityJson(HierarchicalObjectLayerGameObject layerGameObject, Coordinate origin)
         {
-            if (!holgo.TransformIsSetFromProperty) //move the camera only if this is is a user imported object, not if this is a project import. We know this because a project import has its Transform property set.
+            if (layerGameObject.LayerData.IsNew) //move the camera only if this is is a user imported object, not if this is a project import. We know this because a project import has its Transform property set.
             {
                 var cameraMover = Camera.main.GetComponent<MoveCameraToCoordinate>();
                 cameraMover.LookAtTarget(origin, cameraDistanceFromGeoReferencedObject); //move the camera to the georeferenced position, this also shifts the origin if needed.
             }
 
-            holgo.WorldTransform.MoveToCoordinate(origin); //set this object to the georeferenced position, since this is the correct position.
+            layerGameObject.WorldTransform.MoveToCoordinate(origin); //set this object to the georeferenced position, since this is the correct position.
 
             // imported object should stay where it is initially, and only then apply any user transformations if present.
-            if (holgo.TransformIsSetFromProperty)
+            if (!layerGameObject.LayerData.IsNew)
             {
-                var transformPropterty = (TransformLayerPropertyData)((ILayerWithPropertyData)holgo).PropertyData;
-                holgo.WorldTransform.MoveToCoordinate(transformPropterty.Position); //apply saved user changes to position.
+                var transformProperty = layerGameObject.LayerData.GetProperty<TransformLayerPropertyData>();
+                layerGameObject.WorldTransform.MoveToCoordinate(transformProperty.Position); //apply saved user changes to position.
             }
-        }
-
-        public void SetCityJSONPathInPropertyData(string fullPath)
-        {
-            var propertyData = PropertyData as CityJSONPropertyData;
-            propertyData.CityJsonFile = AssetUriFactory.CreateProjectAssetUri(fullPath);
         }
 
         private string GetCityJsonPathFromPropertyData()
         {
-            if (propertyData.CityJsonFile == null)
-                return "";
+            if (propertyData.CityJsonFile == null) return "";
 
-            var localPath = AssetUriFactory.GetLocalPath(propertyData.CityJsonFile);
-            return localPath;
+            return AssetUriFactory.GetLocalPath(propertyData.CityJsonFile);
         }
     }
 }
