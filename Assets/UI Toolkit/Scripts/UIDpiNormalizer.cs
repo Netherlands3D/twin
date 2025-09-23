2274b90476bdccb09bc1391e9c4e0a53eaac1321
Mike van Riel <me@mikevanriel.com>
2025-09-23 10:30:38 +0200

    Attempt X to make sure sizing and DPI works in builds

---
+++ Assets/UI Toolkit/Scripts/UIDpiNormalizer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Netherlands3D.UI
+{
+    public class UiDpiNormalizer : MonoBehaviour
+    {
+        [SerializeField] private UIDocument doc;
+        [SerializeField] private float targetDpi = 72f; // what you want it to look like
+        [SerializeField] private float fallbackDpi = 96f; // used when Screen.dpi <= 0
+
+        private void Awake()
+        {
+            var ps = doc.panelSettings;
+            ps.scaleMode = PanelScaleMode.ConstantPixelSize;
+            ps.referenceDpi = fallbackDpi;
+            ps.fallbackDpi = fallbackDpi;
+
+            var reported = Screen.dpi > 0 ? Screen.dpi : fallbackDpi;
+            ps.scale = targetDpi / reported; // e.g., Win(96) => 0.75, Mac(72) => 1.0
+        }
+    }
+}
