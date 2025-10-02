5b5c4af086b4b36fdde86f827ae53c1a7bdcd3f6
Tom Simons <tom.v.simons@gmail.com>
2025-10-02 13:41:01 +0200

    cleanup

--- Assets/_Application/Rendering/BatchedMeshInstanceRenderer2.cs
+++ Assets/_Application/Rendering/BatchedMeshInstanceRenderer2.cs
@@ -37,7 +37,7 @@ namespace Netherlands3D.Twin.Rendering
         }
     }
 
-    public class BatchedMeshInstanceRenderer2 : MonoBehaviour
+    public abstract class BatchedMeshInstanceRenderer2 : MonoBehaviour
     {
         [Tooltip("The mesh to use for the points/joints")] [SerializeField]
         private Mesh pointMesh;
@@ -117,6 +117,7 @@ namespace Netherlands3D.Twin.Rendering
         }
 #endif
 
+        protected abstract void GenerateTransformMatrixCache(int collectionStartIndex = -1);
         protected void Start()
         {
             SetProjected(true);
@@ -168,41 +169,7 @@ namespace Netherlands3D.Twin.Rendering
                 }
             }
         }
-
-
-        protected virtual void GenerateTransformMatrixCache(int collectionStartIndex = -1)
-        {
-            var batchCount = (pointCount / 1023) + 1; //x batches of 1023 + 1 for the remainder
-
-            if (collectionStartIndex < 0) //reset cache completely
-            {
-                pointTransformMatrixCache = new List<List<Matrix4x4>>(batchCount);
-                collectionStartIndex = 0;
-            }
-
-            pointTransformMatrixCache.Capacity = batchCount;
-
-            var flattenedStartIndex = GetFlattenedStartIndex(collectionStartIndex);
-            var matrixIndices = GetMatrixIndices(flattenedStartIndex); //each point in the line is a joint
-
-            for (var i = collectionStartIndex; i < positionCollections.Count; i++)
-            {
-                var collection = positionCollections[i];
-                for (int j = 0; j < collection.Count; j++)
-                {
-                    var currentPoint = collection[j].ToUnity();
-
-                    // Flatten the Y axis if needed
-                    currentPoint.y = (FlattenY ? 0 : currentPoint.y) + offsetY;
-
-                    // Create the joint using a sphere aligned with the cylinder (with matching faces for smooth transition between the two)
-                    var scale = new Vector3(PointMeshScale, PointMeshScale, PointMeshScale);
-                    Matrix4x4 jointTransformMatrix = Matrix4x4.TRS(currentPoint, Quaternion.identity, scale); //todo: add serialized rotation?
-                    AppendMatrixToBatches(pointTransformMatrixCache, ref matrixIndices.batchIndex, ref matrixIndices.matrixIndex, jointTransformMatrix);
-                }
-            }
-        }
-
+        
         protected int GetFlattenedStartIndex(int collectionStartIndex)
         {
             if (collectionStartIndex < 0 || collectionStartIndex >= positionCollections.Count)
--- Assets/_Application/Rendering/PointRenderer3D.cs
+++ Assets/_Application/Rendering/PointRenderer3D.cs
@@ -1,7 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Netherlands3D.Twin.Rendering
 {
     public class PointRenderer3D : BatchedMeshInstanceRenderer2
     {
-        
+        protected override void GenerateTransformMatrixCache(int collectionStartIndex = -1)
+        {
+            var batchCount = (pointCount / 1023) + 1; //x batches of 1023 + 1 for the remainder
+
+            if (collectionStartIndex < 0) //reset cache completely
+            {
+                pointTransformMatrixCache = new List<List<Matrix4x4>>(batchCount);
+                collectionStartIndex = 0;
+            }
+
+            pointTransformMatrixCache.Capacity = batchCount;
+
+            var flattenedStartIndex = GetFlattenedStartIndex(collectionStartIndex);
+            var matrixIndices = GetMatrixIndices(flattenedStartIndex); //each point in the line is a joint
+
+            for (var i = collectionStartIndex; i < positionCollections.Count; i++)
+            {
+                var collection = positionCollections[i];
+                for (int j = 0; j < collection.Count; j++)
+                {
+                    var currentPoint = collection[j].ToUnity();
+
+                    // Flatten the Y axis if needed
+                    currentPoint.y = (FlattenY ? 0 : currentPoint.y) + offsetY;
+
+                    // Create the joint using a sphere aligned with the cylinder (with matching faces for smooth transition between the two)
+                    var scale = new Vector3(PointMeshScale, PointMeshScale, PointMeshScale);
+                    Matrix4x4 jointTransformMatrix = Matrix4x4.TRS(currentPoint, Quaternion.identity, scale); //todo: add serialized rotation?
+                    AppendMatrixToBatches(pointTransformMatrixCache, ref matrixIndices.batchIndex, ref matrixIndices.matrixIndex, jointTransformMatrix);
+                }
+            }
+        }
     }
 }
