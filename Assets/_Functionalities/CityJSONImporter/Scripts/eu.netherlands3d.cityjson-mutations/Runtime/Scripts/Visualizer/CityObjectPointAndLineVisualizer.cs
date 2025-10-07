efbebfd45e3ca01a0afb28ca700512415bb60571
Tom Simons <tom.v.simons@gmail.com>
2025-10-07 11:33:27 +0200

    Changed CityJSONPointVisualizer to also support MultiLineStrings with a LineRenderer3D

---
+++ Assets/_Functionalities/CityJSONImporter/Scripts/eu.netherlands3d.cityjson-mutations/Runtime/Scripts/Visualizer/CityObjectPointAndLineVisualizer.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using Netherlands3D.CityJson.Structure;
+using Netherlands3D.Coordinates;
+using Netherlands3D.Twin.Rendering;
+using UnityEngine;
+
+namespace Netherlands3D.CityJson.Visualisation
+{
+    [RequireComponent(typeof(CityObject))]
+    [RequireComponent(typeof(BatchedMeshInstanceRenderer))]
+    public class CityObjectPointAndLineVisualizer : CityObjectVisualizer
+    {
+        [SerializeField] private GeometryType geometryType;
+        [SerializeField] private BatchedMeshInstanceRenderer batchedMeshInstanceRenderer;
+        [SerializeField] private CityMaterialConverter materialConverter;
+
+        protected override void Visualize()
+        {
+            materialConverter.Initialize(cityObject.Appearance);
+            foreach (var geometry in cityObject.Geometries)
+            {
+                if (geometry.Type != geometryType)
+                    continue; // other types have their own visualizer and don't create meshes
+
+                var collections = GetPositionCollections(geometry.BoundaryObject, cityObject.CoordinateSystem);
+                batchedMeshInstanceRenderer.SetPositionCollections(collections);
+            }
+        }
+
+        private static List<List<Coordinate>> GetPositionCollections(CityBoundary boundary, CoordinateSystem coordinateSystem)
+        {
+            List<List<Coordinate>> result = new List<List<Coordinate>>();
+            switch (boundary)
+            {
+                case CityMultiPoint multiPoint:
+                    var coordinates = ToCoordinates(multiPoint.Points, coordinateSystem);
+                    result.Add(coordinates);
+                    break;
+                case CityMultiLineString multiLineString:
+                    foreach (var line in multiLineString.LineStrings)
+                    {
+                        coordinates = ToCoordinates(line, coordinateSystem);
+                        result.Add(coordinates);
+                    }
+                    break;
+                default:
+                    throw new NotSupportedException("Boundary of type " + boundary.GetType() + " is not supported by this visualizer");
+            }
+
+            return result;
+        }
+
+        private static List<Coordinate> ToCoordinates(CityPolygon polygon, CoordinateSystem coordinateSystem)
+        {
+            var coordinates = new List<Coordinate>(polygon.Count);
+            foreach (var vert in polygon.Vertices)
+            {
+                var coord = new Coordinate(coordinateSystem, vert.x, vert.y, vert.z);
+                coordinates.Add(coord);
+            }
+
+            return coordinates;
+        }
+    }
+}
