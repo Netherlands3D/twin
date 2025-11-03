0efcca8944722f8545bc5995e97766e67d4080fb
Jim Dekker <jimcornee@live.nl>
2025-11-03 14:59:21 +0100

    FPV modus visualisation on modus switch

---
+++ Assets/_Functionalities/FirstPersonViewer/Scripts/UI/FPV MovementModiUI.cs
@@ -0,0 +1,47 @@
+using Netherlands3D.FirstPersonViewer;
+using Netherlands3D.FirstPersonViewer.ViewModus;
+using Netherlands3D.Services;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace Netherlands3D
+{
+    public class FPVMovementModiUI : MonoBehaviour
+    {
+        [SerializeField] private Image currentMovemodeImage;
+
+        [SerializeField] private float fadeInLength;
+        [SerializeField] private float intervalLength;
+        [SerializeField] private float fadeOutLength;
+
+        private MovementModusSwitcher switcher;
+
+        private Sequence uiSequence;
+
+        private void OnEnable()
+        {
+            switcher = ServiceLocator.GetService<MovementModusSwitcher>();
+            if (switcher != null) switcher.OnMovementPresetChanged += SwitchModeUI;
+        }
+
+        private void OnDisable()
+        {
+            if (switcher != null) switcher.OnMovementPresetChanged -= SwitchModeUI;
+        }
+
+        private void SwitchModeUI(ViewerState state)
+        {
+            currentMovemodeImage.sprite = state.viewIcon;
+
+            uiSequence?.Kill();
+            uiSequence = DOTween.Sequence();
+            uiSequence.AppendCallback(() => currentMovemodeImage.color = new Color(1,1,1,0));
+            uiSequence.Append(currentMovemodeImage.DOFade(0.48f, fadeInLength).SetEase(Ease.OutSine));
+            uiSequence.AppendInterval(intervalLength);
+            uiSequence.Append(currentMovemodeImage.DOFade(0, fadeOutLength).SetEase(Ease.InSine));
+        }
+
+
+    }
+}
