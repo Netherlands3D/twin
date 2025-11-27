472ef6ddd7be1046a39acf4b3fd658a92019f70a
Jim Dekker <jimcornee@live.nl>
2025-11-27 15:40:40 +0100

    FPV Settings window ready

---
+++ Assets/_Functionalities/FirstPersonViewer/Scripts/UI/FPVSettingsUI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Netherlands3D.FirstPersonViewer.UI
+{
+    public class FPVSettingsUI : MonoBehaviour
+    {
+        [SerializeField] private Slider slider;
+        [SerializeField] private Toggle useMouseLock;
+
+
+    }
+}
