835ad0e60b6f86f3a8f115a4bdb9bcf0058464a4
Wesley de Graaf <double.gezicht@gmail.com>
2025-12-01 15:13:41 +0100

    Fixed 2 nullrefs in First Person Viewer (when entering the main viewer)

--- Assets/_Functionalities/FirstPersonViewer/Scripts/UI/FPVMouseUnlockAnimation.cs
+++ Assets/_Functionalities/FirstPersonViewer/Scripts/UI/FPVMouseUnlockAnimation.cs
@@ -18,15 +18,20 @@ namespace Netherlands3D.FirstPersonViewer.UI
 
         private void OnEnable()
         {
-            firstPersonInput = ServiceLocator.GetService<FirstPersonViewer>().Input;
-            firstPersonInput.OnLockStateChanged += PlayUnlockCircleAnimation;
+            FirstPersonViewer fpv = ServiceLocator.GetService<FirstPersonViewer>();
+            if (fpv != null)
+            {
+                firstPersonInput = fpv.Input;
+                firstPersonInput.OnLockStateChanged += PlayUnlockCircleAnimation;
+            }
+
             unlockCircleImage = GetComponent<Image>();
             unlockCircleImageTransform = GetComponent<Transform>();
         }
 
         private void OnDisable()
         {
-            firstPersonInput.OnLockStateChanged -= PlayUnlockCircleAnimation;
+            if(firstPersonInput != null) firstPersonInput.OnLockStateChanged -= PlayUnlockCircleAnimation;
         }
 
         void Update()
