c895055f572e4d3ff0d1de01fd94b54872cf7470
Tom Simons <tom.v.simons@gmail.com>
2025-12-17 17:09:33 +0100

    removed unneeded pragma

--- Assets/_Application/Layers/LayerTypes/Polygons/ObjectScatterLayerGameObject.cs
+++ Assets/_Application/Layers/LayerTypes/Polygons/ObjectScatterLayerGameObject.cs
@@ -15,10 +15,6 @@ using Netherlands3D.Twin.Samplers;
 using Netherlands3D.Twin.Utility;
 using UnityEngine;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 namespace Netherlands3D.Twin.Layers.LayerTypes.Polygons
 {
     public enum FillType
