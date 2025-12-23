17b719a02054b8903993e0f7f7ad85eb4e73c3ee
Wesley de Graaf <double.gezicht@gmail.com>
2025-12-23 10:51:16 +0100

    Use old fpv locking & Changed settings window

--- Assets/_Functionalities/FirstPersonViewer/Scripts/UI/FPVSettingsUI.cs
+++ Assets/_Functionalities/FirstPersonViewer/Scripts/UI/FPVSettingsUI.cs
@@ -7,7 +7,6 @@ namespace Netherlands3D.FirstPersonViewer.UI
 {
     public class FPVSettingsUI : MonoBehaviour
     {
-        [SerializeField] private Toggle mouseLockingToggle;
         [SerializeField] private Slider sensitivitySlider;
 
         private void OnEnable()
@@ -19,17 +18,11 @@ namespace Netherlands3D.FirstPersonViewer.UI
             //Invoke this so the text display will update.
             sensitivitySlider.onValueChanged?.Invoke(currentSensitivity);
 
-            mouseLockingToggle.SetValue(firstPersonViewer.Input.GetMouseLockModus());
         }
 
         public void OnSensitivityChanged(float sensitivity)
         {
             ServiceLocator.GetService<FirstPersonViewer>().FirstPersonCamera.SetSensitivity(sensitivity / 100);
-        }
-
-        public void OnMouseLockModeChanged(bool useMouseLocking)
-        {
-            ServiceLocator.GetService<FirstPersonViewer>().Input.SetMouseLockModus(useMouseLocking);
         }
     }
 }
