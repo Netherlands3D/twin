5e52491e6a90c7487b17bb88e1124b91aa619d94
Tom Simons <tom.v.simons@gmail.com>
2025-12-23 15:27:47 +0100

    added 3 colors of lights, fixed spacing

--- Assets/Special2025/SpawnLights.cs
+++ Assets/Special2025/SpawnLights.cs
@@ -7,17 +7,19 @@ namespace Netherlands3D
     public class SpawnLights : MonoBehaviour
     {
         private const float GOLDEN_ANGLE = 137.50776405f;
-        [SerializeField] private GameObject lightPrefab;
+        [SerializeField] private LightColor lightPrefab;
         public Transform cylinder;
 
         [SerializeField] private float lightDensity = 10f;
         [SerializeField] private float surfaceOffset = 0.02f;
-  
+        [SerializeField] private Material[] lightMaterials;
+        
         private IEnumerator Start()
         {
             yield return null; //wait for the length to be set
             GenerateLights();
         }
+        
 
         public void GenerateLights()
         {
@@ -29,9 +31,10 @@ namespace Netherlands3D
             float length = cylinder.localScale.y;
 
             // int lightCount = Mathf.RoundToInt(length *1.2f* lightDensity);
-            int lightCount = 1;
-            float spacing = 1f / lightDensity;
+            int lightCount = 3;
+            // float spacing = 1f / lightDensity;
 
+            float spacing = length / (lightCount - 1);
             float start = -length * 0.5f;
 
             for (int i = 0; i < lightCount; i++)
@@ -45,16 +48,24 @@ namespace Netherlands3D
                 float x = Mathf.Cos(angle) * (radius + surfaceOffset);
                 float z = Mathf.Sin(angle) * (radius + surfaceOffset);
 
-                Vector3 localPos = new Vector3(x, y, z);
+                Vector3 localPos = new Vector3(x, z, y);
                 Vector3 worldPos = cylinder.TransformPoint(localPos);
 
-                GameObject light = Instantiate(lightPrefab, worldPos, Quaternion.identity, transform);
-
+                var light = Instantiate(lightPrefab, worldPos, Quaternion.identity, transform);
+                SetLightMaterial(i, light);
+                
                 Vector3 localNormal = new Vector3(x, 0f, z).normalized;
                 Vector3 worldNormal = cylinder.TransformDirection(localNormal);
 
                 light.transform.rotation = Quaternion.LookRotation(worldNormal, cylinder.up) * Quaternion.Euler(90, 0, 0);;
             }
+        }
+
+        private void SetLightMaterial(int i, LightColor lightColor)
+        {
+            var index = i%lightMaterials.Length;
+            var mat = lightMaterials[index];
+            lightColor.SetMaterial(mat);
         }
 
         private void ClearChildren()
