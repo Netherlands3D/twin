2865e04a680a0f12d143a05643b844164b394481
Tom Simons <tom.v.simons@gmail.com>
2026-01-29 15:44:48 +0100

    Added button to convert selected polygon feature into layer(s)

---
+++ Assets/_Functionalities/WFS/Scripts/PolygonFeatureToLayer.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Netherlands3D.Coordinates;
+using Netherlands3D.Functionalities.ObjectInformation;
+using Netherlands3D.Twin;
+using Netherlands3D.Twin.Layers;
+using Netherlands3D.Twin.Layers.LayerTypes.GeoJsonLayers;
+using Netherlands3D.Twin.Layers.LayerTypes.Polygons;
+using Netherlands3D.Twin.Layers.LayerTypes.Polygons.Properties;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Netherlands3D
+{
+    public class PolygonFeatureToLayer : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        private ObjectSelectorService objectSelector;
+        private Feature feature;
+
+        private void Awake()
+        {
+            objectSelector = FindAnyObjectByType<ObjectSelectorService>();
+        }
+
+        private void OnEnable()
+        {
+            objectSelector.SelectFeature.AddListener(OnFeatureSelected);
+            objectSelector.OnDeselect.AddListener(OnFeatureDeselected);
+            button.onClick.AddListener(ConvertToLayer);
+        }
+
+        private void ConvertToLayer()
+        {
+            if (feature == null)
+            {
+                Debug.LogError("Feature mapping not set, cannot convert anything to layer.");
+                return;
+            }
+
+            // if (feature.Geometry is not MultiPolygon multiPolygon && feature.Geometry is not Polygon polygon)
+            // {
+            //     return;
+            // }
+            if (feature.Geometry is MultiPolygon multiPolygon)
+            {
+                foreach (var polygon in multiPolygon.Coordinates)
+                {
+                    CreatePolygonLayer(polygon);
+                }
+            }
+            else if (feature.Geometry is Polygon polygon)
+            {
+                CreatePolygonLayer(polygon);
+            }
+        }
+
+        private void CreatePolygonLayer(Polygon polygon)
+        {
+            var solidPolygon = polygon.Coordinates[0];
+            var list = GeometryVisualizationFactory.ConvertToUnityCoordinates(solidPolygon, GeoJSONParser.GetCoordinateSystem(feature.CRS));
+            
+            var polygonPropertyData = new PolygonSelectionLayerPropertyData();
+            polygonPropertyData.OriginalPolygon = list;
+            
+            var preset = new PolygonLayerPreset.Args(
+                "GeoJSONFeature",
+                ShapeType.Polygon,
+                list
+            );
+            App.Layers.Add(preset);
+        }
+
+        private void OnDisable()
+        {
+            objectSelector.SelectFeature.RemoveListener(OnFeatureSelected);
+            objectSelector.OnDeselect.RemoveListener(OnFeatureDeselected);
+            button.onClick.RemoveListener(ConvertToLayer);
+        }
+
+        private void OnFeatureSelected(FeatureMapping featureMapping)
+        {
+            button.gameObject.SetActive(true);
+            feature = featureMapping.Feature;
+        }
+
+        private void OnFeatureDeselected()
+        {
+            button.gameObject.SetActive(false);
+            feature = null;
+        }
+    }
+}
