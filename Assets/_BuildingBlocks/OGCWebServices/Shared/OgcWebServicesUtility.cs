02cd31d01be2b33db24ea6ee628306fd3d3fefe4
Mike van Riel <mike.vanriel@flevoland.nl>
2026-02-02 13:54:42 +0100

    Normalize all query parameters for a WMS url.
    Query parameters in HTTP are case-sensitive, but the OWS/WMS spec makes them case-insensitive. In our code we assumed they are always lowercase, but this need not be the case.
    
    These changes introduce URL normalisation once when importing by making all query parameter keys lowercase. Values should remain at their intended case, as layer and style identifications are case sensitive.
    
    This change included unit tests to check the correctness of the action.

--- Assets/_BuildingBlocks/OGCWebServices/Shared/OgcWebServicesUtility.cs
+++ Assets/_BuildingBlocks/OGCWebServices/Shared/OgcWebServicesUtility.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using KindMen.Uxios;
 
 namespace Netherlands3D.OgcWebServices.Shared
@@ -10,6 +11,69 @@ namespace Netherlands3D.OgcWebServices.Shared
             var uri = new Uri(url);
             var baseUrl = uri.GetLeftPart(UriPartial.Path);
             return $"{baseUrl}?request=GetCapabilities&service={serviceType.ToString().ToUpper()}";
+        }
+        
+        /// <summary>
+        /// Makes all query parameters' keys lowercase. Values cannot be made lowercase, as the capitalisation of the
+        /// values _does_ matter!
+        ///
+        /// Even though the HTTP specification states that keys in query parameters are case-sensitive and you normally
+        /// do not want to change the case of these, the OWS/WMS/WFS specification states that query parameter keys
+        /// should be treated case-insensitive.
+        ///
+        /// This means that any form of capitalisation could be received from the end-user, making it hard to identify
+        /// or replace values by their keys.
+        ///
+        /// This method will make all query parameter keys lowercase, ensuring a predictable case for all further
+        /// operations.
+        ///
+        /// Should a key exist multiple times in different capitalisations - they are merged as one according to the
+        /// HTTP spec's rules. Thus "SERVICE=wms&service=wfs" becomes "service=wms&service=wfs".
+        /// </summary>
+        /// <param name="sourceUrl">The URL to normalize</param>
+        /// <returns>A Uri object with the normalized URL.</returns>
+        public static Uri NormalizeUrl(string sourceUrl)
+        {
+            var urlBuilder = new UriBuilder(sourceUrl);
+            
+            // Decode the query parameters, ensuring that characters are properly decoded as well.
+            var queryParameters = QueryString.Decode(urlBuilder.Query);
+            
+            // Build a hashset of all keys that are not lowercase - this will prevent exceptions because you cannot 
+            // modify a dictionary while iterating it.
+            HashSet<string> parametersToBeReplaced = new HashSet<string>();
+            foreach (var queryParameter in queryParameters)
+            {
+                if (queryParameter.Key == queryParameter.Key.ToLower()) continue;
+                parametersToBeReplaced.Add(queryParameter.Key);
+            }
+
+            // Iterate through all non-lowercase keys and replace them with a lowercase key.
+            foreach (var queryParameterKey in parametersToBeReplaced)
+            {
+                var value = queryParameters[queryParameterKey];
+                queryParameters.Remove(queryParameterKey);
+                
+                var lowercaseKey = queryParameterKey.ToLower();
+
+                // if the lowercase version doesn't exist yet - add it, or ...
+                if (!queryParameters.TryGetValue(lowercaseKey, out var existingValue))
+                {
+                    queryParameters.Add(lowercaseKey, value);
+                    continue;
+                }
+
+                // ... when it does exist, merge the new one into it
+                foreach (var part in value.Values)
+                {
+                    existingValue.Add(part);
+                }
+            }
+            
+            // Encode it again and inject it into the given url.
+            urlBuilder.Query = QueryString.Encode(queryParameters);
+
+            return urlBuilder.Uri;
         }
 
         public static bool IsValidUrl(Uri url, RequestType requestType)
