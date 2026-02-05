6344e862cde9e8ff39787df87596bf33a30a8a00
bozmir <bozmir@hotmail.com>
2026-02-05 13:56:08 +0100

    reverted metadata

--- Packages/eu.netherlands3d.layer-styles/Runtime/Scripts/Metadata.cs
+++ Packages/eu.netherlands3d.layer-styles/Runtime/Scripts/Metadata.cs
@@ -7,7 +7,5 @@ namespace Netherlands3D.LayerStyles
     public class Metadata
     {
         [DataMember(Name = "name")] public string Name { get; set; }
-
-        [DataMember(Name = "data")] public Dictionary<string, string> Data { get; } = new();
     }
 }
