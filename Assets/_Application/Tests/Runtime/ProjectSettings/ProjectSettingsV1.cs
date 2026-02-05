73b39cbe118cde4d2ca298617ba9d688e1187fdf
martijnvangog <69241087+martijnvangog@users.noreply.github.com>
2026-02-05 09:06:52 +0100

    layerpaneltest
    test toegevoegd om te controleren of een layer in het layerpanel geplaatst is en of deze daar actief of inactief is

--- Assets/_Application/Tests/Runtime/ProjectSettings/ProjectSettingsV1.cs
+++ Assets/_Application/Tests/Runtime/ProjectSettings/ProjectSettingsV1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Diagnostics;
 using System.Numerics;
@@ -6,6 +6,7 @@ using Netherlands3D.CartesianTiles;
 using Netherlands3D.Coordinates;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.TestTools;
 using Netherlands3D.Twin.Projects;
 using System.Threading.Tasks;
@@ -13,6 +14,9 @@ using System.Runtime.CompilerServices;
 using Netherlands3D.E2ETesting.PageObjectModel;
 using Netherlands3D.E2ETesting;
 using System.IO;
+using System.Linq.Expressions;
+using System.Net.NetworkInformation;
+
 
 
 namespace Netherlands3D.Twin.Tests.Projectsettings
@@ -27,49 +31,36 @@ namespace Netherlands3D.Twin.Tests.Projectsettings
             yield return TestFunctions.TestSetup(Path.Combine(Application.persistentDataPath,Application.streamingAssetsPath,"testfiles/projectV1.nl3d"));
         }
 
+
         [UnityTest]
-        public IEnumerator testCameraPosition()
+        public IEnumerator Version1()
         {
+            yield return TestFunctions.lagenmenuOpenen();
             yield return TestFunctions.Cameraposition(new Coordinate(CoordinateSystem.RDNAP,156663.02404785156,463895.14367675781,494.97858460061252));
-        }
-        [UnityTest]
-        public IEnumerator testCameraRotation()
-        {
             yield return TestFunctions.CameraRotation(new UnityEngine.Vector3(30,0,0));
+            yield return TestFunctions.checkGameObject(TestFunctions.scene.DefaultMaaiveld, "Maaiveld is niet ingeladen");
+            yield return TestFunctions.checkGameObject(TestFunctions.scene.DefaultBuildings,"gebouwen is niet ingeladen");
+            yield return TestFunctions.checkGameObject(TestFunctions.scene.DefaultBomen,"bomen zijn niet ingeladen");
+            yield return TestFunctions.checkGameObject(TestFunctions.scene.DefaultBossen, "bossen zijn niet ingeladen");
+            yield return TestFunctions.LaagCorrectInLagenMenu("Bomen",true,true);
+            
+
         }
-        [UnityTest]
-        public IEnumerator terreinLayerIsLoaded()
-        {
-            yield return TestFunctions.checkGameObject(TestFunctions.scene.DefaultMaaiveld);
-        }  
-        [UnityTest]
-        public IEnumerator BuildingLayerIsLoaded()
-        {
-            yield return TestFunctions.checkGameObject(TestFunctions.scene.DefaultBuildings);
-        } 
-        [UnityTest]
-        public IEnumerator BomenLayerIsLoaded()
-        {
-            yield return TestFunctions.checkGameObject(TestFunctions.scene.DefaultBomen);
-        } 
-         [UnityTest]
-        public IEnumerator BossenLayerIsLoaded()
-        {
-            yield return TestFunctions.checkGameObject(TestFunctions.scene.DefaultBossen);
-        } 
+        
+
+
+
     }
 
     public class Scene : Element
     {
         public Element<GameObject> DefaultMaaiveld => E2E.Find("Functionalities/CartesianTiles/Maaiveld (Clone)");
         public Element<GameObject> DefaultBuildings => E2E.Find("Functionalities/CartesianTiles/Gebouwen (Clone)");
-
         public Element<GameObject> DefaultBomen => E2E.Find("Functionalities/CartesianTiles/Bomen (Clone)");
-
         public Element<GameObject> DefaultBossen => E2E.Find("Functionalities/CartesianTiles/Bossen (Clone)");
+        public Element<GameObject> LagenMenuButton => E2E.Find("CanvasUI/Base/Body/Sidebar & Toolbar/Toolbar/TopButtons/ToolbarButton_Layers");
+        public Element<GameObject> Layerspanel => E2E.Find("CanvasUI/Base/Body/Sidebar & Toolbar/Sidebar/Content/LayersInspector(Clone)/LayersPanel/Layers");
 
-
-         public Element<GameObject> projectdataHandler => E2E.Find("ProjectDataHandler");
     }
 
     public static class TestFunctions
@@ -114,7 +105,7 @@ namespace Netherlands3D.Twin.Tests.Projectsettings
             yield return null;
 
         }
-    public static IEnumerator TestSetup(string projectfilepath)
+        public static IEnumerator TestSetup(string projectfilepath)
         {
             yield return E2E.EnsureMainSceneIsLoaded();
             
@@ -123,7 +114,7 @@ namespace Netherlands3D.Twin.Tests.Projectsettings
             
            ProjectDataHandler pdh  =(ProjectDataHandler)E2E.Find("ProjectDataHandler").Component<ProjectDataHandler>();
            projectIsLoaded=false;
-           pdh.OnLoadCompleted.AddListener(ReCeivoProjectLoadedEvent);
+           pdh.OnLoadCompleted.AddListener(ReCeiveProjectLoadedEvent);
 
             pdh.LoadFromFile(projectfilepath);
            while( projectIsLoaded==false)
@@ -132,13 +123,36 @@ namespace Netherlands3D.Twin.Tests.Projectsettings
             }
             
         }
-        private static void ReCeivoProjectLoadedEvent()
+        private static void ReCeiveProjectLoadedEvent()
         {
             projectIsLoaded=true;   
         }
-        public static IEnumerator checkGameObject(Netherlands3D.E2ETesting.PageObjectModel.Element<UnityEngine.GameObject> testSubject)
+        public static IEnumerator checkGameObject(Netherlands3D.E2ETesting.PageObjectModel.Element<UnityEngine.GameObject> testSubject,string failMessage="")
         {
-            yield return E2E.Expect(()=>testSubject.IsActive,Is.True,20,"");
+            yield return E2E.Expect(()=>testSubject.IsActive,Is.True,20,failMessage);
+        }
+        public static IEnumerator lagenmenuOpenen()
+        {
+            UnityEngine.GameObject lagenmenuButton = (UnityEngine.GameObject)scene.LagenMenuButton;
+            UnityEngine.UI.Button button = lagenmenuButton.GetComponent<Button>();
+            button.onClick.Invoke();
+            yield return checkGameObject(scene.Layerspanel);
+            
+        }
+    
+        public static IEnumerator LaagCorrectInLagenMenu(string laagnaam,bool shouldBeInList, bool shouldBeActive=true)
+        {
+            UnityEngine.GameObject layerspanel= (UnityEngine.GameObject)scene.Layerspanel;
+            UnityEngine.GameObject laagobject = layerspanel.transform.Find(laagnaam).gameObject;
+
+            bool laagobjectAanwezig = laagobject!=null;
+            E2E.Then(laagobjectAanwezig,Is.EqualTo(shouldBeInList),laagnaam +"niet correct in lagenpaneel");
+
+            UnityEngine.GameObject enableToggleObject = laagobject.transform.Find("ParentRow/EnableToggle").gameObject;
+            UnityEngine.UI.Toggle Toggle = enableToggleObject.GetComponent<Toggle>();
+            bool ToggleValue = Toggle.isOn;
+            E2E.Then(ToggleValue,Is.EqualTo(shouldBeActive),laagnaam +" isActive niet correct in lagenpaneel");
+            yield return null;
         }
     }
 }
