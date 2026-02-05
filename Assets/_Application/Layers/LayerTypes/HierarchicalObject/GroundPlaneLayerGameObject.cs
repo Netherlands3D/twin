e99b045ed11b9183b0f5da9c4324620ec41d2ded
bozmir <bozmir@hotmail.com>
2026-02-05 09:22:49 +0100

    should not check for togglescattering

--- Assets/_Application/Layers/LayerTypes/HierarchicalObject/GroundPlaneLayerGameObject.cs
+++ Assets/_Application/Layers/LayerTypes/HierarchicalObject/GroundPlaneLayerGameObject.cs
@@ -26,5 +26,10 @@ namespace Netherlands3D.Twin.Layers.LayerTypes.HierarchicalObject
                 scaleUnitCharacter);
             InitProperty<ColorPropertyData>(properties);
         }
+        
+        public override void OnLayerDataParentChanged()
+        {
+         
+        }
     }
 }
