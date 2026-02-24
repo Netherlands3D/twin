9fb211de9e5b603734bf3ca2f32278e3c566af5a
Tom Simons <tom.v.simons@gmail.com>
2026-02-24 11:58:47 +0100

    added debug logs

--- Packages/eu.netherlands3d.tiles3d/Runtime/Scripts/TileContent/NL3DMaterialGenerator.cs
+++ Packages/eu.netherlands3d.tiles3d/Runtime/Scripts/TileContent/NL3DMaterialGenerator.cs
@@ -3,7 +3,9 @@ using GLTFast;
 using GLTFast.Materials;
 using GLTFast.Schema;
 using GLTFast.Logging;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.Rendering;
 using Material = UnityEngine.Material;
 
@@ -80,7 +82,7 @@ namespace Netherlands3D.Tiles3D
                 if (specGloss != null)
                 {
                     baseColorLinear = specGloss.DiffuseColor;
-                    var success = TrySetTexture(
+                    var success = TrySetTexture2(
                         specGloss.diffuseTexture,
                         material,
                         gltf,
@@ -101,7 +103,7 @@ namespace Netherlands3D.Tiles3D
                 if (materialType != MaterialType.SpecularGlossiness)
                 {
                     // baseColorTexture can be used by both MetallicRoughness AND Unlit materials
-                    var success = TrySetTexture(
+                    var success = TrySetTexture2(
                         gltfMaterial.PbrMetallicRoughness.BaseColorTexture,
                         material,
                         gltf,
@@ -243,6 +245,163 @@ namespace Netherlands3D.Tiles3D
             }
 
             return feature;
+        }
+        
+        private bool TrySetTexture2(
+            TextureInfoBase textureInfo,
+            UnityEngine.Material material,
+            IGltfReadable gltf,
+            int texturePropertyId,
+            int scaleTransformPropertyId = -1,
+            int rotationPropertyId = -1,
+            int uvChannelPropertyId = -1
+            )
+        {
+            Debug.Log("starting trySetTexture");
+            Debug.Log("textureInfo: " + textureInfo);
+            Debug.Log("textureInfo.Index: " + textureInfo?.index);
+
+            if (textureInfo != null && textureInfo.index >= 0)
+            {
+                int textureIndex = textureInfo.index;
+                var srcTexture = gltf.GetSourceTexture(textureIndex);
+                Debug.Log("srcTexture: " + srcTexture);
+                
+                if (srcTexture != null)
+                {
+                    var texture = gltf.GetTexture(textureIndex);
+                    Debug.Log("texture: " + texture);
+
+                    if (texture != null)
+                    {
+                        Debug.Log("setting texture at  id: " + texturePropertyId);
+                        material.SetTexture(texturePropertyId, texture);
+                        // TODO: Implement texture transform and UV channel selection for all texture types and remove
+                        // this condition
+                        if (scaleTransformPropertyId >= 0 && rotationPropertyId >= 0 && uvChannelPropertyId >= 0)
+                        {
+                            var flipY = gltf.IsTextureYFlipped(textureIndex);
+                            Debug.Log("setting texture transform. FlipY: "+ flipY);
+                            TrySetTextureTransform(
+                                textureInfo,
+                                material,
+                                texturePropertyId,
+                                scaleTransformPropertyId,
+                                rotationPropertyId,
+                                uvChannelPropertyId,
+                                flipY
+                                );
+                        }
+                        Debug.Log("succesfully set texture");
+                        return true;
+                    }
+#if UNITY_IMAGECONVERSION
+                    Logger?.Error(LogCode.TextureLoadFailed,textureIndex.ToString());
+#endif
+                    Debug.Log("Texture is null");
+                }
+                else
+                {
+                    Debug.Log("srcTexture is null");
+                    Logger?.Error(LogCode.TextureNotFound, textureIndex.ToString());
+                }
+            }
+            Debug.LogError("could not set texture");
+            return false;
+        }
+        
+        
+        void TrySetTextureTransform(
+            TextureInfoBase textureInfo,
+            UnityEngine.Material material,
+            int texturePropertyId,
+            int scaleTransformPropertyId = -1,
+            int rotationPropertyId = -1,
+            int uvChannelPropertyId = -1,
+            bool flipY = false
+            )
+        {
+            var hasTransform = false;
+            // Scale (x,y) and Transform (z,w)
+            var textureScaleTranslation = new float4(
+                1, 1,// scale
+                0, 0 // translation
+                );
+
+            var texCoord = textureInfo.texCoord;
+
+            if (textureInfo.Extensions?.KHR_texture_transform != null)
+            {
+                hasTransform = true;
+                var tt = textureInfo.Extensions.KHR_texture_transform;
+                if (tt.texCoord >= 0)
+                {
+                    texCoord = tt.texCoord;
+                }
+
+                if (tt.offset != null)
+                {
+                    textureScaleTranslation.z = tt.offset[0];
+                    textureScaleTranslation.w = 1 - tt.offset[1];
+                }
+                if (tt.scale != null)
+                {
+                    textureScaleTranslation.x = tt.scale[0];
+                    textureScaleTranslation.y = tt.scale[1];
+                }
+                if (math.abs(tt.rotation) >= float.Epsilon)
+                {
+                    var cos = math.cos(tt.rotation);
+                    var sin = math.sin(tt.rotation);
+
+                    var newRot = new Vector2(textureScaleTranslation.x * sin, textureScaleTranslation.y * -sin);
+
+                    Assert.IsTrue(rotationPropertyId >= 0, "Texture rotation property invalid!");
+                    material.SetVector(rotationPropertyId, newRot);
+
+                    textureScaleTranslation.x *= cos;
+                    textureScaleTranslation.y *= cos;
+
+                    textureScaleTranslation.z -= newRot.y; // move offset to move rotation point (horizontally)
+                }
+                else
+                {
+                    // Make sure the rotation is properly zeroed
+                    material.SetVector(rotationPropertyId, Vector4.zero);
+                }
+
+                textureScaleTranslation.w -= textureScaleTranslation.y; // move offset to move flip axis point (vertically)
+            }
+
+            if (texCoord != 0)
+            {
+                if (uvChannelPropertyId >= 0 && texCoord < 2f)
+                {
+                    material.EnableKeyword(UVChannelSelectKeyword);
+                    material.SetFloat(uvChannelPropertyId, texCoord);
+                }
+                else
+                {
+                    Logger?.Error(LogCode.UVMulti, texCoord.ToString());
+                }
+            }
+
+            if (flipY)
+            {
+                hasTransform = true;
+                textureScaleTranslation.w = 1 - textureScaleTranslation.w; // flip offset in Y
+                textureScaleTranslation.y = -textureScaleTranslation.y; // flip scale in Y
+            }
+
+            if (hasTransform)
+            {
+                material.EnableKeyword(TextureTransformKeyword);
+            }
+
+            material.SetTextureOffset(texturePropertyId, textureScaleTranslation.zw);
+            material.SetTextureScale(texturePropertyId, textureScaleTranslation.xy);
+            Assert.IsTrue(scaleTransformPropertyId >= 0, "Texture scale/transform property invalid!");
+            material.SetVector(scaleTransformPropertyId, textureScaleTranslation);
         }
     }
 }
