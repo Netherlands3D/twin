2b0ee3a8deb21c14c88bd2acd3c7f3fd6130ee3e
Tom Simons <tom.v.simons@gmail.com>
2026-03-06 13:40:51 +0100

    added scripts for Dome masking ui

---
+++ Assets/UI Toolkit/Scripts/Components/MaskLayerRowElement.cs
@@ -0,0 +1,24 @@
+﻿using Netherlands3D.UI.ExtensionMethods;
+using UnityEngine.UIElements;
+
+namespace Netherlands3D.UI.Components
+{
+    [UxmlElement]
+    public partial class MaskLayerRowElement : VisualElement
+    {
+        public Toggle MaskActiveToggle => this.Q<Toggle>("MaskActiveToggle");
+        public Label LayerNameLabel => this.Q<Label>("LayerNameLabel");
+        
+        public MaskLayerRowElement()
+        {
+            this.CloneComponentTree("Components");
+            this.AddComponentStylesheet("Components");
+        }
+
+        public MaskLayerRowElement(string name, bool isActive) : this()
+        {
+            MaskActiveToggle.value = isActive;
+            LayerNameLabel.text = name;
+        }
+    }
+}
