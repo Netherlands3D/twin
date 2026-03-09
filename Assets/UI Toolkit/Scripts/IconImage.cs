3721f7f26907dbf0d6e99e19a26cfb4552c784bf
bozmir <bozmir@hotmail.com>
2026-03-09 19:17:06 +0100

    other enum values for visibility and invisibility

--- Assets/UI Toolkit/Scripts/IconImage.cs
+++ Assets/UI Toolkit/Scripts/IconImage.cs
@@ -98,7 +98,7 @@
         VideoCamera = 18,
         Warning = 17,
         Width = 16,
-        Visibility = 95,
-        Invisible = 96,
+        Visibility = 97,
+        Invisible = 98,
     }
 }
