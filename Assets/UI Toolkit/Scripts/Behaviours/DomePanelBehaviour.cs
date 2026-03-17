b6137156769e61376111d33fe5bb6bc6e53a68ff
Tom Simons <tom.v.simons@gmail.com>
2026-03-17 12:01:43 +0100

    fixed dome panel behaviour according to new scriptable structure

--- Assets/UI Toolkit/Scripts/Behaviours/DomePanelBehaviour.cs
+++ Assets/UI Toolkit/Scripts/Behaviours/DomePanelBehaviour.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Netherlands3D.Functionalities.ObjectInformation;
-using Netherlands3D.Services;
+using Netherlands3D.Twin.Layers.Properties;
 using Netherlands3D.Twin.Projects;
 using Netherlands3D.Twin.Tools;
 using UnityEngine;
@@ -10,6 +8,7 @@ using UnityEngine.UIElements;
 
 namespace Netherlands3D.UI.Panels
 {
+    [CreateAssetMenu(fileName = "DomePanelBehaviour", menuName = "ScriptableObjects/FloatingPanelBehaviours/DomePanelBehaviour", order = 1)]
     public class DomePanelBehaviour : FloatingPanelBehaviour
     {
         [SerializeField] private Tool domeTool;
@@ -18,13 +17,23 @@ namespace Netherlands3D.UI.Panels
             var layers = ProjectData.Current.RootLayer.GetFlatHierarchy();
             return layers.ToDictionary(layer => layer.Id.ToString(), layer => (object)layer);
         }
-
-        public override VisualElement SpawnFloatingPanelContent(Dictionary<string, object> data = null)
+        
+        public override VisualElement SpawnFloatingPanelContent(FloatingPanel floatingPanel, Dictionary<string,object> data = null)
         {
-            DomePanel panel = new DomePanel(data);
-            return panel;
+            base.SpawnFloatingPanelContent(floatingPanel, data);
+            DomePanel content = new DomePanel(data);
+            return content;
         }
 
+        public override void Dispose()
+        {
+        }
+
+        public override void LoadProperties(List<LayerPropertyData> properties)
+        {
+            //Dome is not a layer so it has no properties to load. We also do not want to keep the selected layers that the dome affects between sessions.
+        }
+        
         public override bool ShouldBeActive()
         {
             return domeTool.Open;
