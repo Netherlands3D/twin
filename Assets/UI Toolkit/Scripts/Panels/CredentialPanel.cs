8395f03fbc740edef71b6664e371815d950244d3
bozmir <bozmir@hotmail.com>
2026-03-25 17:01:19 +0100

    credentialpanel next content state

--- Assets/UI Toolkit/Scripts/Panels/CredentialPanel.cs
+++ Assets/UI Toolkit/Scripts/Panels/CredentialPanel.cs
@@ -1,3 +1,4 @@
+using System;
 using Netherlands3D.UI.Components;
 using Netherlands3D.UI.ExtensionMethods;
 using UnityEngine.UIElements;
@@ -9,21 +10,76 @@ namespace Netherlands3D.UI.Panels
     [UxmlElement]
     public partial class CredentialPanel : VisualElement
     {
+        public Action OnShow;
+        public Action OnHide;
+        public Action OnConfirm;
+        
         private Button button;
         private Button Button => button ??= this.Q<Button>("RetryButton");
 
+        private VisualElement warning;
+        private VisualElement update;
+        private VisualElement Warning => warning ??= this.Q<VisualElement>("MessageTitleWarning");
+        private VisualElement Update => update ??= this.Q<VisualElement>("MessageTitleUpdate");
+
+        private enum ContentState
+        {
+            Warning,
+            Key,
+            UsernameAndPassword
+        }
+        
+        private ContentState contentState;
+        
         public CredentialPanel()
         {
             this.CloneComponentTree("Panels");
             this.AddComponentStylesheet("Panels");
-
+            
+            OnShow += () => EnableInClassList("active", true);
+            OnHide += () => EnableInClassList("active", false);
+            
+            
+            SetContentState(ContentState.Warning);
             Button.clicked += () =>
             {
-                EnableInClassList("secondary", true);
-                this.ReplacePrefixedValueInClassList("credential-panel__message-title-text", ".secondary");
+                if (contentState == ContentState.Warning)
+                    SetContentState(ContentState.Key);
+                else
+                {
+                    OnConfirm?.Invoke();
+                    Hide();
+                }
             };
         }
 
+        private void SetContentState(ContentState state)
+        {
+            contentState = state;
+            switch (state)
+            {
+                case ContentState.Warning:
+                    Warning.SetEnabled(true);
+                    Update.SetEnabled(false);
+                    Button.LabelText = "Update";
+                    Button.ShowIcon = Button.ButtonStyle.WithIcon;
+                    break;
+                case ContentState.Key:
+                    Update.SetEnabled(true);
+                    Warning.SetEnabled(false);
+                    Button.LabelText = "Bevestigen";
+                    Button.ShowIcon =  Button.ButtonStyle.Normal;
+                    break;
+                case ContentState.UsernameAndPassword:
+                    Update.SetEnabled(true);
+                    Warning.SetEnabled(false);
+                    Button.LabelText = "Bevestigen";
+                    Button.ShowIcon =  Button.ButtonStyle.Normal;
+                    break;
+            }
+        }
         
+        public void Show() => OnShow?.Invoke();
+        public void Hide() => OnHide?.Invoke();
     }
 }
