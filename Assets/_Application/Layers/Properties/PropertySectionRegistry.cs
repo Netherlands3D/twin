cc2e87eacca81cd344626003da78f893f7dea275
Tom Simons <tom.v.simons@gmail.com>
2026-03-25 20:05:42 +0100

    removed UnityEditor namespace

--- Assets/_Application/Layers/Properties/PropertySectionRegistry.cs
+++ Assets/_Application/Layers/Properties/PropertySectionRegistry.cs
@@ -1,29 +1,37 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor;
+// using UnityEditor;
 using UnityEngine;
 
 namespace Netherlands3D.Twin.Layers.Properties
 {
-    public static class PropertySectionRegistry //todo: rename this when we can delete the old Registry
+    public static class PropertySectionRegistry
     {
         public static Dictionary<Type, Type> TypeRegistry = new Dictionary<Type, Type>();
             
         static PropertySectionRegistry()
         {
-            var propertySectionTypes = TypeCache.GetTypesWithAttribute<PropertySectionAttribute>();
-            foreach (var type in propertySectionTypes)
+            var attributeAssembly = typeof(PropertySectionAttribute).Assembly;
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(a => a == attributeAssembly || 
+                            a.GetReferencedAssemblies()
+                                .Any(r => r.FullName == attributeAssembly.FullName));
+
+            foreach (var assembly in assemblies)
             {
-                if (type
-                        .GetCustomAttributes(typeof(PropertySectionAttribute), false)
-                        .FirstOrDefault() is PropertySectionAttribute attr)
+                foreach (var type in assembly.GetTypes())
                 {
-                    if (type.IsNested) continue; //The [UxmlElement] attribute causes Unity to code-generate a nested UxmlSerializedData class inside the panel classes, and that nested class inherits the attributes (PropertySectionAttribute) of its parent, so it will be picked up twice here.
-                    if (type.IsSubclassOf(typeof(MonoBehaviour))) continue; //todo: Remove this once all property panels are converted
-                    
-                    Debug.Log("adding type: " + attr.RequiredPropertyType + "\tpanel:" + type);
-                    TypeRegistry.Add(attr.RequiredPropertyType, type);
+                    if (type
+                            .GetCustomAttributes(typeof(PropertySectionAttribute), false)
+                            .FirstOrDefault() is PropertySectionAttribute attr)
+                    {
+                        if (type.IsNested) continue; //The [UxmlElement] attribute causes Unity to code-generate a nested UxmlSerializedData class inside the panel classes, and that nested class inherits the attributes (PropertySectionAttribute) of its parent, so it will be picked up twice here.
+                        if (type.IsSubclassOf(typeof(MonoBehaviour))) continue; //todo: Remove this once all property panels are converted
+
+                        Debug.Log("adding type: " + attr.RequiredPropertyType + "\tpanel:" + type);
+                        TypeRegistry.Add(attr.RequiredPropertyType, type);
+                    }
                 }
             }
         }
