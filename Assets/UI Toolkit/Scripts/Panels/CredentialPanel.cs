64f2cef904eca0aa600745d481ceeb9f9221ec25
bozmi <bozmi@OPGGAMES>
2026-03-25 14:26:12 +0100

    temp work to try and switch content of credentialpanel

--- Assets/UI Toolkit/Scripts/Panels/CredentialPanel.cs
+++ Assets/UI Toolkit/Scripts/Panels/CredentialPanel.cs
@@ -1,15 +1,29 @@
+using Netherlands3D.UI.Components;
 using Netherlands3D.UI.ExtensionMethods;
 using UnityEngine.UIElements;
+using static Netherlands3D.UI.Components.Toggle;
+using Button = Netherlands3D.UI.Components.Button;
 
 namespace Netherlands3D.UI.Panels
 {
     [UxmlElement]
     public partial class CredentialPanel : VisualElement
     {
+        private Button button;
+        private Button Button => button ??= this.Q<Button>("RetryButton");
+
         public CredentialPanel()
         {
             this.CloneComponentTree("Panels");
             this.AddComponentStylesheet("Panels");
+
+            Button.clicked += () =>
+            {
+                EnableInClassList("secondary", true);
+                this.ReplacePrefixedValueInClassList("credential-panel__message-title-text", ".secondary");
+            };
         }
+
+        
     }
 }
