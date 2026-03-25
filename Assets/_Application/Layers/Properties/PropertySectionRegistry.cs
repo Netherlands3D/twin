07bbabd4c2fa7e539905c478c9fdb2cfb7c34472
Tom Simons <tom.v.simons@gmail.com>
2026-03-25 20:37:07 +0100

    cleanup

--- Assets/_Application/Layers/Properties/PropertySectionRegistry.cs
+++ Assets/_Application/Layers/Properties/PropertySectionRegistry.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-// using UnityEditor;
 using UnityEngine;
 
 namespace Netherlands3D.Twin.Layers.Properties
@@ -12,12 +11,7 @@ namespace Netherlands3D.Twin.Layers.Properties
             
         static PropertySectionRegistry()
         {
-            var attributeAssembly = typeof(PropertySectionAttribute).Assembly;
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
-                .Where(a => a == attributeAssembly || 
-                            a.GetReferencedAssemblies()
-                                .Any(r => r.FullName == attributeAssembly.FullName));
-
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
             foreach (var assembly in assemblies)
             {
                 foreach (var type in assembly.GetTypes())
@@ -29,7 +23,6 @@ namespace Netherlands3D.Twin.Layers.Properties
                         if (type.IsNested) continue; //The [UxmlElement] attribute causes Unity to code-generate a nested UxmlSerializedData class inside the panel classes, and that nested class inherits the attributes (PropertySectionAttribute) of its parent, so it will be picked up twice here.
                         if (type.IsSubclassOf(typeof(MonoBehaviour))) continue; //todo: Remove this once all property panels are converted
 
-                        Debug.Log("adding type: " + attr.RequiredPropertyType + "\tpanel:" + type);
                         TypeRegistry.Add(attr.RequiredPropertyType, type);
                     }
                 }
