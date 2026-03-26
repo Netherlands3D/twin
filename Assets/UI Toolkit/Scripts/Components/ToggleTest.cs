5dbf10d551f2d69ccf4d49de946f88d927f28d3a
Tom Simons <tom.v.simons@gmail.com>
2026-03-26 11:26:44 +0100

    added test components with shared styling

---
+++ Assets/UI Toolkit/Scripts/Components/ToggleTest.cs
@@ -0,0 +1,91 @@
+using Netherlands3D.UI_Toolkit.Scripts;
+using Netherlands3D.UI.ExtensionMethods;
+using UnityEngine.UIElements;
+using static Netherlands3D.UI.Components.Toggle;
+
+namespace Netherlands3D.UI.Components
+{
+    [UxmlElement]
+    public partial class ToggleTest : UnityEngine.UIElements.Toggle
+    {
+        public enum ToggleType
+        {
+            Standard,
+            transparent
+        }
+
+        public enum ToggleStyle
+        {
+            Normal,
+            WithIcon,
+            IconOnly
+        }
+
+        public enum ToggleIconPosition
+        {
+            Left,
+            Right
+        }
+
+        // Query and cache icon component
+        private Icon icon;
+        private Icon Icon => icon ??= this.Q<Icon>("Icon");
+
+        // Query and cache label component
+        private Label labelField;
+        private Label Label => labelField ??= this.Q<Label>("Label");
+
+        private ToggleType toggleType = ToggleType.Standard;
+        [UxmlAttribute("toggle-type")]
+        public ToggleType Type
+        {
+            get => toggleType;
+            set { toggleType = value; UpdateClassList(); }
+        }
+
+        private ToggleStyle toggleStyle = ToggleStyle.WithIcon;
+        [UxmlAttribute("toggle-style")]
+        public ToggleStyle ShowIcon
+        {
+            get => toggleStyle;
+            set { toggleStyle = value; UpdateClassList(); }
+        }
+
+        private ToggleIconPosition toggleIconPosition = ToggleIconPosition.Left;
+        [UxmlAttribute("toggle-icon-position")]
+        public ToggleIconPosition IconPosition
+        {
+            get => toggleIconPosition;
+            set { toggleIconPosition = value; UpdateClassList(); }
+        }
+
+        [UxmlAttribute("icon")]
+        public IconImage Image
+        {
+            get => Icon.Image;
+            set => Icon.Image = value;
+        }
+
+        [UxmlAttribute("LabelText")]
+        public string LabelText
+        {
+            get => Label.text;
+            set => Label.text = value;
+        }
+
+        public ToggleTest()
+        {
+            this.CloneComponentTree("Components");
+            this.AddComponentStylesheet("Components");
+
+            RegisterCallback<AttachToPanelEvent>(_ => UpdateClassList());
+        }
+
+        private void UpdateClassList()
+        {
+            this.ReplacePrefixedValueInClassList("toggleTest-type-", toggleType.ToString().ToKebabCase());
+            this.ReplacePrefixedValueInClassList("toggleTest-style-", toggleStyle.ToString().ToKebabCase());
+            this.ReplacePrefixedValueInClassList("toggleTest-icon-position-", toggleIconPosition.ToString().ToKebabCase());
+        }
+    }
+}
