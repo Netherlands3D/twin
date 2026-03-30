f70c8076b9b4ae01a3c9569619c682fa1fd0cc12
Tom Simons <tom.v.simons@gmail.com>
2026-03-30 10:10:37 +0200

    started implementing scatter settings panel, not working correctly yet

--- Assets/UI Toolkit/Scripts/Panels/ScatterPropertySection.cs
+++ Assets/UI Toolkit/Scripts/Panels/ScatterPropertySection.cs
@@ -1,58 +1,213 @@
 using System.Collections.Generic;
-using System.Xml.Schema;
-using Netherlands3D.Coordinates;
 using Netherlands3D.Twin.Layers.ExtensionMethods;
 using Netherlands3D.Twin.Layers.LayerTypes.HierarchicalObject.Properties;
+using Netherlands3D.Twin.Layers.LayerTypes.Polygons;
+using Netherlands3D.Twin.Layers.LayerTypes.Polygons.Properties;
 using Netherlands3D.Twin.Layers.Properties;
 using Netherlands3D.UI.Components;
 using Netherlands3D.UI.ExtensionMethods;
-using RuntimeHandle;
 using UnityEngine;
 using UnityEngine.UIElements;
-using Toggle = UnityEngine.UIElements.Toggle;
+using RadioButtonGroup = Netherlands3D.UI.Components.RadioButtonGroup;
+using Slider = UnityEngine.UIElements.Slider;
 
 namespace Netherlands3D.UI.Panels
 {
     [UxmlElement]
     [PropertySection(typeof(ToggleScatterPropertyData))]
     public partial class ScatterPropertySection : VisualElement, IVisualizationWithPropertyData
-    { 
+    {
         const string inactiveUSSClassName = "inactive";
         private ToggleScatterPropertyData convertToScatterPropertyData;
+        private ScatterGenerationSettingsPropertyData settings;
 
-        private VisualElement toggleScatterSection; 
+        private VisualElement toggleScatterSection;
         private CheckboxToggle convertToggle;
-        
+
+        private VisualElement scatterSettingsSection;
+        private RadioButtonGroup scatterAreaRadioButtonGroup;
+        // private RadioButton strokeRadioButton;
+        // private RadioButton fillRadioButton;
+        // private RadioButton strokeAndFillRadioButton;
+        private Slider strokeWidthSlider;
+        private Slider densitySlider;
+        private Slider positionRandomnessSlider;
+        private Slider rotationSlider;
+        private SliderRange heightSliderRange;
+        private SliderRange diameterSliderRange;
+
         public ScatterPropertySection()
         {
             this.CloneComponentTree("Panels");
             this.AddComponentStylesheet("Panels");
-            
+
             toggleScatterSection = this.Q<VisualElement>("ToggleScatterSection");
-            convertToggle =  toggleScatterSection.Q<CheckboxToggle>();
+            convertToggle = toggleScatterSection.Q<CheckboxToggle>();
+
+            scatterSettingsSection = this.Q<VisualElement>("ScatterSettingsSection");
+            scatterAreaRadioButtonGroup = scatterSettingsSection.Q<RadioButtonGroup>("LocatieVerspreiding");
+            // fillRadioButton = scatterAreaRadioButtonGroup.Q<RadioButton>("Midden");
+            // strokeRadioButton = scatterAreaRadioButtonGroup.Q<RadioButton>("Rand");
+            // strokeAndFillRadioButton = scatterAreaRadioButtonGroup.Q<RadioButton>("Beide");
+            
+            strokeWidthSlider = scatterSettingsSection.Q<Slider>("RandBreedte");
+            densitySlider = scatterSettingsSection.Q<Slider>("Dichtheid");
+            positionRandomnessSlider = scatterSettingsSection.Q<Slider>("Verspreidingsgraad");
+            rotationSlider = scatterSettingsSection.Q<Slider>("RotatieRaster");
+            heightSliderRange = scatterSettingsSection.Q<SliderRange>("HoogteVariatie");
+            diameterSliderRange = scatterSettingsSection.Q<SliderRange>("DiameterVariatie");
         }
 
         public void LoadProperties(List<LayerPropertyData> properties)
         {
             convertToScatterPropertyData = properties.Get<ToggleScatterPropertyData>();
-            SetSectionVisible(convertToScatterPropertyData.AllowScatter);
-            
-            convertToScatterPropertyData.AllowScatterChanged.AddListener(SetSectionVisible);
-            convertToggle.RegisterValueChangedCallback(ToggleScatter);
+            SetEntireSectionVisible(convertToScatterPropertyData.AllowScatter);
+
+            convertToScatterPropertyData.AllowScatterChanged.AddListener(SetEntireSectionVisible);
+            convertToggle.RegisterValueChangedCallback(OnConvertToggleValueChanged);
             convertToggle.SetValueWithoutNotify(convertToScatterPropertyData.IsScattered);
+
+            convertToScatterPropertyData.IsScatteredChanged.AddListener(SetScatterSettingsSectionVisible);
+            SetScatterSettingsSectionVisible(convertToScatterPropertyData.IsScattered);
+
+            settings = properties.Get<ScatterGenerationSettingsPropertyData>();
+            if (settings == null)
+            {
+                SetSectionVisible(scatterSettingsSection, false);
+                return;
+            }
+            
+            OnScatterSettingsChanged();
+            settings.ScatterSettingsChanged.AddListener(OnScatterSettingsChanged);
+
+            scatterAreaRadioButtonGroup.RegisterValueChangedCallback(OnFillTypeChanged);
+
+            strokeWidthSlider.RegisterValueChangedCallback(OnStrokeWidthValueChanged);
+            // strokeWidthSlider.SetValueWithoutNotify(settings.StrokeWidth);
+            
+            densitySlider.RegisterValueChangedCallback(OnDensitySliderValueChanged);
+            // densitySlider.SetValueWithoutNotify(settings.Density);
+
+            positionRandomnessSlider.RegisterValueChangedCallback(OnPositionRandomnessValueChanged);
+            // positionRandomnessSlider.SetValueWithoutNotify(settings.Scatter);
+
+            rotationSlider.RegisterValueChangedCallback(OnRotationValueChanged);
+            // rotationSlider.SetValueWithoutNotify(settings.Angle);
+
+            heightSliderRange.RegisterValueChangedCallback(OnHeightRangeChanged);
+            // var heightRange = new Vector2(settings.MinScale.y, settings.MaxScale.y);
+            // heightSliderRange.SetValueWithoutNotify(heightRange);
+
+            diameterSliderRange.RegisterValueChangedCallback(OnDiameterRangeChanged);
+            // var diameterRange = new Vector2(settings.MinScale.x, settings.MaxScale.x); //x and z are the same for diameter
+            // diameterSliderRange.SetValueWithoutNotify(diameterRange);
         }
 
-        private void SetSectionVisible(bool isVisible)
+        private void SetSectionVisible(VisualElement section ,bool isVisible)
         {
-            if(isVisible)
-                RemoveFromClassList(inactiveUSSClassName);
+            if (isVisible)
+                section.RemoveFromClassList(inactiveUSSClassName);
             else
-                AddToClassList(inactiveUSSClassName);
+                section.AddToClassList(inactiveUSSClassName);
         }
         
-        private void ToggleScatter(ChangeEvent<bool> evt)
+        private void SetEntireSectionVisible(bool isVisible)
+        {
+            SetSectionVisible(this, isVisible);
+        }
+        
+        private void OnFillTypeChanged(ChangeEvent<int> evt)
+        {
+            switch (evt.newValue)
+            {
+                case 0:
+                    settings.FillType = FillType.Fill;
+                    break;
+                case 1:
+                    settings.FillType = FillType.Stroke;
+                    break;
+                case 2:
+                    settings.FillType = FillType.Complete;
+                    break;
+            }
+        }
+
+        private void OnConvertToggleValueChanged(ChangeEvent<bool> evt)
         {
             convertToScatterPropertyData.IsScattered = evt.newValue;
+        }
+
+        private void SetScatterSettingsSectionVisible(bool isScattered)
+        {
+            SetSectionVisible(scatterSettingsSection, isScattered);
+        }
+
+        private void OnScatterSettingsChanged()
+        {
+            scatterAreaRadioButtonGroup.value = settings.FillType switch
+            {
+                FillType.Complete => 2, // Beide
+                FillType.Stroke => 1, // Rand
+                FillType.Fill => 0  // Midden
+            };
+
+            strokeWidthSlider.value = settings.StrokeWidth;
+            densitySlider.value = settings.Density;
+            positionRandomnessSlider.value = settings.Scatter;
+            rotationSlider.value = settings.Angle;
+            // ShowAngleSlider = !settings.AutoRotateToLine;
+            SetSectionVisible(rotationSlider, !settings.AutoRotateToLine);
+            rotationSlider.AddToClassList("test");
+            heightSliderRange.value = new Vector2(settings.MinScale.y,  settings.MaxScale.y);
+            diameterSliderRange.value = new Vector2(settings.MinScale.x, settings.MaxScale.x); //x and z are the same for diameter
+        }
+
+        private void OnStrokeWidthValueChanged(ChangeEvent<float> evt)
+        {
+            settings.StrokeWidth = evt.newValue;
+        }
+        
+        private void OnDensitySliderValueChanged(ChangeEvent<float> evt)
+        {
+            settings.Density = evt.newValue;
+        }
+
+        private void OnPositionRandomnessValueChanged(ChangeEvent<float> evt)
+        {
+            settings.Scatter = evt.newValue;
+        }
+        
+        private void OnRotationValueChanged(ChangeEvent<float> evt)
+        {
+            settings.Angle = evt.newValue;
+        }
+        
+        private void OnHeightRangeChanged(ChangeEvent<Vector2> evt)
+        {
+            var minScale = settings.MinScale;
+            var maxScale = settings.MaxScale;
+
+            //change.x = y.min, change.y = y.max
+            minScale.y = evt.newValue.x;
+            maxScale.y = evt.newValue.y;
+
+            settings.MinScale = minScale;
+            settings.MaxScale = maxScale;
+        }
+        
+        private void OnDiameterRangeChanged(ChangeEvent<Vector2> evt)
+        {
+            var minScale = settings.MinScale;
+            var maxScale = settings.MaxScale;
+
+            //change.x = x.min and z.min, change.y = x.max and z.max
+            minScale.x = evt.newValue.x;
+            maxScale.x = evt.newValue.y;
+            minScale.z = evt.newValue.x;
+            maxScale.z = evt.newValue.y;
+
+            settings.MinScale = minScale;
+            settings.MaxScale = maxScale;
         }
     }
 }
