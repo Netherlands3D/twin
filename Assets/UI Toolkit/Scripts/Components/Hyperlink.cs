12d0282d0625b1a76061a8eb6d94edc350ae9107
Tom Simons <tom.v.simons@gmail.com>
2026-04-03 13:58:19 +0200

    hyperlink underline now appears only when hovering

--- Assets/UI Toolkit/Scripts/Components/Hyperlink.cs
+++ Assets/UI Toolkit/Scripts/Components/Hyperlink.cs
@@ -8,24 +8,42 @@ namespace Netherlands3D.UI.Components
     public partial class Hyperlink : UnityEngine.UIElements.Label
     {
         [UxmlAttribute] public string url { get; set; }
+        private const string underlineStartTag = "<u>";
+        private const string underlineEndTag = "</u>";
 
         public Hyperlink()
         {
             this.CloneComponentTree("Components");
             this.AddComponentStylesheet("Components");
-            
+
             RegisterCallback<ClickEvent>(OnClick);
-            RegisterCallback<AttachToPanelEvent>(OnAttachToPanel);
+            RegisterCallback<PointerOverEvent>(OnPointerOver);
+            RegisterCallback<PointerOutEvent>(OnPointerOut);
         }
 
-        private void OnAttachToPanel(AttachToPanelEvent evt)
+        private void OnPointerOver(PointerOverEvent evt)
         {
-            text = UnderlineText(text);
+            text = AddUnderline(text);
         }
 
-        private string UnderlineText(string s)
+        private void OnPointerOut(PointerOutEvent evt)
         {
-            return "<u>" + s +"</u>";
+            text = RemoveUnderline(text);
+        }
+
+        private string RemoveUnderline(string s)
+        {
+            if (s.StartsWith(underlineStartTag))
+            {
+                return s.Substring(underlineStartTag.Length, s.Length - underlineStartTag.Length - underlineEndTag.Length);
+            }
+
+            return s;
+        }
+
+        private string AddUnderline(string s)
+        {
+            return underlineStartTag + s + underlineEndTag;
         }
 
         private void OnClick(ClickEvent evt)
