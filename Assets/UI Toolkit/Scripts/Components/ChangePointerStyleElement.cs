922987f00c6b9bdd552d1e83986a5fe9704e661b
Tom Simons <tom.v.simons@gmail.com>
2026-04-03 14:48:47 +0200

    added change pointer style element for hyperlink, needs testing in build

---
+++ Assets/UI Toolkit/Scripts/Components/ChangePointerStyleElement.cs
@@ -0,0 +1,193 @@
+﻿using System.Runtime.InteropServices;
+using Netherlands3D.UI.ExtensionMethods;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Netherlands3D.UI.Components
+{
+    [UxmlElement]
+    public partial class ChangePointerStyleElement : VisualElement
+    {
+        [DllImport("__Internal")]
+        private static extern string SetCSSCursor(string cursorName = "auto");
+
+        public enum PointerStyle
+        {
+            AUTO,
+            DEFAULT,
+            NONE,
+            CONTEXT_MENU,
+            HELP,
+            POINTER,
+            PROGRESS,
+            WAIT,
+            CELL,
+            CROSSHAIR,
+            TEXT,
+            VERTICAL_TEXT,
+            ALIAS,
+            COPY,
+            MOVE,
+            NO_DROP,
+            NOT_ALLOWED,
+            GRAB,
+            GRABBING,
+            ALL_SCROLL,
+            COL_RESIZE,
+            ROW_RESIZE,
+            N_RESIZE,
+            NE_RESIZE,
+            E_RESIZE,
+            SE_RESIZE,
+            S_RESIZE,
+            SW_RESIZE,
+            W_RESIZE,
+            NW_RESIZE,
+            EW_RESIZE,
+            NS_RESIZE,
+            NESW_RESIZE,
+            NWSE_RESIZE
+        }
+        
+        private PointerStyle styleOnHover = PointerStyle.POINTER;
+        public static PointerStyle pointerType = PointerStyle.AUTO;
+        
+        [UxmlAttribute("pointer-style-hover")]
+        public PointerStyle StyleOnHover { get => styleOnHover; set => styleOnHover = value; }
+        
+        public ChangePointerStyleElement()
+        {
+            // this.CloneComponentTree("Components");
+            this.AddComponentStylesheet("Components");
+
+            RegisterCallback<PointerOverEvent>(OnPointerOver);
+            RegisterCallback<PointerOutEvent>(OnPointerOut);
+        }
+
+        private void OnPointerOver(PointerOverEvent evt)
+        {
+            ChangeCursor(StyleOnHover);
+        }
+
+        private void OnPointerOut(PointerOutEvent evt)
+        {
+            ChangeCursor(PointerStyle.AUTO);
+        }
+        
+        public static void ChangeCursor(PointerStyle type)
+        {
+            pointerType = type;
+
+            var cursorString = "";
+            switch (pointerType)
+            {
+                case PointerStyle.AUTO:
+                    cursorString = "auto";
+                    break;
+                case PointerStyle.DEFAULT:
+                    cursorString = "default";
+                    break;
+                case PointerStyle.NONE:
+                    cursorString = "none";
+                    break;
+                case PointerStyle.CONTEXT_MENU:
+                    cursorString = "context-menu";
+                    break;
+                case PointerStyle.HELP:
+                    cursorString = "help";
+                    break;
+                case PointerStyle.POINTER:
+                    cursorString = "pointer";
+                    break;
+                case PointerStyle.PROGRESS:
+                    cursorString = "progress";
+                    break;
+                case PointerStyle.WAIT:
+                    cursorString = "wait";
+                    break;
+                case PointerStyle.CELL:
+                    cursorString = "cell";
+                    break;
+                case PointerStyle.CROSSHAIR:
+                    cursorString = "crosshair";
+                    break;
+                case PointerStyle.TEXT:
+                    cursorString = "text";
+                    break;
+                case PointerStyle.VERTICAL_TEXT:
+                    cursorString = "vertical-text";
+                    break;
+                case PointerStyle.ALIAS:
+                    cursorString = "alias";
+                    break;
+                case PointerStyle.COPY:
+                    cursorString = "copy";
+                    break;
+                case PointerStyle.MOVE:
+                    cursorString = "move";
+                    break;
+                case PointerStyle.NO_DROP:
+                    cursorString = "no-drop";
+                    break;
+                case PointerStyle.NOT_ALLOWED:
+                    cursorString = "not-allowed";
+                    break;
+                case PointerStyle.GRAB:
+                    cursorString = "grab";
+                    break;
+                case PointerStyle.GRABBING:
+                    cursorString = "grabbing";
+                    break;
+                case PointerStyle.ALL_SCROLL:
+                    cursorString = "all-scroll";
+                    break;
+                case PointerStyle.COL_RESIZE:
+                    cursorString = "col-resize";
+                    break;
+                case PointerStyle.ROW_RESIZE:
+                    cursorString = "row-resize";
+                    break;
+                case PointerStyle.N_RESIZE:
+                    cursorString = "n-resize";
+                    break;
+                case PointerStyle.NE_RESIZE:
+                    cursorString = "ne-resize";
+                    break;
+                case PointerStyle.E_RESIZE:
+                    cursorString = "e-resize";
+                    break;
+                case PointerStyle.SE_RESIZE:
+                    cursorString = "se-resize";
+                    break;
+                case PointerStyle.S_RESIZE:
+                    cursorString = "s-resize";
+                    break;
+                case PointerStyle.SW_RESIZE:
+                    cursorString = "sw-resize";
+                    break;
+                case PointerStyle.W_RESIZE:
+                    cursorString = "w-resize";
+                    break;
+                case PointerStyle.NW_RESIZE:
+                    cursorString = "nw-resize";
+                    break;
+                case PointerStyle.EW_RESIZE:
+                    cursorString = "ew-resize";
+                    break;
+                case PointerStyle.NS_RESIZE:
+                    cursorString = "ns-resize";
+                    break;
+                case PointerStyle.NESW_RESIZE:
+                    cursorString = "nesw-resize";
+                    break;
+                case PointerStyle.NWSE_RESIZE:
+                    cursorString = "nwse-resize";
+                    break;
+            }
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+            SetCSSCursor(cursorString);
+#endif
+        }
+    }
+}
--- Assets/UI Toolkit/Scripts/Components/Hyperlink.cs
+++ Assets/UI Toolkit/Scripts/Components/Hyperlink.cs
@@ -5,47 +5,55 @@ using UnityEngine.UIElements;
 namespace Netherlands3D.UI.Components
 {
     [UxmlElement]
-    public partial class Hyperlink : UnityEngine.UIElements.Label
+    public partial class Hyperlink : ChangePointerStyleElement
     {
-        [UxmlAttribute] public string url { get; set; }
         private const string underlineStartTag = "<u>";
         private const string underlineEndTag = "</u>";
-
-        public Hyperlink()
+        
+        private Label label;
+        [UxmlAttribute] public string url { get; set; }
+        [UxmlAttribute] public string text {get => label.text; set => label.text = value; }
+        
+        public Hyperlink() : base()
         {
             this.CloneComponentTree("Components");
             this.AddComponentStylesheet("Components");
-
+        
+            label = this.Q<Label>();
+            if (text == string.Empty)
+                text = url;
+            
             RegisterCallback<ClickEvent>(OnClick);
             RegisterCallback<PointerOverEvent>(OnPointerOver);
             RegisterCallback<PointerOutEvent>(OnPointerOut);
+            
         }
-
+        
         private void OnPointerOver(PointerOverEvent evt)
         {
             text = AddUnderline(text);
         }
-
+        
         private void OnPointerOut(PointerOutEvent evt)
         {
             text = RemoveUnderline(text);
         }
-
+        
         private string RemoveUnderline(string s)
         {
             if (s.StartsWith(underlineStartTag))
             {
                 return s.Substring(underlineStartTag.Length, s.Length - underlineStartTag.Length - underlineEndTag.Length);
             }
-
+        
             return s;
         }
-
+        
         private string AddUnderline(string s)
         {
             return underlineStartTag + s + underlineEndTag;
         }
-
+        
         private void OnClick(ClickEvent evt)
         {
             Application.OpenURL(url);
