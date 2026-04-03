8e790134d2d6b1ad0eb5b1bc0ce777b5acdb7051
Tom Simons <tom.v.simons@gmail.com>
2026-04-03 10:46:12 +0200

    added hyperlink component

---
+++ Assets/UI Toolkit/Scripts/Components/Hyperlink.cs
@@ -0,0 +1,25 @@
+﻿using Netherlands3D.UI.ExtensionMethods;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Netherlands3D.UI.Components
+{
+    [UxmlElement]
+    public partial class Hyperlink : UnityEngine.UIElements.Label
+    {
+        [UxmlAttribute] public string url { get; set; }
+
+        public Hyperlink()
+        {
+            this.CloneComponentTree("Components");
+            this.AddComponentStylesheet("Components");
+            
+            RegisterCallback<ClickEvent>(OnClick);
+        }
+
+        private void OnClick(ClickEvent evt)
+        {
+            Application.OpenURL(url);
+        }
+    }
+}
