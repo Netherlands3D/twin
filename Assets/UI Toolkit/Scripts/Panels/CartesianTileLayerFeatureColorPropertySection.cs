c144267bcee5860acf8a965153bba9cfc5d13862
bozmir <bozmir@hotmail.com>
2026-04-28 14:28:23 +0200

    basic setup for maaiveld coloring propertysection

--- Assets/UI Toolkit/Scripts/Panels/CartesianTileLayerFeatureColorPropertySection.cs
+++ Assets/UI Toolkit/Scripts/Panels/CartesianTileLayerFeatureColorPropertySection.cs
@@ -9,8 +9,10 @@ using Netherlands3D.UI.Components;
 using Netherlands3D.UI.ExtensionMethods;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using ListView = Netherlands3D.UI.Components.ListView;
 using Toggle = UnityEngine.UIElements.Toggle;
 
 namespace Netherlands3D.UI.Panels
@@ -19,54 +21,119 @@ namespace Netherlands3D.UI.Panels
     [PropertySection(typeof(CartesianTileLayerFeatureColorPropertyData))]
     public partial class CartesianTileLayerFeatureColorPropertySection : VisualElement, IVisualizationWithPropertyData
     {
-        private ContentContainer contentContainer;
-        
-        [SerializeField] private RectTransform content;
-        [SerializeField] private GameObject colorSwatchPrefab;
-        [SerializeField] private RectTransform layerContent;
-
-        private readonly Dictionary<int, ColorSwatch> swatches = new();
-        [SerializeField] private ColorPickerPropertySection colorPicker;
-
-        public int SelectedButtonIndex { get; set; } = -1;
-        public List<ISelectable> SelectedItems { get; } = new();
-        public List<ISelectable> Items { get; set; } = new();
-        public ISelectable FirstSelectedItem { get; set; }
-
         private CartesianTileLayerFeatureColorPropertyData stylingPropertyData;
+        
+        private ListView swatchesListView;
+        private ListView SwatchesListView => swatchesListView ??= this.Q<ListView>();
+        private List<string> colors = new();
         
         public CartesianTileLayerFeatureColorPropertySection()
         {
             this.CloneComponentTree("Panels");
             this.AddComponentStylesheet("Panels");
             
-            contentContainer = this.Q<ContentContainer>();
+            SwatchesListView.virtualizationMethod = CollectionVirtualizationMethod.DynamicHeight;
+            SwatchesListView.selectionType = SelectionType.Multiple;
+            
+            SwatchesListView.makeItem = MakeListViewItem;
+            SwatchesListView.bindItem = BindListViewItem;
+            
+            
+            SwatchesListView.RegisterCallback<AttachToPanelEvent>(evt =>
+            {
+                //when clicked outside the listview, deselect the current selection
+                RegisterOutsidePanelClick();
+            });
+            
+            SwatchesListView.selectedIndicesChanged += indices =>
+            {
+                //show selection in world when items in panel are selected
+                UpdateSelectionForIndices(indices);
+            };
+            
+            RegisterCallback<DetachFromPanelEvent>(_ => 
+            {
+                OnDestroy();  
+            });
+        }
+        
+        private void RegisterOutsidePanelClick()
+        {
+            var pointerAction = new InputAction(binding: "<Pointer>/press");
+            pointerAction.performed += _ =>
+            {
+                var pos = Pointer.current.position.ReadValue();
+                var panelPos = RuntimePanelUtils.ScreenToPanel(
+                    SwatchesListView.panel,
+                    new Vector2(pos.x, Screen.height - pos.y)
+                );
+                    
+                if (!SwatchesListView.worldBound.Contains(panelPos))
+                {
+                    SwatchesListView.ClearSelection();
+                }
+            };
+            pointerAction.Enable();
+    
+            SwatchesListView.RegisterCallback<DetachFromPanelEvent>(_ => pointerAction.Dispose());
+        }
+        
+        private VisualElement MakeListViewItem()
+        {
+            ColorTile item = new();
+            item.RegisterCallback<PointerUpEvent>(evt =>
+            {
+                //open color wheel
+            });
+            return item;
+        }
+        
+        private void BindListViewItem(VisualElement item, int index)
+        {
+            if (item is not ColorTile listViewItem) return;
+           
+            string color = SwatchesListView.itemsSource[index] as string;
+            listViewItem.Color = color;
+        }
+        
+        private void UpdateSelectionForIndices(IEnumerable<int> indices)
+        {
+            // foreach (int i in indices)
+            // {
+            //     var id = ListView.itemsSource[i] as string;
+            //     bool? visibility = stylingPropertyData.GetVisibilityForSubObjectById(id);
+            //     if (visibility == true)
+            //     {
+            //         Coordinate coord = (Coordinate)stylingPropertyData.GetVisibilityCoordinateForSubObjectById(id);
+            //         selector.SelectBagId(id, coord);
+            //     }
+            // }
         }
 
         public void LoadProperties(List<LayerPropertyData> properties)
         {
-            stylingPropertyData = properties.GetDefaultStylingPropertyData<CartesianTileLayerFeatureColorPropertyData>(); 
+            stylingPropertyData = properties.GetDefaultStylingPropertyData<CartesianTileLayerFeatureColorPropertyData>();
+            if (stylingPropertyData == null) return;
             
-            CreateSwatches();
-
+            
+            UpdateSwatches();
+            
             stylingPropertyData.OnStylingChanged.AddListener(UpdateSwatches);
-            colorPicker.ColorWheel.colorChanged.AddListener(OnPickColor);
+            //colorPicker.ColorWheel.colorChanged.AddListener(OnPickColor);
 
-            HideColorPicker();
+            //HideColorPicker();
         }
 
         private void OnDestroy()
         {
             stylingPropertyData.OnStylingChanged.RemoveListener(UpdateSwatches);
-            colorPicker.ColorWheel.colorChanged.RemoveListener(OnPickColor);
+            //colorPicker.ColorWheel.colorChanged.RemoveListener(OnPickColor);
         }
 
 
-        private void CreateSwatches()
+        private void UpdateSwatches()
         {
-            swatches.Clear();
-            layerContent.ClearAllChildren();
-            
+            colors.Clear();
             foreach(KeyValuePair<string, StylingRule> kv in stylingPropertyData.StylingRules)
             {
                 if(kv.Key.Contains(CartesianTileLayerFeatureColorPropertyData.ColoringIdentifier))
@@ -76,14 +143,14 @@ namespace Netherlands3D.UI.Panels
                     //we need to expect a value here or else the stylingrule is not properly initialized
                     if (color.HasValue)
                     {
-                        //swatches[index] = CreateSwatch(index);
-                        SetSwatchColorFromFeature(index);
+                        //swatch.SetColor(color.GetValueOrDefault(Color.white));
                     }
                     else
                         Debug.LogError("stylingrule not initialized because the colorvalue is missing");
                 }
             }
-            Items = swatches.Values.OfType<ISelectable>().ToList();
+            SwatchesListView.itemsSource = colors;
+            SwatchesListView.RefreshItems();
         }
 
         // private ColorSwatch CreateSwatch(int index)
@@ -105,7 +172,7 @@ namespace Netherlands3D.UI.Panels
         private void OnClickedOnSwatch(PointerEventData _, ColorSwatch swatch)
         {
             //select layer
-            SelectedButtonIndex = Items.IndexOf(swatch);
+            //SelectedButtonIndex = Items.IndexOf(swatch);
             // MultiSelectionUtility.ProcessLayerSelection(this, anySelected =>
             // {
             //     if(anySelected)
@@ -120,44 +187,27 @@ namespace Netherlands3D.UI.Panels
             // });
         }
 
-        private void OnPickColor(Color color)
-        {
-            foreach ((int index, ColorSwatch swatch) in swatches)
-            {
-                if (!swatch.IsSelected) continue;
-                
-                swatch.SetColor(color);
-                stylingPropertyData.SetColorByMaterialIndex(index, swatch.LayerName, color);
-            }
-        }
+        // private void OnPickColor(Color color)
+        // {
+        //     foreach ((int index, ColorSwatch swatch) in swatches)
+        //     {
+        //         if (!swatch.IsSelected) continue;
+        //         
+        //         swatch.SetColor(color);
+        //         stylingPropertyData.SetColorByMaterialIndex(index, swatch.LayerName, color);
+        //     }
+        // }
+      
 
-        private void UpdateSwatches()
-        {
-            foreach (var (layerFeature, _) in swatches)
-            {
-                SetSwatchColorFromFeature(layerFeature);
-            }
-        }
-
-        private void SetSwatchColorFromFeature(int index)
-        {
-            // if there is no swatch matching this layer feature, we can skip this update
-            if (!swatches.TryGetValue(index, out var swatch)) return;
-            
-            var color = stylingPropertyData.GetColorByMaterialIndex(index);
-
-            swatch.SetColor(color.GetValueOrDefault(Color.white));
-        }
-
-        private void ShowColorPicker()
-        {
-            colorPicker.gameObject.SetActive(true);
-            colorPicker.LoadProperties(new List<LayerPropertyData>() { stylingPropertyData });
-        }
-
-        private void HideColorPicker()
-        {
-            colorPicker.gameObject.SetActive(false);
-        }       
+        // private void ShowColorPicker()
+        // {
+        //     colorPicker.gameObject.SetActive(true);
+        //     colorPicker.LoadProperties(new List<LayerPropertyData>() { stylingPropertyData });
+        // }
+        //
+        // private void HideColorPicker()
+        // {
+        //     colorPicker.gameObject.SetActive(false);
+        // }       
     }
 }
