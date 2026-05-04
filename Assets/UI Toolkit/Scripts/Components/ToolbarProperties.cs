9b4c549199fd2c74c955dc05d45c1a81684ef660
Tom Simons <tom.v.simons@gmail.com>
2026-05-04 13:35:41 +0200

    fixed deselectable tab

--- Assets/UI Toolkit/Scripts/Components/ToolbarProperties.cs
+++ Assets/UI Toolkit/Scripts/Components/ToolbarProperties.cs
@@ -6,28 +6,14 @@ namespace Netherlands3D.UI.Components
     [UxmlElement]
     public partial class ToolbarProperties : VisualElement
     {
-        public ToggleButtonGroup Group => this.Q<ToggleButtonGroup>("ButtonGroup");
         public Button Information => this.Q<Button>("Information");
         public Button Settings => this.Q<Button>("Settings");
         public Button Styles => this.Q<Button>("Styles");
-
-        public EventCallback<ChangeEvent<bool>> OnOpenInformationToggled { get; set; }
-        public EventCallback<ChangeEvent<bool>> OnSettingsToggled { get; set; }
-        public EventCallback<ChangeEvent<bool>> OnStylesToggled { get; set; }
 
         public ToolbarProperties()
         {
             this.CloneComponentTree("Components");
             this.AddComponentStylesheet("Components");
-
-            // Register exposed events
-
-            RegisterCallback<AttachToPanelEvent>(_ =>
-            {
-                // Defaults: single selection, empty selection allowed
-                Group.allowEmptySelection = true;
-                Group.isMultipleSelection = false;
-            });
         }
     }
 }
