0e4e4da2de666b81fd40bbdf9e5d95e8822103f4
Tom Simons <tom.v.simons@gmail.com>
2026-06-16 12:21:59 +0200

    Added quality settings section

--- Assets/UI Toolkit/Scripts/Panels/SettingsPanel.cs
+++ Assets/UI Toolkit/Scripts/Panels/SettingsPanel.cs
@@ -1,16 +1,10 @@
-using System;
-using Netherlands3D.Credentials;
-using Netherlands3D.Credentials.StoredAuthorization;
-using Netherlands3D.Services;
-using Netherlands3D.Twin;
+using Netherlands3D.Twin.Configuration;
+using Netherlands3D.Twin.Quality;
 using Netherlands3D.UI_Toolkit.Scripts.Panels;
-using Netherlands3D.UI.Components;
 using Netherlands3D.UI.ExtensionMethods;
-using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UIElements;
-using Button = UnityEngine.UIElements.Button;
-using TextField = Netherlands3D.UI.Components.TextField;
+using QualitySettings = UnityEngine.QualitySettings;
+using RadioButtonGroup = UnityEngine.UIElements.RadioButtonGroup;
 
 namespace Netherlands3D.UI.Panels
 {
@@ -19,11 +13,26 @@ namespace Netherlands3D.UI.Panels
     public partial class SettingsPanel : BaseInspectorContentPanel
     {
         public override string Title => "Instellingen";
+        private RadioButtonGroup radioButtonGroup;
 
         public SettingsPanel()
         {
+        }
+
+        public SettingsPanel(Configuration configuration) : this()
+        {
             this.CloneComponentTree("Panels"); 
             this.AddComponentStylesheet("Panels");
+            
+            radioButtonGroup = this.Q<RadioButtonGroup>("QualitySettings");
+            radioButtonGroup.value = QualitySettings.GetQualityLevel();
+            radioButtonGroup.RegisterValueChangedCallback(OnQualitySettingsChanged);
+        }
+
+        private void OnQualitySettingsChanged(ChangeEvent<int> evt)
+        {
+            var level = (GraphicsQualityLevel)evt.newValue;
+            Twin.Quality.QualitySettings.SetGraphicsQuality(level, true);
         }
     }
 }
