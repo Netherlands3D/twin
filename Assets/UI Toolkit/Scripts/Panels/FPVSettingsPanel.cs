021da0b7cd53c8ecdfb55adc39cf245c32b027dd
Tom Simons <tom.v.simons@gmail.com>
2026-07-10 17:17:20 +0200

    cleanup

--- Assets/UI Toolkit/Scripts/Panels/FPVSettingsPanel.cs
+++ Assets/UI Toolkit/Scripts/Panels/FPVSettingsPanel.cs
@@ -1,16 +1,8 @@
-using System.Linq;
 using Netherlands3D.Services;
-using Netherlands3D.Twin.Configuration;
 using Netherlands3D.Twin.Functionalities;
-using Netherlands3D.Twin.Quality;
 using Netherlands3D.UI_Toolkit;
-using Netherlands3D.UI_Toolkit.Scripts.Panels;
-using Netherlands3D.UI.Components;
 using Netherlands3D.UI.ExtensionMethods;
 using UnityEngine.UIElements;
-using ListView = UnityEngine.UIElements.ListView;
-using QualitySettings = UnityEngine.QualitySettings;
-using RadioButtonGroup = UnityEngine.UIElements.RadioButtonGroup;
 using Slider = Netherlands3D.UI.Components.Slider;
 using Toggle = UnityEngine.UIElements.Toggle;
 
