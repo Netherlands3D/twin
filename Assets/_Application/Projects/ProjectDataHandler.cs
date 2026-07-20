ce551229d2835694188c51cd962e6069afa9dc18
bozmir <bozmir@hotmail.com>
2026-07-20 12:05:41 +0200

    projectdatastore gone

--- Assets/_Application/Projects/ProjectDataHandler.cs
+++ Assets/_Application/Projects/ProjectDataHandler.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
 using JetBrains.Annotations;
 using Netherlands3D.Credentials;
 using Netherlands3D.DataTypeAdapters;
 using Netherlands3D.Functionalities.AssetBundles;
 using Netherlands3D.Services;
+using Netherlands3D.Twin.Layers.Properties;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -25,8 +32,7 @@ namespace Netherlands3D.Twin.Projects
         [UsedImplicitly] private DataTypeChain fileImporter; // don't remove, this is used in LoadDefaultProject()
         [UsedImplicitly] private CredentialHandler credentialHandler; // don't remove, this is used in LoadDefaultProject()
         [SerializeField] private string defaultProjectFileName = "ProjectTemplate.nl3d";
-        [SerializeField] private ProjectDataStore projectDataStore;
-        [SerializeField] private InputActionAsset applicationActionMap;
+       [SerializeField] private InputActionAsset applicationActionMap;
 
         public UnityEvent OnSaveStarted;
         public UnityEvent OnSaveCompleted;
@@ -35,6 +41,14 @@ namespace Netherlands3D.Twin.Projects
         public UnityEvent OnLoadFailed;
 
         private static ProjectDataHandler instance;
+        
+        private string DefaultFileName = "NL3D_Project_";
+        private string ProjectFileExtension = "nl3d";
+        private string ProjectJsonFileNameInZip = "project.json";
+
+        private ZipOutputStream zipOutputStream;
+
+        private string lastSavePath;
 
         public static ProjectDataHandler Instance
         {
@@ -50,6 +64,8 @@ namespace Netherlands3D.Twin.Projects
 
         private void Awake()
         {
+            gameObject.name = gameObject.name + Guid.NewGuid();
+            
             if (ProjectData.Current == null)
             {
                 Debug.LogError("Current ProjectData object reference is not set in ProjectData", this.gameObject);
@@ -110,7 +126,7 @@ namespace Netherlands3D.Twin.Projects
         public void SaveProject()
         {
             Debug.Log("SAVING PROJECT DATA");
-            projectDataStore.SaveAsFile(this);
+            SaveAsFile();
             Debug.Log("SAVING PROJECT DATA Finished");
             ToolService tools = ServiceLocator.GetService<ToolService>();
             tools.GetTool(ToolType.SaveProject).Close();
@@ -127,7 +143,7 @@ namespace Netherlands3D.Twin.Projects
 #else
             var filePath = Path.Combine(Application.persistentDataPath, Application.streamingAssetsPath, defaultProjectFileName);
             Debug.Log("loading default project file: " + filePath);
-            projectDataStore.LoadFromFile(filePath);
+            LoadFromFile(filePath);
 #endif
         }
 
@@ -138,7 +154,37 @@ namespace Netherlands3D.Twin.Projects
             if (filePath.ToLower().EndsWith(".nl3d"))
             {
                 Debug.Log("loading nl3d file: " + filePath);
-                var project = projectDataStore.LoadFromFile(filePath);
+                App.Layers.Remove(ProjectData.Current.RootLayer);
+                ProjectData.Current.ClearFunctionalityData();
+            
+                Resources.UnloadUnusedAssets();
+
+                // Open the zip file
+                using FileStream fs = File.OpenRead(filePath);
+
+                //Extract specific project.json from zip using CsharpLib
+                using ZipFile zf = new(fs);
+
+                foreach (ZipEntry zipEntry in zf)
+                {
+                    // TODO: this does not yet support directories
+                    if (!zipEntry.IsFile) continue;
+
+                    using Stream zipStream = zf.GetInputStream(zipEntry);
+                    if (zipEntry.Name == ProjectJsonFileNameInZip)
+                    {
+                        using StreamReader sr = new(zipStream);
+                        string json = sr.ReadToEnd();
+
+                        LoadJSON(json);
+                        continue;
+                    }
+                
+                    string fullZipToPath = Path.Combine(Application.persistentDataPath, zipEntry.Name);
+                    using FileStream streamWriter = File.Create(fullZipToPath);
+                    zipStream.CopyTo(streamWriter);
+                }
+                var project = ProjectData.Current;
                 OnLoadCompleted.Invoke(project);
                 return project;
             }
@@ -147,9 +193,131 @@ namespace Netherlands3D.Twin.Projects
             return null;
         }
         
+        [DllImport("__Internal")]
+        private static extern void DownloadFromIndexedDB(string filename, string callbackObjectName, string callbackMethodName);
+
+        [DllImport("__Internal")]
+        [UsedImplicitly]
+        private static extern void SyncFilesToIndexedDB(string callbackObjectName, string callbackMethodName);
+
+        private readonly JsonSerializerSettings serializerSettings = new()
+        {
+            TypeNameHandling = TypeNameHandling.Auto,
+            Formatting = Formatting.Indented,
+            SerializationBinder = new DataContractSerializationBinder(new DefaultSerializationBinder())
+        };
+        
+        private void LoadJSON(string json)
+        {
+            try
+            {
+                JsonConvert.PopulateObject(json, ProjectData.Current, serializerSettings);
+            }
+            catch(Exception e)
+            {
+                Debug.LogException(e);
+            }
+
+            ProjectData.Current.RootLayer.ReconstructParentsRecursive();
+
+            ProjectData.Current.RootLayer.UpdateLayerTreeOrder(0);
+            Debug.Log("Loaded project with uuid: " + ProjectData.Current.UUID);
+            ProjectData.Current.OnDataChanged.Invoke(ProjectData.Current);
+            ProjectData.Current.LoadVisualizations();
+        }
+
+        public void SaveAsFile()
+        {
+            ProjectData.Current.RefreshUUID();
+
+            // Set the timestamp when the data was saved
+            ProjectData.Current.SavedTimestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+            var readableTimeStamp = DateTime.Now.ToString("yyyy-MM-dd_HHmm");
+
+            // Start the zip output stream
+            var lastSavePath = Application.persistentDataPath + $"/{DefaultFileName}{readableTimeStamp}.{ProjectFileExtension}";
+            zipOutputStream = new ZipOutputStream(File.Create(lastSavePath));
+            zipOutputStream.SetLevel(9); // 0-9 where 9 means best compression
+            
+            var projectData = ProjectData.Current;
+            WriteProjectToZip(projectData, zipOutputStream);
+            WriteProjectAssetsToZipfile(projectData, zipOutputStream);
+
+            zipOutputStream.Finish();
+            zipOutputStream.Close();
+            Debug.Log("SAVING PROJECT DATA path " + lastSavePath);
+            SaveFile(lastSavePath);
+        }
+
+        private void WriteProjectAssetsToZipfile(ProjectData projectData, ZipOutputStream zipOutputStream)
+        {
+            var projectAssets = projectData
+                .GetAssets().Where(asset => asset.IsStoredInProject)
+                .ToList();
+            
+            foreach (var layerAsset in projectAssets)
+            {
+                WriteProjectAssetToZipFile(layerAsset, zipOutputStream);
+            }
+        }
+
+        private void WriteProjectAssetToZipFile(LayerAsset layerAsset, ZipOutputStream zipOutputStream)
+        {
+            var relativePath = layerAsset.Uri.LocalPath.TrimStart('\\', '/');
+            var absolutePath = Path.Combine(Application.persistentDataPath, relativePath);
+            
+            var entry = new ZipEntry(relativePath);
+            zipOutputStream.PutNextEntry(entry);
+            byte[] fileBytes = File.ReadAllBytes(absolutePath);
+            zipOutputStream.Write(fileBytes, 0, fileBytes.Length);
+        }
+
+        private void WriteProjectToZip(ProjectData projectData, ZipOutputStream zipOutputStream)
+        {
+            var jsonProject = JsonConvert.SerializeObject(projectData, serializerSettings);
+            var entry = new ZipEntry(ProjectJsonFileNameInZip);
+            zipOutputStream.PutNextEntry(entry);
+            byte[] jsonBytes = System.Text.Encoding.UTF8.GetBytes(jsonProject.ToString());
+            zipOutputStream.Write(jsonBytes, 0, jsonBytes.Length);
+        }
+
+        private void SaveFile(string lastSavePath)
+        {
+            // Make sure indexedDB is synced
+#if !UNITY_EDITOR && UNITY_WEBGL
+            this.lastSavePath = lastSavePath; 
+            SyncFilesToIndexedDB(gameObject.name, "ProjectSavedToIndexedDB");
+#elif UNITY_EDITOR
+            //Request using file write dialog of unity editor where to copy the file from lastSavePath path
+            var fileName = Path.GetFileName(lastSavePath);
+            var fileTargetPath = EditorUtility.SaveFilePanel("Save project", Application.persistentDataPath, fileName, ProjectFileExtension);
+            if (fileTargetPath.Length > 0)
+            {
+                File.Copy(lastSavePath, fileTargetPath, true);
+            }
+
+            //Open the folder where the file is saved
+            EditorUtility.RevealInFinder(fileTargetPath);
+#endif
+        }
+
+        public void AppendFileToZip(string fileName)
+        {
+            var persistentDataPath = Application.persistentDataPath + "/" + fileName;
+
+            byte[] buffer = new byte[4096];
+            var randomFileTag = DateTime.Now.ToString("yyyyMMddHHmmss");
+            ZipEntry entry = new ZipEntry(randomFileTag + "_" + fileName);
+            zipOutputStream.PutNextEntry(entry);
+
+            using FileStream fs = File.OpenRead(persistentDataPath);
+            StreamUtils.Copy(fs, zipOutputStream, buffer);
+        }
+        
         public void ProjectSavedToIndexedDB()
         {
-            projectDataStore.ProjectSavedToIndexedDB();
+            var fileName = Path.GetFileName(lastSavePath);
+            DownloadFromIndexedDB($"{fileName}", gameObject.name, "DownloadedProject");
         }
     }
 }
