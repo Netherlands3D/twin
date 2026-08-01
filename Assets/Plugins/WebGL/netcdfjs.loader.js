7f8e1e56069d24edfabf3e1be5f0a3374be60169
bozmir <bozmir@hotmail.com>
2026-08-01 21:26:10 +0200

    succesful parsing knmi volumetric data webgl + cleanup

--- Assets/Plugins/WebGL/netcdfjs.loader.js
+++
@@ -1,7 +0,0 @@
-console.log("loading netcdfjs");
-
-(function () {
-    var script = document.createElement("script");
-    script.src = "netcdfjs.umd.js";
-    document.head.appendChild(script);
-})();
