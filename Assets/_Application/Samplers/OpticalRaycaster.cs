e05e6df4a326f6628023a9a51523549c87749c7f
DRAAGBAREWYTZE\wytze <wytzeaaa@gmail.com>
2026-08-21 12:04:17 +0200

    ﻿Create depth renderTexture runtime, so it's compatible with target device.

--- Assets/_Application/Samplers/OpticalRaycaster.cs
+++ Assets/_Application/Samplers/OpticalRaycaster.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 namespace Netherlands3D.Twin.Samplers
 {
@@ -6,12 +7,30 @@ namespace Netherlands3D.Twin.Samplers
     {
         [SerializeField] private Camera depthCamera;
         private Texture2D samplerTexture;
+        private RenderTexture renderTexture;
         private const int defaultRaycastLayers = ~((1 << 2) + (1 << 12) + (1 << 13) + (1 << 14)); // all layers except IgnoreRaycast, Projected, PolygonMask, PolygonMaskInverted
-
+        private const int MINIMUM_DEPTH_BUFFER_FORMAT = 16; //In the render graph API, the output Render Texture must have a depth buffer, this is the minimum value to keep the render texture light weight.
+        
         void Start()
         {
             //We will only render on demand using camera.Render()
             depthCamera.enabled = false;
+            
+            //because of webgl we cannot create a rendertexture with the prefered format.
+            //the following error will occur in webgl if done so:
+            //RenderTexture.Create failed: format unsupported for random writes - RGBA32 SFloat (52).
+            //weirdly enough creating a deptht5exture in project and passing it through a serializefield is ok on webgl
+            //but we cannot do this since we need a pool and create a rendertexture for each request
+            
+            var graphicsFormat = SystemInfo.GetCompatibleFormat(GraphicsFormat.R32G32B32A32_SFloat, FormatUsage.Render);
+            renderTexture = new RenderTexture(1, 1, MINIMUM_DEPTH_BUFFER_FORMAT, RenderTextureFormat.Depth)
+            {
+                graphicsFormat = graphicsFormat,
+                filterMode = FilterMode.Point,
+                wrapMode = TextureWrapMode.Clamp
+            };
+            renderTexture.Create();
+            depthCamera.targetTexture = renderTexture;
             
             //Create a red channel texture that we can sample depth from
             samplerTexture = new Texture2D(depthCamera.targetTexture.width, depthCamera.targetTexture.height, TextureFormat.RGBAFloat, false);
@@ -20,6 +39,9 @@ namespace Netherlands3D.Twin.Samplers
         private void OnDestroy()
         {
             Destroy(samplerTexture);
+            depthCamera.targetTexture = null; 
+            renderTexture.Release();
+            Destroy(renderTexture);
         }
         
         /// <summary>
