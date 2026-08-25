5affa40f65983e79bffe5c9918450509a80de92e
ProtofunkWombat <sterre@protofunk.nl>
2026-08-25 14:03:52 +0200

    Uniform input field styling
    Standardize text and number fields with consistent heights, corners, spacing, and interaction states. Standard text fields retain their light grey background, while the search bar has no hover state. This also makes search and property inputs less bulky and includes a styling fix for the H input.

--- Assets/_Functionalities/FirstPersonViewer/Scripts/FirstPersonViewerInput.cs
+++ Assets/_Functionalities/FirstPersonViewer/Scripts/FirstPersonViewerInput.cs
@@ -6,6 +6,7 @@ using Netherlands3D.Twin;
 using UnityEngine.Events;
 using Cursor = UnityEngine.Cursor;
 using Netherlands3D.UI_Toolkit.Scripts;
+using UnityEngine.UIElements;
 
 namespace Netherlands3D.FirstPersonViewer
 {
@@ -184,19 +185,13 @@ namespace Netherlands3D.FirstPersonViewer
 
         public bool IsInputfieldSelected()
         {
-            return false; //todo: rewrite this function for UI Toolkit. We have no inputfields in the MVP, so this is currently always false
-            // GameObject selected = EventSystem.current.currentSelectedGameObject;
-            //
-            // if (selected == null)
-            // {
-            //     selectedUI = null;
-            //     return false;
-            // }
-            //
-            // if (selected == selectedUI) return isEditingInputfield;
-            //
-            // selectedUI = selected;
-            // return selected.GetComponent<TMP_InputField>() != null;
+            var focusedElement = App.UIRoot.Root?.focusController?.focusedElement as VisualElement;
+
+            return focusedElement is TextField
+                || focusedElement is IntegerField
+                || focusedElement is FloatField
+                || focusedElement is DoubleField
+                || focusedElement is LongField;
         }
 
         public void SetMouseLockModus(bool lockMouseModus) => this.lockMouseModus = lockMouseModus;
