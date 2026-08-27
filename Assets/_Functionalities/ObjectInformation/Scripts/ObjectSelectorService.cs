7e649fa3592521604cf0755f6145b00cc6d377e1
Tom Simons <tom.v.simons@gmail.com>
2026-08-27 17:30:16 +0200

    Fixed using wrong camera in fpv mode

--- Assets/_Functionalities/ObjectInformation/Scripts/ObjectSelectorService.cs
+++ Assets/_Functionalities/ObjectInformation/Scripts/ObjectSelectorService.cs
@@ -172,7 +172,7 @@ namespace Netherlands3D.Functionalities.ObjectInformation
             //dont select any feature if a gizmo handle is interacted with
             //todo make sure these colliders are associated with gizmo colliders
             Vector2 screenPoint = Pointer.current.position.ReadValue();
-            Ray ray = Camera.main.ScreenPointToRay(screenPoint);
+            Ray ray = App.Cameras.ActiveCamera.ScreenPointToRay(screenPoint);
             int hitCount = Physics.RaycastNonAlloc(ray, selectedColliderHits, Mathf.Infinity);
             //we have to loop through all potential colliders in case of a gizmo handle on top of another visualisation
             bool hasHit = false;
