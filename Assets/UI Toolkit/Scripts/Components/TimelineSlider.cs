3f822e6708ab65cf4b996d1b9f93542cb133e764
Tom Simons <tom.v.simons@gmail.com>
2026-09-10 15:20:37 +0200

    started adding TimelineSlider component

--- Assets/UI Toolkit/Scripts/Components/TimelineSlider.cs
+++ Assets/UI Toolkit/Scripts/Components/TimelineSlider.cs
@@ -1,3 +1,4 @@
+using System;
 using Netherlands3D.Services;
 using Netherlands3D.Sun;
 using Netherlands3D.UI.ExtensionMethods;
@@ -7,81 +8,153 @@ using UnityEngine.UIElements;
 namespace Netherlands3D.UI.Components
 {
     [UxmlElement]
-    public partial class TimelineSlider : UnityEngine.UIElements.Slider
+    public partial class TimelineSlider : VisualElement
     {
-        public enum HeaderType
+        private float maxDragDistance = 100f;
+        private float maxScrubSpeed = 2f;
+        
+        private Slider slider;
+        private VisualElement scrubber;
+        private Label currentTimeLabel;
+        private NumberField minField;
+        private NumberField maxField;
+
+        private bool isDragging;
+        private float dragStartX;
+        private float currentDragOffset;
+        // private IVisualElementScheduledItem scrubTicker;
+        
+        [UxmlAttribute("max-drag-distance")]
+        public float MaxDragDistance
         {
-            Normal,
-            NoHeader
+            get => maxDragDistance;
+            set => maxDragDistance = value;
         }
-
-        private HeaderType headerType = HeaderType.Normal;
-
-        [UxmlAttribute("header-type")]
-        public HeaderType Header
+        
+        [UxmlAttribute("max-scrub-speed")]
+        public float MaxScrubSpeed
         {
-            get => headerType;
-            set { headerType = value; UpdateClassList(); }
+            get => maxScrubSpeed;
+            set => maxScrubSpeed = value;
         }
-
+        
         public TimelineSlider()
         {
             this.CloneComponentTree("Components");
             this.AddComponentStylesheet("Components");
-
-            if (string.IsNullOrEmpty(label))
-                label = "Label";
-
-            showInputField = false;
-            fill = true;
             
-            RegisterCallback<AttachToPanelEvent>(_ => ApplyTextFieldClassToInput());
-            schedule.Execute(SetInitialDate);
-
-            this.RegisterValueChangedCallback(OnSliderChanged);
+            slider = this.Q<Slider>("Timeline");
+            scrubber = this.Q<VisualElement>("Scrubber");
+            currentTimeLabel = scrubber.Q<Label>("CurrentTimeLabel");
+            // minField = this.Q<NumberField>("MinField");
+            // maxField = this.Q<NumberField>("MaxField");
+             
+            SetInitialDate();
+            // InitBoundsFields();
             
+            RegisterCallback<AttachToPanelEvent>(ApplyTextFieldClassToInput);
+            slider.RegisterValueChangedCallback(OnSliderChanged);
+            var dragManipulator = new DragManipulator(4);
+            scrubber.AddManipulator(dragManipulator);
+            dragManipulator.DragStarted.AddListener(OnScrubberDragStart);
+            dragManipulator.Dragging.AddListener(OnScrubberDrag);
+            dragManipulator.DragEnded.AddListener(OnScrubberDragEnd);
+            
+            if(Application.isPlaying)
+                schedule.Execute(UpdateSlider).Every(0);
         }
 
+        private void OnScrubberDragStart(Vector2 startPosition)
+        {
+            currentDragOffset = 0f;
+            isDragging = true;
+        }
+
+        private void OnScrubberDrag(Vector2 delta)
+        {
+            currentDragOffset += delta.x;
+            scrubber.style.translate = new Translate(Mathf.Clamp(currentDragOffset, -MaxDragDistance, MaxDragDistance), 0);
+        }
+        
+        private void UpdateSlider()
+        {
+            var normalized = Mathf.Clamp(currentDragOffset / MaxDragDistance, -1f, 1f);
+            var eased = Mathf.Sign(normalized) * normalized * normalized;
+            var speed = eased * MaxScrubSpeed;
+            var newValue = Mathf.Clamp(slider.value + speed * Time.deltaTime, slider.lowValue, slider.highValue);
+            slider.value = newValue;
+        }
+        
+        private void OnScrubberDragEnd(Vector2 endPosition)
+        {
+            currentDragOffset = 0f;
+            isDragging = false;
+        }
+        
+        private void InitBoundsFields()
+        {
+            minField.SetValueWithoutNotify(slider.lowValue);
+            maxField.SetValueWithoutNotify(slider.highValue);
+
+            // minField.RegisterValueChangedCallback(OnMinFieldChanged);
+            // maxField.RegisterValueChangedCallback(OnMaxFieldChanged);
+        }
+        
+        private void OnMinFieldChanged(ChangeEvent<float> evt)
+        {
+            var newValue = Mathf.Min(evt.newValue, slider.highValue);
+            slider.lowValue = newValue;
+            minField.SetValueWithoutNotify(newValue);
+        }
+
+        private void OnMaxFieldChanged(ChangeEvent<float> evt)
+        {
+            var newMax = Mathf.Max(evt.newValue, slider.lowValue);
+            slider.highValue = newMax;
+            maxField.SetValueWithoutNotify(newMax);
+        }
+        
         private void OnSliderChanged(ChangeEvent<float> evt)
         {
-            var year = (int)evt.newValue;
-            var month = (int)Mathf.Lerp(1, 12, evt.newValue-year);
-            SetDate(month, year);
+            int year = Mathf.FloorToInt(evt.newValue);
+            float fraction = evt.newValue - year;
+            int totalDays = DateTime.IsLeapYear(year) ? 366 : 365;
+            int dayOfYear = Mathf.Clamp(Mathf.FloorToInt(fraction * totalDays), 0, totalDays - 1);
+
+            DateTime date = new DateTime(year, 1, 1).AddDays(dayOfYear);
+
+            SetDate(date);
         }
 
-        private void SetDate(int month, int year)
+
+        private void SetDate(DateTime dateTime)
         {
-            ServiceLocator.GetService<SunTime>().SetDate(1, month,  year);
-            label = "1 - " + month +  " - " + year;
+            ServiceLocator.GetService<SunTime>().SetDate(dateTime.Day, dateTime.Month,  dateTime.Year);
+            currentTimeLabel.text = dateTime.ToString("MM/dd/yyyy");
         }
 
         
         void SetInitialDate()
         {
-            var time = ServiceLocator.GetService<SunTime>()?.Time;
-            if(time.HasValue)
-                SetDate(time.Value.Month, time.Value.Year);
+            var dateTime = ServiceLocator.GetService<SunTime>()?.Time;
+            if(dateTime.HasValue)
+                SetDate(dateTime.Value);
         }
 
         
-        private void ApplyTextFieldClassToInput()
+        private void ApplyTextFieldClassToInput(AttachToPanelEvent evt)
         {
-            var inputText = this.Q<TextElement>(className: "unity-text-element--inner-input-field-component");
-            if (inputText == null)
-                return;
-
-            inputText.AddToClassList("text-base");
-
-            var input = this.Q<VisualElement>(className: "unity-base-text-field");
-            if (input == null)
-                return;
-
-            input.AddToClassList("textfield");
-        }
-
-        private void UpdateClassList()
-        {
-            this.ReplacePrefixedValueInClassList("slider-header-", headerType.ToString().ToKebabCase());
+            // var inputText = this.Q<TextElement>(className: "unity-text-element--inner-input-field-component");
+            // if (inputText == null)
+            //     return;
+            //
+            // inputText.AddToClassList("text-base");
+            //
+            // var input = this.Q<VisualElement>(className: "unity-base-text-field");
+            // if (input == null)
+            //     return;
+            //
+            // input.AddToClassList("textfield");
         }
     }
 }
