44279025f938f88116b404b98b6e48c545ebb50d
bozmir <bozmir@hotmail.com>
2026-09-17 16:25:21 +0200

    GeoJsonAnnotationVisualisationLayer setup + expanding the prefab

---
+++ Assets/_Application/Layers/LayerTypes/GeoJsonLayers/GeoJSONAnnotationLayer.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GeoJSON.Net;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Netherlands3D.Coordinates;
+using Netherlands3D.LayerStyles;
+using Netherlands3D.Twin.Rendering;
+using Netherlands3D.Twin.Utility;
+using UnityEngine;
+
+namespace Netherlands3D.Twin.Layers.LayerTypes.GeoJsonLayers
+{
+    [Serializable]
+    public class GeoJSONAnnotationLayer : MonoBehaviour, IGeoJsonVisualisationLayer
+    {
+        public string DisplayName => "Annotaties";
+        public string StylingColorProperty => Symbolizer.FillColorProperty;
+        public bool SupportsGeometryType(Feature feature)
+        {
+            return  feature is ImageAnnotation;
+        }
+
+        public int FeatureCount => spawnedVisualisations.Count;
+
+        public Transform Transform => transform;
+
+        public event IGeoJsonVisualisationLayer.GeoJsonHandler FeatureRemoved;
+
+        private Dictionary<Feature, GeoJSONPointLayer.FeaturePointVisualisations> spawnedVisualisations = new();
+        
+        private List<List<Coordinate>> visualisationsToRemove = new();
+
+        [SerializeField] private Material annotationMaterial;
+
+        public struct ImageAnnotation
+        {
+            //private WorldText worldText;
+        }
+        
+
+        public Color RenderColor
+        {
+            get
+            {
+                return annotationMaterial.color;
+            }
+            set
+            {
+                annotationMaterial = new Material(annotationMaterial);
+            }
+        }
+
+        public Material RenderMaterial
+        {
+            get
+            {
+                return annotationMaterial;
+            }
+        }
+
+        public List<Mesh> GetMeshData(Feature feature)
+        {
+            return null;
+        }
+
+        public Bounds GetFeatureBounds(Feature feature)
+        {
+            return spawnedVisualisations[feature].trueBounds;
+        }
+
+        public float GetSelectionRange()
+        {
+            return 5;
+        }
+
+        //here we have to local offset the vertices with the position of the transform because the transform gets shifted
+        //also we are using the actual feature geometry to find the vertices in the targeted buffers
+        public void SetVisualisationSelected(Transform transform, List<Mesh> meshes, Color color)
+        {
+           
+        }
+
+        public void SetVisualisationDeselected() 
+        {
+            
+        }
+
+        public void OnLayerActiveInHierarchyChanged(bool activeInHierarchy)
+        {
+            
+        }
+
+        public void AddAndVisualizeFeature(Feature feature, CoordinateSystem originalCoordinateSystem, bool activeInHierarchy)
+        {
+            // Skip if feature already exists (comparison is done using hashcode based on geometry)
+            if (spawnedVisualisations.ContainsKey(feature))
+                return;
+
+            // var newFeatureVisualisation = new GeoJSONPointLayer.FeaturePointVisualisations { feature = feature };
+            //
+            // if (feature.Geometry is MultiPoint multiPoint)
+            // {
+            //     var newPointCollection = GeometryVisualizationFactory.CreatePointVisualisation(multiPoint, originalCoordinateSystem, PointRenderer3D);
+            //     newFeatureVisualisation.Data.Add(newPointCollection);
+            // }
+            // else if (feature.Geometry is Point point)
+            // {
+            //     var newPointCollection = GeometryVisualizationFactory.CreatePointVisualization(point, originalCoordinateSystem, PointRenderer3D);
+            //     newFeatureVisualisation.Data.Add(newPointCollection);
+            // }
+
+            // newFeatureVisualisation.SetBoundsPadding(Vector3.one * GetSelectionRange());
+            // newFeatureVisualisation.CalculateBounds();
+            // spawnedVisualisations.Add(feature, newFeatureVisualisation);
+        }
+
+        /// <summary>
+        /// Checks the Bounds of the visualisations and checks them against the camera frustum
+        /// to remove visualisations that are out of view
+        /// </summary>
+        public void RemoveFeaturesOutOfView()
+        {
+            // var frustumPlanes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+            //
+            // visualisationsToRemove.Clear();
+            // foreach (var kvp in spawnedVisualisations.Reverse())
+            // {
+            //     var inCameraFrustum = GeometryUtility.TestPlanesAABB(frustumPlanes, kvp.Value.tiledBounds);
+            //     if (inCameraFrustum) continue;
+            //
+            //     visualisationsToRemove.AddRange(kvp.Value.Data);
+            //     RemoveFeature(kvp.Value);
+            // }
+            // PointRenderer3D.RemovePointCollections(visualisationsToRemove);
+        }
+
+        private void RemoveFeature(GeoJSONPointLayer.FeaturePointVisualisations featureVisualisation)
+        {
+            FeatureRemoved?.Invoke(featureVisualisation.feature);
+            spawnedVisualisations.Remove(featureVisualisation.feature);
+        }
+        
+        public BoundingBox GetBoundingBoxOfVisibleFeatures()
+        {
+            if (spawnedVisualisations.Count == 0)
+                return null;
+
+            BoundingBox bbox = null;
+            foreach (var vis in spawnedVisualisations.Values)
+            {
+                if (bbox == null)
+                    bbox = new BoundingBox(vis.trueBounds);
+                else
+                    bbox.Encapsulate(vis.trueBounds);
+            }
+            var crs2D = CoordinateSystems.To2D(bbox.CoordinateSystem);
+            bbox.Convert(crs2D); //remove the height, since a GeoJSON is always 2D. This is needed to make the centering work correctly
+            return bbox;
+        }
+    }
+}
--- Assets/_Application/Layers/LayerTypes/GeoJsonLayers/IGeoJsonVisualisationLayer.cs
+++ Assets/_Application/Layers/LayerTypes/GeoJsonLayers/IGeoJsonVisualisationLayer.cs
@@ -1,7 +1,6 @@
 using GeoJSON.Net.Feature;
 using Netherlands3D.Coordinates;
 using System.Collections.Generic;
-using GeoJSON.Net;
 using Netherlands3D.Twin.Utility;
 using UnityEngine;
 
@@ -27,67 +26,5 @@ namespace Netherlands3D.Twin.Layers.LayerTypes.GeoJsonLayers
         void RemoveFeaturesOutOfView();
         delegate void GeoJsonHandler(Feature feature);
         event GeoJsonHandler FeatureRemoved;
-    }
-
-    public class GeoJsonAnnotationVisualisationLayer : IGeoJsonVisualisationLayer
-    {
-        public string DisplayName { get; }
-        public string StylingColorProperty { get; }
-
-        public bool SupportsGeometryType(Feature geometryType)
-        {
-            return geometryType.Equals(GeoJSONObjectType.Point) && geometryType.Properties.ContainsKey("imageUrl");
-        }
-
-        public int FeatureCount { get; }
-        public Transform Transform { get; }
-        public Color RenderColor { get; set; }
-        public Material RenderMaterial { get; }
-        public List<Mesh> GetMeshData(Feature feature)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void SetVisualisationSelected(Transform transform, List<Mesh> meshes, Color color)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void SetVisualisationDeselected()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void AddAndVisualizeFeature(Feature feature, CoordinateSystem originalCoordinateSystem, bool activeInHierarchy)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnLayerActiveInHierarchyChanged(bool activeInHierarchy)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public BoundingBox GetBoundingBoxOfVisibleFeatures()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Bounds GetFeatureBounds(Feature feature)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public float GetSelectionRange()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void RemoveFeaturesOutOfView()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public event IGeoJsonVisualisationLayer.GeoJsonHandler FeatureRemoved;
     }
 }
