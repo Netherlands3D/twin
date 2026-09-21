1e5e1a607b00943c60cd7e19d8963b07a36c2928
DRAAGBAREWYTZE\wytze <wytzeaaa@gmail.com>
2026-09-21 15:32:18 +0200

    Show hide sections on roll over

--- Assets/UI Toolkit/Scripts/Components/DefaultHUD.cs
+++ Assets/UI Toolkit/Scripts/Components/DefaultHUD.cs
@@ -6,10 +6,35 @@ namespace Netherlands3D.UI.Panels
     [UxmlElement]
     public partial class DefaultHUD : VisualElement
     {
+        private const string HiddenClass = "presentation-section--hidden";
+        
         public DefaultHUD()
         {
             this.CloneComponentTree("Components");
             this.AddComponentStylesheet("Components");
+            
+            SetupAutoHideSection("LeftSection", "LeftRevealZone");
+            SetupAutoHideSection("TopSection", "TopRevealZone");
+            SetupAutoHideSection("BottomSection", "BottomRevealZone");
         }
+
+        private void SetupAutoHideSection(
+            string sectionName,
+            string revealZoneName)
+        {
+            var section = this.Q<VisualElement>(sectionName);
+            var revealZone = this.Q<VisualElement>(revealZoneName);
+
+            void Show() => section.RemoveFromClassList(HiddenClass);
+            void Hide() => section.AddToClassList(HiddenClass);
+
+            revealZone.RegisterCallback<PointerEnterEvent>(_ => Show());
+            revealZone.RegisterCallback<PointerLeaveEvent>(_ => Hide());
+
+            section.RegisterCallback<PointerEnterEvent>(_ => Show());
+            section.RegisterCallback<PointerLeaveEvent>(_ => Hide());
+        }
+        
+        
     }
 }
